fc20382588d8bb6208ffce85dd6d1ec69ff049ce
pezholio <pezholio@gmail.com>
2022-07-20 14:02:02 +0100

    Add method to return a summary list for a premises
    This adds a new `getPremisesDetails` method that returns data for
    a particular premises for use in show template. It returns the name of
    the premises and the details as an object which can be used in a
    `govukSummaryList` macro. I’ve also refactored the `tableRows` method
    to use a private method to return either a HMTL or Text item to DRY
    things up a bit, as well as adding some new types.

--- server/@types/approved-premises/index.d.ts
+++ server/@types/approved-premises/index.d.ts
@@ -1,9 +1,39 @@
 declare module 'approved-premises' {
   export type Premises = schemas['Premises']
 
-  export type TableCell = { text: string; attributes?: { [key: string]: string } } | { html: string }
+  export interface HtmlAttributes {
+    [key: string]: string
+  }
+
+  export interface TextItem {
+    text: string
+  }
+
+  export interface HtmlItem {
+    html: string
+  }
+
+  export type TableCell = { text: string; attributes?: HtmlAttributes } | { html: string }
   export interface TableRow {
     [index: number]: TableCell
+  }
+
+  export interface SummaryListActionItem {
+    href: string
+    text: string
+    visuallyHiddenText: string
+  }
+
+  export interface SummaryListItem {
+    key: TextItem | HtmlItem
+    value: TextItem | HtmlItem
+    actions?: { items: Array<SummaryListActionItem> }
+  }
+
+  export interface SummaryList {
+    classes?: string
+    attributes?: HtmlAttributes
+    rows: Array<SummaryListItem>
   }
 
   export interface schemas {
--- server/services/premisesService.ts
+++ server/services/premisesService.ts
@@ -1,33 +1,63 @@
-import type { Premises, TableRow } from 'approved-premises'
+import type { Premises, TableRow, SummaryList } from 'approved-premises'
 import type { RestClientBuilder, PremisesClient } from '../data'
 
 export default class PremisesService {
+  // TODO: We need to do some more work on authentication to work
+  // out how to get this token, so let's stub for now
+  token = 'FAKE_TOKEN'
+
   constructor(private readonly premisesClientFactory: RestClientBuilder<PremisesClient>) {}
 
   async tableRows(): Promise<Array<TableRow>> {
-    // TODO: We need to do some more work on authentication to work
-    // out how to get this token, so let's stub for now
-    const token = 'FAKE_TOKEN'
-    const premisesClient = this.premisesClientFactory(token)
+    const premisesClient = this.premisesClientFactory(this.token)
     const premises = await premisesClient.getAllPremises()
 
     return premises
       .sort((a, b) => a.name.localeCompare(b.name))
       .map((p: Premises) => {
         return [
-          {
-            text: p.name,
-          },
-          {
-            text: p.apCode,
-          },
-          {
-            text: p.bedCount.toString(),
-          },
-          {
-            html: `<a href="/premises/${p.id}">View<span class="govuk-visually-hidden">about ${p.name}</span></a>`,
-          },
+          this.textValue(p.name),
+          this.textValue(p.apCode),
+          this.textValue(p.bedCount.toString()),
+          this.htmlValue(
+            `<a href="/premises/${p.id}">View<span class="govuk-visually-hidden">about ${p.name}</span></a>`,
+          ),
         ]
       })
+  }
+
+  async getPremisesDetails(id: string): Promise<{ name: string; summaryList: SummaryList }> {
+    const premisesClient = this.premisesClientFactory(this.token)
+    const premises = await premisesClient.getPremises(id)
+    const summaryList = await this.summaryListForPremises(premises)
+
+    return { name: premises.name, summaryList }
+  }
+
+  async summaryListForPremises(premises: Premises): Promise<SummaryList> {
+    return {
+      rows: [
+        {
+          key: this.textValue('Code'),
+          value: this.textValue(premises.apCode),
+        },
+        {
+          key: this.textValue('Postcode'),
+          value: this.textValue(premises.postcode),
+        },
+        {
+          key: this.textValue('Number of Beds'),
+          value: this.textValue(premises.bedCount.toString()),
+        },
+      ],
+    }
+  }
+
+  private textValue(value: string) {
+    return { text: value }
+  }
+
+  private htmlValue(value: string) {
+    return { html: value }
   }
 }

8db0b6c5946a921531bca51652cb4beb32f86280
rich <rich.james@dxw.com>
2022-07-20 16:01:07 +0100

    Add view and E2E tests
    Here we add the view to create the booking and a happy path test to
    verify that the inputs exist and the user can enter details into them.
    Again there is not validation for the inputs, the user could enter bad
    data or no data - validation is carried out by the API and will be
    passed up to the view.

---
+++ integration_tests/e2e/booking.cy.ts
@@ -0,0 +1,52 @@
+import premisesFactory from '../../server/testutils/factories/premises'
+import bookingFactory from '../../server/testutils/factories/booking'
+import BookingPage from '../pages/booking'
+import PremisesPage from '../pages/premises'
+import Page from '../pages/page'
+
+context('Booking', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.task('stubAuthUser')
+  })
+
+  it('should show booking form', () => {
+    const booking = bookingFactory.build({
+      CRN: '1bee477b-462f-47c1-8f71-7835a76a2c42',
+      arrivalDate: new Date(Date.UTC(2022, 5, 1, 0, 0, 0)),
+      expectedDepartureDate: new Date(Date.UTC(2022, 5, 3, 0, 0, 0)),
+      keyWorker: 'Lizeth Hagenes',
+    })
+
+    const premises = premisesFactory.buildList(5)
+    cy.task('stubBookingCreate', { premisesId: 'premisesId', booking })
+    cy.task('stubPremises', premises)
+
+    // Given I am signed in
+    cy.signIn()
+
+    // When I visit the booking page
+    const page = BookingPage.visit()
+
+    // And I fill in the booking form
+    page.completeForm(booking)
+    page.clickSubmit()
+
+    // Then I should be redirected to the premises page
+    Page.verifyOnPage(PremisesPage)
+
+    PremisesPage.shouldShowFlashMessage()
+
+    // And the booking should be created in the API
+    cy.task('verifyBookingCreate', { premisesId: 'premisesId' }).then(requests => {
+      expect(requests).to.have.length(1)
+      const requestBody = JSON.parse(requests[0].body)
+
+      expect(requestBody.CRN).equal('1bee477b-462f-47c1-8f71-7835a76a2c42')
+      expect(requestBody.arrivalDate).equal((booking.arrivalDate as Date).toISOString())
+      expect(requestBody.expectedDepartureDate).equal((booking.expectedDepartureDate as Date).toISOString())
+      expect(requestBody.keyWorker).equal('Lizeth Hagenes')
+    })
+  })
+})
---
+++ integration_tests/pages/booking.ts
@@ -0,0 +1,71 @@
+import type { Booking } from 'approved-premises'
+import Page, { PageElement } from './page'
+
+export default class BookingPage extends Page {
+  constructor() {
+    super('Make a booking')
+  }
+
+  static visit(): BookingPage {
+    cy.visit('/premises/premisesId/booking/new')
+    return new BookingPage()
+  }
+
+  getLabel(labelName: string): void {
+    cy.get('label').should('contain', labelName)
+  }
+
+  getLegend(legendName: string): void {
+    cy.get('legend').should('contain', legendName)
+  }
+
+  getByInputByIdAndEnterDetails(id: string, details: string): void {
+    cy.get(`#${id}`).type(details)
+  }
+
+  arrivalDay(): PageElement {
+    return cy.get('#arrival-date-day')
+  }
+
+  arrivalMonth(): PageElement {
+    return cy.get('#arrival-date-month')
+  }
+
+  arrivalYear(): PageElement {
+    return cy.get('#arrival-date-year')
+  }
+
+  expectedDepartureDay(): PageElement {
+    return cy.get('#expected-departure-date-day')
+  }
+
+  expectedDepartureMonth(): PageElement {
+    return cy.get('#expected-departure-date-month')
+  }
+
+  expectedDepartureYear(): PageElement {
+    return cy.get('#expected-departure-date-year')
+  }
+
+  clickSubmit(): void {
+    cy.get('button').click()
+  }
+
+  completeForm(booking: Booking): void {
+    this.getLabel('CRN')
+    this.getByInputByIdAndEnterDetails('CRN', booking.CRN)
+
+    this.getLegend('What is the arrival date?')
+    this.arrivalDay().type((booking.arrivalDate as Date).getDate().toString())
+    this.arrivalMonth().type(`${(booking.arrivalDate as Date).getMonth() + 1}`)
+    this.arrivalYear().type((booking.arrivalDate as Date).getFullYear().toString())
+
+    this.getLegend('What is the expected departure date?')
+    this.expectedDepartureDay().type((booking.expectedDepartureDate as Date).getDate().toString())
+    this.expectedDepartureMonth().type(`${(booking.expectedDepartureDate as Date).getMonth() + 1}`)
+    this.expectedDepartureYear().type((booking.expectedDepartureDate as Date).getFullYear().toString())
+
+    this.getLabel('Key Worker')
+    this.getByInputByIdAndEnterDetails('keyWorker', booking.keyWorker)
+  }
+}

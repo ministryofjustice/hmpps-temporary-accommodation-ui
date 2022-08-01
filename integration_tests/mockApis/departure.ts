a089b432c9362fc12a663d78d62efb84efb49d0a
rich <rich.james@dxw.com>
2022-08-01 16:25:25 +0100

    Create departures form
    There isn’t currently a time input in the govuk npm package so I’ve
    opted to go with a native HTML time input styled like a govuk input.
    We will have to be conscious of any usability issues with this input
    especially across browsers.

---
+++ integration_tests/mockApis/departure.ts
@@ -0,0 +1,27 @@
+import { SuperAgentRequest } from 'superagent'
+
+import type { Departure } from 'approved-premises'
+
+import { stubFor, getMatchingRequests } from '../../wiremock'
+
+export default {
+  stubDepartureCreate: (args: { premisesId: string; bookingId: string; departure: Departure }): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'POST',
+        url: `/premises/${args.premisesId}/bookings/${args.bookingId}/departures`,
+      },
+      response: {
+        status: 201,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: args.departure,
+      },
+    }),
+  verifyDepartureCreate: async (args: { premisesId: string; bookingId: string }) =>
+    (
+      await getMatchingRequests({
+        method: 'POST',
+        url: `/premises/${args.premisesId}/bookings/${args.bookingId}/departures`,
+      })
+    ).body.requests,
+}
---
+++ integration_tests/pages/departureCreate.ts
@@ -0,0 +1,42 @@
+import type { Departure, Booking } from 'approved-premises'
+
+import Page from './page'
+
+export default class DepartureCreatePage extends Page {
+  constructor(private readonly premisesId: string, private readonly bookingId: string) {
+    super('Log a departure')
+  }
+
+  static visit(premisesId: string, bookingId: string): DepartureCreatePage {
+    cy.visit(`/premises/${premisesId}/bookings/${bookingId}/departures/new`)
+    return new DepartureCreatePage(premisesId, bookingId)
+  }
+
+  public verifySummary(booking: Booking): void {
+    this.assertDefinition('Name', booking.name)
+    this.assertDefinition('CRN', booking.CRN)
+  }
+
+  public completeForm(departure: Departure): void {
+    const dateTime = new Date(Date.parse(departure.dateTime))
+    const minutes = dateTime.getMinutes()
+    const hours = dateTime.getHours()
+
+    cy.get('input[name="dateTime-day"]').type(String(dateTime.getDate()))
+    cy.get('input[name="dateTime-month"]').type(String(dateTime.getMonth() + 1))
+    cy.get('input[name="dateTime-year"]').type(String(dateTime.getFullYear()))
+    cy.get('input[name="dateTime-time"]').type(`${hours}:${minutes}`)
+
+    cy.get('#destinationAp').select(departure.destinationAp)
+
+    cy.get('input[name="departure[reason]"]').last().check()
+
+    cy.get('input[name="departure[moveOnCategory]"]').last().check()
+
+    cy.get('input[name="departure[destinationProvider]"]').last().check()
+
+    cy.get('textarea[name="departure[notes]"]').type(departure.notes)
+
+    cy.get('button').click()
+  }
+}

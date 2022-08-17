fd9e92f77edc1c8784fed3b8b255713a577b838b
pezholio <pezholio@gmail.com>
2022-08-17 11:07:15 +0100

    Add `availableBedsForToday` to a premises

--- server/testutils/factories/premises.ts
+++ server/testutils/factories/premises.ts
@@ -9,5 +9,6 @@ export default Factory.define<Premises>(() => ({
   apCode: faker.random.alphaNumeric(5, { casing: 'upper' }),
   postcode: faker.address.zipCode(),
   bedCount: 50,
+  availableBedsForToday: faker.datatype.number({ min: 0, max: 50 }),
   apAreaId: faker.random.alphaNumeric(2, { casing: 'upper' }),
 }))

790ae32032a8091adb3a327345e767830ef51fc9
rich <rich.james@dxw.com>
2022-09-02 11:51:12 +0100

    Create view for finding a person by CRN
    We also need to adapt the premises/show.njk to take us to the new view
    when ‘Create a booking’ is clicked.
    We need a hidden input here to pass through the CRN to the create method
    on the BookingController

---
+++ integration_tests/pages/booking/find.ts
@@ -0,0 +1,27 @@
+import Page from '../page'
+import paths from '../../../server/paths/manage'
+
+export default class BookingFindPage extends Page {
+  constructor() {
+    super('Make a booking')
+  }
+
+  static visit(premisesId: string): BookingFindPage {
+    cy.visit(paths.bookings.new({ premisesId }))
+    return new BookingFindPage()
+  }
+
+  enterCrn(crn: string): void {
+    this.getLabel('Find someone by CRN')
+    this.getTextInputByIdAndEnterDetails('crn', crn)
+  }
+
+  clickSubmit(): void {
+    cy.get('button').click()
+  }
+
+  completeForm(crn: string): void {
+    this.enterCrn(crn)
+    this.clickSubmit()
+  }
+}

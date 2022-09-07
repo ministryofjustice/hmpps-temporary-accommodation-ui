521e1e64952ec407a443fbac39299d62539f00c3
pezholio <pezholio@gmail.com>
2022-09-07 12:37:18 +0100

    Add a service method to get a current page
    This accepts the request and fetches a page from the list of classes
    exported in the `form-pages` directory. If a task is not defined, or
    a page is not found, an `UnknownPageError` is raised.

--- server/services/applicationService.test.ts
+++ server/services/applicationService.test.ts
@@ -1,6 +1,28 @@
+import type { Request } from 'express'
+import { createMock } from '@golevelup/ts-jest'
+
+import { UnknownPageError } from '../utils/errors'
 import ApplicationService from './applicationService'
 import ApplicationClient from '../data/applicationClient'
+
+import pages from '../form-pages/apply'
+
+const FirstPage = jest.fn()
+const SecondPage = jest.fn()
+
+jest.mock('../form-pages/apply', () => {
+  return {
+    'my-task': {},
+  }
+})
+
+pages['my-task'] = {
+  first: FirstPage,
+  second: SecondPage,
+}
+
 jest.mock('../data/applicationClient.ts')
+
 describe('ApplicationService', () => {
   const applicationClient = new ApplicationClient(null) as jest.Mocked<ApplicationClient>
   const applicationClientFactory = jest.fn()
@@ -28,4 +50,41 @@ describe('ApplicationService', () => {
     })
   })
 
+  describe('getCurrentPage', () => {
+    const request = createMock<Request>({ params: { task: 'my-task' } })
+
+    it('should return the first page if the page is not defined', () => {
+      const result = service.getCurrentPage(request)
+
+      expect(result).toBeInstanceOf(FirstPage)
+
+      expect(FirstPage).toHaveBeenCalledWith(request.body)
+    })
+
+    it('should return a page from a page list', () => {
+      request.params.page = 'second'
+
+      const result = service.getCurrentPage(request)
+
+      expect(result).toBeInstanceOf(SecondPage)
+
+      expect(SecondPage).toHaveBeenCalledWith(request.body)
+    })
+
+    it('should raise an error if the page is not found', () => {
+      request.params.page = 'bar'
+
+      expect(() => {
+        service.getCurrentPage(request)
+      }).toThrow(UnknownPageError)
+    })
+
+    it('should raise an error if the task is not specified', () => {
+      request.params.task = undefined
+
+      expect(() => {
+        service.getCurrentPage(request)
+      }).toThrow(UnknownPageError)
+    })
+  })
 })
--- server/services/applicationService.ts
+++ server/services/applicationService.ts
@@ -1,5 +1,11 @@
 import type { Request } from 'express'
+import type { TasklistPage } from 'approved-premises'
+
 import type { RestClientBuilder, ApplicationClient } from '../data'
+import { UnknownPageError } from '../utils/errors'
+
+import pages from '../form-pages/apply'
+
 export default class ApplicationService {
   constructor(private readonly applicationClientFactory: RestClientBuilder<ApplicationClient>) {}
 
@@ -9,5 +15,18 @@ export default class ApplicationService {
     const uuid = await applicationClient.create()
 
     return uuid
+  }
+
+  getCurrentPage(request: Request): TasklistPage {
+    if (!request.params.task) {
+      throw new UnknownPageError()
+    }
+    const pageList = pages[request.params.task]
+    const Page = request.params.page ? pageList[request.params.page] : Object.values(pageList)[0]
+
+    if (!Page) {
+      throw new UnknownPageError()
+    }
+    return new Page(request.body)
   }
 }

ab1233a338e37b693d4ebc6949af0b25cfbe8e88
pezholio <pezholio@gmail.com>
2022-09-07 14:47:33 +0100

    Add release type step
    For this question, we’ll need to change the options depending on what
    the answer to the previous question was. Parking this until we have
    session handling in place.

--- server/form-pages/apply/basic-information/index.ts
+++ server/form-pages/apply/basic-information/index.ts
@@ -3,11 +3,13 @@
 import EnterCRN from './enterCrn'
 import ConfirmDetails from './confirmDetails'
 import SentenceType from './sentenceType'
+import ReleaseType from './releaseType'
 
 const pages = {
   'enter-crn': EnterCRN,
   'confirm-details': ConfirmDetails,
   'sentence-type': SentenceType,
+  'release-type': ReleaseType,
 }
 
 export default pages
---
+++ server/form-pages/apply/basic-information/releaseType.ts
@@ -0,0 +1,50 @@
+import type { TasklistPage } from 'approved-premises'
+
+// TODO: The options that appear will depend on the response to the previous question.
+// For now we'll return all options
+export enum ReleaseTypes {
+  rotl = 'Release on Temporary Licence (ROTL)',
+  hdc = 'Home detention curfew (HDC)',
+  licence = 'Licence',
+  pss = 'Post Sentence Supervision (PSS)',
+  reRelase = 'Re-release following recall',
+}
+
+export default class ReleaseType implements TasklistPage {
+  name = 'release-type'
+
+  title = 'What type of release will the application support'
+
+  constructor(private readonly body: Record<string, unknown>) {}
+
+  next() {
+    return 'release-date'
+  }
+
+  previous() {
+    return 'sentence-type'
+  }
+
+  errors() {
+    const errors = []
+
+    if (!this.body.releaseType) {
+      errors.push({
+        propertyName: 'releaseType',
+        errorType: 'blank',
+      })
+    }
+
+    return errors
+  }
+
+  items() {
+    return Object.keys(ReleaseTypes).map(key => {
+      return {
+        value: key,
+        text: ReleaseTypes[key],
+        checked: this.body.releaseType === key,
+      }
+    })
+  }
+}

6282cf56f901c7f2039e2e82b862b6139a0b6dbd
pezholio <pezholio@gmail.com>
2022-09-07 14:51:39 +0100

    Add an EnterCRN step
    This will be the first page on the first step of the application
    process. It accepts the body of the request as a constuctor argument
    and returns an error if the crn field is blank.

---
+++ server/form-pages/apply/basic-information/enterCrn.ts
@@ -0,0 +1,26 @@
+import type { TasklistPage } from 'approved-premises'
+
+export default class EnterCRN implements TasklistPage {
+  name = 'enter-crn'
+
+  title = "Enter the individual's CRN"
+
+  constructor(private readonly body: Record<string, unknown>) {}
+
+  next() {
+    return 'confirm-details'
+  }
+
+  errors() {
+    const errors = []
+
+    if (!this.body.crn) {
+      errors.push({
+        propertyName: 'crn',
+        errorType: 'blank',
+      })
+    }
+
+    return errors
+  }
+}
---
+++ server/form-pages/apply/basic-information/index.ts
@@ -0,0 +1,9 @@
+/* istanbul ignore file */
+
+import EnterCRN from './enterCrn'
+
+const pages = {
+  'enter-crn': EnterCRN,
+}
+
+export default pages

97b1bcac5cc7da4203317f631784ff43e6dbbbd2
pezholio <pezholio@gmail.com>
2022-09-07 14:46:41 +0100

    Add a situation page
    As per the previous commit, this will need some work doing to determine
    the questions from the answer to the previous question.

--- server/form-pages/apply/basic-information/index.ts
+++ server/form-pages/apply/basic-information/index.ts
@@ -4,12 +4,14 @@ import EnterCRN from './enterCrn'
 import ConfirmDetails from './confirmDetails'
 import SentenceType from './sentenceType'
 import ReleaseType from './releaseType'
+import Situation from './situation'
 
 const pages = {
   'enter-crn': EnterCRN,
   'confirm-details': ConfirmDetails,
   'sentence-type': SentenceType,
   'release-type': ReleaseType,
+  situation: Situation,
 }
 
 export default pages

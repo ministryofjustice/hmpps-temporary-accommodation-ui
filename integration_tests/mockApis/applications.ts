d62078d3f09642f32c5ac96438ba08cdea16d82d
rich <rich.james@dxw.com>
2022-09-09 12:07:23 +0100

    Create view and E2E tests
    This follows the work laid down by Beth in the [prototype repo](https://github.com/ministryofjustice/approved-premises-prototype/blob/main/app/views/pp-dashboard.html)

---
+++ integration_tests/mockApis/applications.ts
@@ -0,0 +1,20 @@
+import { SuperAgentRequest } from 'superagent'
+
+import type { ApplicationSummary } from 'approved-premises'
+
+import { stubFor } from '../../wiremock'
+
+export default {
+  stubApplications: (applicationSummaries: ApplicationSummary): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'GET',
+        url: `/applications/previous`,
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: applicationSummaries,
+      },
+    }),
+}

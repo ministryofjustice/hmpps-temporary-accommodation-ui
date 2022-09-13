46e6b8c4d8246b875ddc964e0409aa056e63886a
rich <rich.james@dxw.com>
2022-09-13 14:33:56 +0100

    Make Situation question page dependent on answer to Sentence type Q
    I’ve had to make a lot of TS annotations here.
    There may be an easier way of achieving what I want.

--- server/form-pages/apply/basic-information/situation.test.ts
+++ server/form-pages/apply/basic-information/situation.test.ts
@@ -3,25 +3,30 @@ import { itShouldHavePreviousValue, itShouldHaveNextValue } from '../../shared-e
 import Situation from './situation'
 
 describe('Situation', () => {
+  let session = { 'basic-information': { 'sentence-type': { sentenceType: 'communityOrder' } } }
+  beforeEach(() => {
+    session = { 'basic-information': { 'sentence-type': { sentenceType: 'communityOrder' } } }
+  })
+
   describe('body', () => {
     it('should strip unknown attributes from the body', () => {
-      const page = new Situation({ situation: 'riskManagement', something: 'else' })
+      const page = new Situation({ situation: 'riskManagement', something: 'else' }, session)
 
       expect(page.body).toEqual({ situation: 'riskManagement' })
     })
   })
 
-  itShouldHavePreviousValue(new Situation({}), 'sentence-type')
-  itShouldHaveNextValue(new Situation({}), 'release-date')
+  itShouldHavePreviousValue(new Situation({}, session), 'sentence-type')
+  itShouldHaveNextValue(new Situation({}, session), 'release-date')
 
   describe('errors', () => {
     it('should return an empty array if the situation is populated', () => {
-      const page = new Situation({ situation: 'riskManagement' })
+      const page = new Situation({ situation: 'riskManagement' }, session)
       expect(page.errors()).toEqual([])
     })
 
     it('should return an errors if the situation is not populated', () => {
-      const page = new Situation({ situation: '' })
+      const page = new Situation({ situation: '' }, session)
       expect(page.errors()).toEqual([
         {
           propertyName: 'situation',
@@ -32,8 +37,36 @@ describe('Situation', () => {
   })
 
   describe('items', () => {
+    describe('sentenceType', () => {
+      it('if the sentence type is "communityOrder" then the items should be correct', () => {
+        const items = new Situation(
+          { situation: 'riskManagement' },
+          { 'basic-information': { 'sentence-type': { sentenceType: 'communityOrder' } } },
+        ).items()
+
+        expect(items.length).toEqual(2)
+        expect(items[0]).toEqual({ text: 'Referral for risk management', value: 'riskManagement', checked: true })
+        expect(items[1]).toEqual({ text: 'Residency management', value: 'residencyManagement', checked: false })
+      })
+
+      it('if the sentence type is "bailPlacement" then the items should be correct', () => {
+        const items = new Situation(
+          { situation: 'bailAssessment' },
+          { 'basic-information': { 'sentence-type': { sentenceType: 'bailPlacement' } } },
+        ).items()
+
+        expect(items.length).toEqual(2)
+        expect(items[0]).toEqual({
+          text: 'Bail assessment for community penalty',
+          value: 'bailAssessment',
+          checked: true,
+        })
+        expect(items[1]).toEqual({ text: 'Bail sentence', value: 'bailSentence', checked: false })
+      })
+    })
+
     it('marks an option as selected when the releaseType is set', () => {
-      const page = new Situation({ situation: 'riskManagement' })
+      const page = new Situation({ situation: 'riskManagement' }, session)
 
       const selectedOptions = page.items().filter(item => item.checked)
 
@@ -42,7 +75,7 @@ describe('Situation', () => {
     })
 
     it('marks no options as selected when the releaseType is not set', () => {
-      const page = new Situation({})
+      const page = new Situation({}, session)
 
       const selectedOptions = page.items().filter(item => item.checked)
 

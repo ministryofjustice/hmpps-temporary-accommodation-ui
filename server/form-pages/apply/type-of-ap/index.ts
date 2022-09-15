1767796cd7c758312137b37d0262bc034bd492cd
pezholio <pezholio@gmail.com>
2022-09-15 16:37:58 +0100

    Add an OPD Screening page

--- server/form-pages/apply/type-of-ap/index.ts
+++ server/form-pages/apply/type-of-ap/index.ts
@@ -2,10 +2,12 @@
 
 import ApType from './apType'
 import PipeReferral from './pipeReferral'
+import PipeOpdScreening from './pipeOpdScreening'
 
 const pages = {
   'ap-type': ApType,
   'pipe-referral': PipeReferral,
+  'pipe-opd-screening': PipeOpdScreening,
 }
 
 export default pages

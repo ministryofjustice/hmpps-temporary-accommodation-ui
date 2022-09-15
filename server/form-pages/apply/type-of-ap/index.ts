d3d5b48a0f37f2bbd59829709c6ed64fd044c700
pezholio <pezholio@gmail.com>
2022-09-15 16:12:57 +0100

    Add a pipe referral page

--- server/form-pages/apply/type-of-ap/index.ts
+++ server/form-pages/apply/type-of-ap/index.ts
@@ -1,9 +1,11 @@
 /* istanbul ignore file */
 
 import ApType from './apType'
+import PipeReferral from './pipeReferral'
 
 const pages = {
   'ap-type': ApType,
+  'pipe-referral': PipeReferral,
 }
 
 export default pages
---
+++ server/form-pages/apply/type-of-ap/pipeReferral.ts
@@ -0,0 +1,56 @@
+import type { YesOrNo, ObjectWithDateParts } from 'approved-premises'
+
+import TasklistPage from '../../tasklistPage'
+import { dateAndTimeInputsAreValidDates, dateIsBlank } from '../../../utils/utils'
+
+export default class PipeReferral implements TasklistPage {
+  name = 'pipe-referral'
+
+  body: ObjectWithDateParts<'opdPathwayDate'> & { opdPathway: YesOrNo }
+
+  title = 'Has xxxx been screened into the OPD pathway?'
+
+  constructor(body: Record<string, unknown>) {
+    this.body = {
+      'opdPathwayDate-year': body['opdPathwayDate-year'] as string,
+      'opdPathwayDate-month': body['opdPathwayDate-month'] as string,
+      'opdPathwayDate-day': body['opdPathwayDate-day'] as string,
+      opdPathway: body.opdPathway as YesOrNo,
+    }
+  }
+
+  next() {
+    return 'pipe-opd-screening'
+  }
+
+  previous() {
+    return 'ap-type'
+  }
+
+  errors() {
+    const errors = []
+
+    if (!this.body.opdPathway) {
+      errors.push({
+        propertyName: 'opdPathway',
+        errorType: 'blank',
+      })
+    }
+
+    if (this.body.opdPathway === 'yes') {
+      if (dateIsBlank(this.body)) {
+        errors.push({
+          propertyName: 'opdPathwayDate',
+          errorType: 'blank',
+        })
+      } else if (!dateAndTimeInputsAreValidDates(this.body, 'opdPathwayDate')) {
+        errors.push({
+          propertyName: 'opdPathwayDate',
+          errorType: 'invalid',
+        })
+      }
+    }
+
+    return errors
+  }
+}

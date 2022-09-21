2f4d1263e7f57111aa5fe45a717bd1f80a7c79ee
pezholio <pezholio@gmail.com>
2022-09-21 10:26:33 +0100

    Update booking factories to return dates
    This matches what we get back from the API

--- server/testutils/factories/booking.ts
+++ server/testutils/factories/booking.ts
@@ -1,6 +1,6 @@
 import { Factory } from 'fishery'
 import { faker } from '@faker-js/faker/locale/en_GB'
-import { addDays, startOfToday } from 'date-fns'
+import { addDays, startOfToday, formatISO } from 'date-fns'
 
 import type { Booking } from 'approved-premises'
 import arrivalFactory from './arrival'
@@ -8,10 +8,11 @@ import departureFactory from './departure'
 import keyWorkerFactory from './keyWorker'
 import personFactory from './person'
 
-const today = startOfToday().toISOString()
-const soon = () => faker.date.soon(5, addDays(new Date(new Date().setHours(0, 0, 0, 0)), 1)).toISOString()
-const past = () => faker.date.past().toISOString()
-const future = () => faker.date.future().toISOString()
+const today = formatISO(startOfToday(), { representation: 'date' })
+const soon = () =>
+  formatISO(faker.date.soon(5, addDays(new Date(new Date().setHours(0, 0, 0, 0)), 1)), { representation: 'date' })
+const past = () => formatISO(faker.date.past(), { representation: 'date' })
+const future = () => formatISO(faker.date.future(), { representation: 'date' })
 class BookingFactory extends Factory<Booking> {
   arrivingToday() {
     return this.params({
@@ -86,8 +87,8 @@ class BookingFactory extends Factory<Booking> {
 
 export default BookingFactory.define(() => ({
   person: personFactory.build(),
-  arrivalDate: faker.date.soon().toISOString(),
-  departureDate: faker.date.future().toISOString(),
+  arrivalDate: formatISO(faker.date.soon(), { representation: 'date' }),
+  departureDate: formatISO(faker.date.future(), { representation: 'date' }),
   keyWorker: keyWorkerFactory.build(),
   name: `${faker.name.firstName()} ${faker.name.lastName()}`,
   id: faker.datatype.uuid(),

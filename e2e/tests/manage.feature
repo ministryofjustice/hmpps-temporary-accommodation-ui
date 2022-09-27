f8a6516cc17cc66a4817ca033cd5212e375f8920
pezholio <pezholio@gmail.com>
2022-09-27 13:10:59 +0100

    Add test for error messages

--- e2e/tests/manage.feature
+++ e2e/tests/manage.feature
@@ -12,5 +12,13 @@ Feature: Manage an Approved Premises
         Scenario: Creating a lost bed
                 When I access the premises homepage
                 And I choose a premises
+                And I navigate to the lost beds create page
                 And I create a lost bed
                 Then I should see a notification that the lost bed has been created
+
+        Scenario: Showing lost bed errors
+                When I access the premises homepage
+                And I choose a premises
+                And I navigate to the lost beds create page
+                And I attempt to create a lost bed without the necessary information
+                Then I should see a list of the problems encountered creating the lost bed
--- e2e/tests/stepDefinitions/lostBeds.ts
+++ e2e/tests/stepDefinitions/lostBeds.ts
@@ -4,6 +4,12 @@ import { LostBedCreatePage } from '../../../cypress_shared/pages/manage'
 import lostBedFactory from '../../../server/testutils/factories/lostBed'
 import referenceDataFactory from '../../../server/testutils/factories/referenceData'
 
+Given('I navigate to the lost beds create page', () => {
+  cy.get('.moj-button-menu__toggle-button')
+    .click()
+    .then(() => cy.get('a').contains('Record a lost bed').click())
+})
+
 Given('I create a lost bed', () => {
   const lostBed = lostBedFactory.build({
     startDate: new Date(2022, 1, 11, 0, 0).toISOString(),
@@ -11,16 +17,22 @@ Given('I create a lost bed', () => {
     reason: referenceDataFactory.build({ id: Cypress.env('lost_bed_reason_id') }),
   })
 
-  cy.get('.moj-button-menu__toggle-button')
-    .click()
-    .then(() => cy.get('a').contains('Record a lost bed').click())
-
-  const lostBedCreatePage = new LostBedCreatePage(lostBed)
+  const lostBedCreatePage = new LostBedCreatePage()
 
   lostBedCreatePage.completeForm(lostBed)
   lostBedCreatePage.clickSubmit()
 })
 
+Given('I attempt to create a lost bed without the necessary information', () => {
+  const lostBedCreatePage = new LostBedCreatePage()
+  lostBedCreatePage.clickSubmit()
+})
+
 Then('I should see a notification that the lost bed has been created', () => {
   cy.get('.govuk-notification-banner').should('contain', 'Lost bed logged')
+})
+
+Then('I should see a list of the problems encountered creating the lost bed', () => {
+  const lostBedCreatePage = new LostBedCreatePage()
+  lostBedCreatePage.shouldShowErrorMessagesForFields(['startDate', 'endDate', 'numberOfBeds', 'reason'])
 })

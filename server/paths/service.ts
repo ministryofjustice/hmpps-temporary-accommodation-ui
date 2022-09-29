2d2f95a4649aa75b31284e9401cb5d3ba7c8880e
Bryan Gale <bryan.gale@dxw.com>
2022-09-29 15:27:10 +0100

    Add stub Temporary Accommodation paths

--- server/paths/service.ts
+++ server/paths/service.ts
@@ -4,3 +4,4 @@ import config from '../config'
 const prependPath = config.serviceSignifier === 'path'
 
 export const approvedPremisesPath = path(prependPath ? `/${config.approvedPremisesRootPath}` : '/')
+export const temporaryAccommodationPath = path(prependPath ? `/${config.temporaryAccommodationRootPath}` : '/')

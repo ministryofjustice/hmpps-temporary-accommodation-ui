a9c00aae54299cc3fa55dd8c856b8c968acd73e9
Bryan Gale <bryan.gale@dxw.com>
2022-10-04 15:28:55 +0100

    Fix not using static paths in ApplicationController

--- server/controllers/applicationController.ts
+++ server/controllers/applicationController.ts
@@ -1,9 +1,10 @@
 import type { Response, Request, RequestHandler } from 'express'
+import paths from '../paths/manage'
 
 export default class ApplicationController {
   index(): RequestHandler {
     return (_req: Request, res: Response) => {
-      res.redirect('/premises')
+      res.redirect(paths.premises.index({}))
     }
   }
 }

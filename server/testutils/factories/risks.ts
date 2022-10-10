4afc199738b3d3d526ad7f6c556352bf788da27b
rich <rich.james@dxw.com>
2022-10-10 16:41:08 +0100

    Add client and factories
    These follow the patterns developed throughout the repo.
    The factory is a little complicated but hopefully easy to follow.
    I wasn’t sure of the possible combinations of MAPPA levels so hardcoded
    for now.

---
+++ server/testutils/factories/risks.ts
@@ -0,0 +1,69 @@
+import { Factory } from 'fishery'
+import { faker } from '@faker-js/faker/locale/en_GB'
+
+import type {
+  PersonRisks,
+  RiskLevel,
+  RiskEnvelopeStatus,
+  TierLetter,
+  TierNumber,
+  RiskTierLevel,
+} from 'approved-premises'
+import { DateFormats } from '../../utils/dateUtils'
+
+const riskLevels: RiskLevel[] = ['Low', 'Medium', 'High', 'Very High']
+const riskEnvelopeStatuses: RiskEnvelopeStatus[] = ['retrieved', 'not_found', 'error']
+
+export default Factory.define<PersonRisks>(() => ({
+  crn: `C${faker.datatype.number({ min: 100000, max: 999999 })}`,
+  roshRisks: roshRisksFactory.build(),
+  mappa: mappaFactory.build(),
+  flags: flagsFactory.build(),
+  tier: tierEnvelopeFactory.build(),
+}))
+
+const roshRisksFactory = Factory.define<PersonRisks['roshRisks']>(() => ({
+  status: faker.helpers.arrayElement(riskEnvelopeStatuses),
+  value: {
+    overallRisk: faker.helpers.arrayElement(riskLevels),
+    riskToChildren: faker.helpers.arrayElement(riskLevels),
+    riskToPublic: faker.helpers.arrayElement(riskLevels),
+    riskToKnownAdult: faker.helpers.arrayElement(riskLevels),
+    riskToStaff: faker.helpers.arrayElement(riskLevels),
+    lastUpdated: DateFormats.formatApiDate(faker.date.past()),
+  },
+}))
+
+const mappaFactory = Factory.define<PersonRisks['mappa']>(() => ({
+  status: faker.helpers.arrayElement(riskEnvelopeStatuses),
+  value: { level: 'CAT 2 / LEVEL 1', lastUpdated: DateFormats.formatApiDate(faker.date.past()) },
+}))
+
+const flagsFactory = Factory.define<PersonRisks['flags']>(() => {
+  return {
+    status: faker.helpers.arrayElement(riskEnvelopeStatuses),
+    value: faker.helpers.arrayElements([
+      'Registered Sex Offender',
+      'Hate Crime',
+      'Non Registered Sex Offender',
+      'Not MAPPA Eligible',
+      'Sexual Harm Prevention Order/Sexual Risk Order',
+      'Street Gangs',
+      'Suicide/Self Harm',
+      'Weapons',
+    ]),
+  }
+})
+
+const lettersFactory: () => TierLetter = () => faker.helpers.arrayElement<TierLetter>(['A', 'B', 'C', 'D'])
+const numbersFactory: () => TierNumber = () => faker.helpers.arrayElement<TierNumber>(['1', '2', '3', '4'])
+
+export const riskTierLevel: RiskTierLevel = `${lettersFactory()}${numbersFactory()}`
+
+const tierEnvelopeFactory = Factory.define<PersonRisks['tier']>(() => ({
+  status: faker.helpers.arrayElement(riskEnvelopeStatuses),
+  value: {
+    level: riskTierLevel,
+    lastUpdated: DateFormats.formatApiDate(faker.date.past()),
+  },
+}))

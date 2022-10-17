5502bcd5283bd3989d18e529725ee49a7f7446ca
rich <rich.james@dxw.com>
2022-10-17 15:00:13 +0100

    Fix arrivals
    We were generating data we didn’t need here so I have removed it

--- server/testutils/factories/arrival.ts
+++ server/testutils/factories/arrival.ts
@@ -5,11 +5,8 @@ import type { Arrival } from '@approved-premises/api'
 import { DateFormats } from '../../utils/dateUtils'
 
 export default Factory.define<Arrival>(() => ({
-  id: faker.datatype.uuid(),
   arrivalDate: DateFormats.formatApiDate(faker.date.soon()),
   bookingId: faker.datatype.uuid(),
   expectedDepartureDate: DateFormats.formatApiDate(faker.date.future()),
   notes: faker.lorem.sentence(),
-  name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-  crn: faker.datatype.uuid(),
 }))

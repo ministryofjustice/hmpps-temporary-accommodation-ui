fe88e8fe167fdc5d199d39b9e24a1ad11a1982fe
Bryan Gale <bryan.gale@dxw.com>
2022-10-18 12:58:09 +0100

    Add a new API path for our local authorities endpoint

--- server/paths/temporary-accommodation/api.ts
+++ server/paths/temporary-accommodation/api.ts
@@ -1,5 +1,15 @@
+import { path } from 'static-path'
 import paths from '../api'
+
+const localAuthoritiesPath = path('/local-authorities')
+
+const localAuthoritiesPaths = {
+  index: localAuthoritiesPath,
+}
 
 export default {
   ...paths,
+  localAuthorities: {
+    index: localAuthoritiesPaths.index,
+  },
 }

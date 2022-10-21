21401df7284ba52e12b542408f07a153eee873e1
Bryan Gale <bryan.gale@dxw.com>
2022-10-21 17:09:49 +0100

    Add escape utility function
    In some instances, we need to pass HTML into Nunjucks, and ask Nunjucks to trust that all necessary escaping has already been performed. In those instances, we will need this utility function

---
+++ server/utils/viewUtils.test.ts
@@ -0,0 +1,16 @@
+import { escape } from './viewUtils'
+
+describe('escape', () => {
+  it('escapes HTML tags', () => {
+    expect(escape('<b>Formatted text</b>')).toEqual('&lt;b&gt;Formatted text&lt;/b&gt;')
+  })
+
+  it('escapes reserved characters', () => {
+    expect(escape('"Quoted text"')).toEqual('&quot;Quoted text&quot;')
+  })
+
+  it('returns the empty string when given null', () => {
+    expect(escape(null)).toEqual('')
+  })
+})
+

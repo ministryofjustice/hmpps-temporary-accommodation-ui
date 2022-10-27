ministryofjustice/hmpps-approved-premises-ui

Add getService helper function for determining service being accessed at request time

server/utils/applicationUtils.test.ts
@@ -1,6 +1,11 @@
+import { createMock } from '@golevelup/ts-jest'
+import { Request } from 'express'
 import applicationFactory from '../testutils/factories/application'
+import config from '../config'
 import paths from '../paths/approved-premises/apply'
-import { taskLink, getTaskStatus } from './applicationUtils'
+import { taskLink, getTaskStatus, getService } from './applicationUtils'
+
+jest.mock('../config')
 
 describe('applicationUtils', () => {
   describe('getTaskStatus', () => {
@@ -30,6 +35,73 @@ describe('applicationUtils', () => {
           page: 'ap-type',
         })}" aria-describedby="eligibility-type-of-ap" data-cy-task-name="type-of-ap">Type of Approved Premises required</a>`,
       )
+    })
+  })
+
+  describe('getService', () => {
+    beforeAll(() => {
+      config.approvedPremisesRootPath = 'approved-premises-root-path'
+      config.temporaryAccommodationSubdomain = 'approved-premises-root-path'
+      config.approvedPremisesSubdomain = 'approved-premises-subdomain'
+      config.temporaryAccommodationSubdomain = 'temporary-accommodation-subdomain'
+    })
+
+    describe('when the app is configured for approved premises only', () => {
+      it("returns 'approved-premises'", () => {
+        const request = createMock<Request>()
+        config.serviceSignifier = 'approved-premises-only'
+
+        expect(getService(request)).toEqual('approved-premises')
+      })
+    })
+
+    describe('when the app is configured for temporary accommodation only', () => {
+      it("returns 'temporary-accommodation'", () => {
+        const request = createMock<Request>()
+        config.serviceSignifier = 'temporary-accommodation-only'
+
+        expect(getService(request)).toEqual('temporary-accommodation')
+      })
+    })
+
+    describe('when the app is configured to use the subdomain as the service signifier', () => {
+      it("returns 'approved-premises' when the subdomain matches the approved premises subdomain", () => {
+        const request = createMock<Request>()
+        request.subdomains = ['approved-premises-subdomain']
+
+        config.serviceSignifier = 'domain'
+
+        expect(getService(request)).toEqual('approved-premises')
+      })
+
+      it("returns 'temporary-accommodation' when the subdomain matches the temporary accommodation subdomain", () => {
+        const request = createMock<Request>()
+        request.subdomains = ['temporary-accommodation-subdomain']
+
+        config.serviceSignifier = 'domain'
+
+        expect(getService(request)).toEqual('temporary-accommodation')
+      })
+    })
+
+    describe('when the app is configured to use the path as the service signifier', () => {
+      it("returns 'approved-premises' when the path matches the approved premises root path", () => {
+        const request = createMock<Request>()
+        request.path = '/approved-premises-root-path/some-path'
+
+        config.serviceSignifier = 'path'
+
+        expect(getService(request)).toEqual('approved-premises')
+      })
+
+      it("returns 'temporary-accommodation' when the subdomain matches the temporary accommodation root path", () => {
+        const request = createMock<Request>()
+        request.path = '/temporary-accommodation-root-path/some-path'
+
+        config.serviceSignifier = 'path'
+
+        expect(getService(request)).toEqual('temporary-accommodation')
+      })
     })
   })
 })

ministryofjustice/hmpps-temporary-accommodation-ui

server/utils/applicationUtils.test.ts
@@ -1,8 +1,13 @@
 import type { Task } from '@approved-premises/ui'
 
+import { createMock } from '@golevelup/ts-jest'
+import { Request } from 'express'
 import applicationFactory from '../testutils/factories/application'
+import config from '../config'
 import paths from '../paths/apply'
-import { taskLink, getTaskStatus, getCompleteSectionCount } from './applicationUtils'
+import { taskLink, getTaskStatus, getCompleteSectionCount, getService } from './applicationUtils'
+
+jest.mock('../config')
 
 describe('applicationUtils', () => {
   const task = {
@@ -95,6 +100,73 @@ describe('applicationUtils', () => {
       })
 
       expect(getCompleteSectionCount(sections, application)).toEqual(2)
+    })
+  })
+
+  describe('getService', () => {
+    beforeAll(() => {
+      config.approvedPremisesRootPath = 'approved-premises-root-path'
+      config.temporaryAccommodationRootPath = 'temporary-accommodation-root-path'
+      config.approvedPremisesSubdomain = 'approved-premises-subdomain'
+      config.temporaryAccommodationSubdomain = 'temporary-accommodation-subdomain'
+    })
+
+    describe('when the app is configured for approved premises only', () => {
+      it("returns 'approved-premises'", () => {
+        const request = createMock<Request>()
+        config.serviceSignifier = 'approved-premises-only'
+
+        expect(getService(request)).toEqual('approved-premises')
+      })
+    })
+
+    describe('when the app is configured for temporary accommodation only', () => {
+      it("returns 'temporary-accommodation'", () => {
+        const request = createMock<Request>()
+        config.serviceSignifier = 'temporary-accommodation-only'
+
+        expect(getService(request)).toEqual('temporary-accommodation')
+      })
+    })
+
+    describe('when the app is configured to use the subdomain as the service signifier', () => {
+      it("returns 'approved-premises' when the subdomain matches the approved premises subdomain", () => {
+        const request = createMock<Request>()
+        request.subdomains = ['approved-premises-subdomain']
+
+        config.serviceSignifier = 'domain'
+
+        expect(getService(request)).toEqual('approved-premises')
+      })
+
+      it("returns 'temporary-accommodation' when the subdomain matches the temporary accommodation subdomain", () => {
+        const request = createMock<Request>()
+        request.subdomains = ['temporary-accommodation-subdomain']
+
+        config.serviceSignifier = 'domain'
+
+        expect(getService(request)).toEqual('temporary-accommodation')
+      })
+    })
+
+    describe('when the app is configured to use the path as the service signifier', () => {
+      it("returns 'approved-premises' when the path matches the approved premises root path", () => {
+        const request = createMock<Request>()
+        request.path = '/approved-premises-root-path/some-path'
+
+        config.serviceSignifier = 'path'
+
+        expect(getService(request)).toEqual('approved-premises')
+      })
+
+      it("returns 'temporary-accommodation' when the subdomain matches the temporary accommodation root path", () => {
+        const request = createMock<Request>()
+        request.path = '/temporary-accommodation-root-path/some-path'
+
+        config.serviceSignifier = 'path'
+
+        expect(getService(request)).toEqual('temporary-accommodation')
+      })
     })
   })
 })
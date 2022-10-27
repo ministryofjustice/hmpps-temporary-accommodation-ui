ministryofjustice/hmpps-approved-premises-ui

Add create method to PremisesClient
We also mock the corresponding API endpoint

server/paths/api.ts
@@ -30,6 +30,7 @@ const applicationPaths = {
 
 export default {
   premises: {
+    create: premisesPaths.premises.create,
     index: premisesPaths.premises.index,
     show: premisesPaths.premises.show,
     capacity: premisesPaths.premises.show.path('capacity'),

ministryofjustice/hmpps-temporary-accommodation-ui

server/paths/api.ts
@@ -7,6 +7,7 @@ const lostBedsPath = singlePremisesPath.path('lost-beds')
 
 const managePaths = {
   premises: {
+    create: premisesPath,
     index: premisesPath,
     show: singlePremisesPath,
   },
@@ -33,6 +34,7 @@ const applyPaths = {
 export default {
   premises: {
     show: managePaths.premises.show,
+    create: managePaths.premises.create,
     index: managePaths.premises.index,
     capacity: managePaths.premises.show.path('capacity'),
     lostBeds: {
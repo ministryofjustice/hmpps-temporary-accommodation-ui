f401bffb0affa172486bcabcffbff3a4670ab675
Bryan Gale <bryan.gale@dxw.com>
2022-10-28 13:44:10 +0100

    Define paths for our new bedspace form

--- server/paths/temporary-accommodation/manage.ts
+++ server/paths/temporary-accommodation/manage.ts
@@ -3,12 +3,18 @@ import { temporaryAccommodationPath } from '../service'
 const premisesPath = temporaryAccommodationPath.path('properties')
 const singlePremisesPath = premisesPath.path(':premisesId')
 
+const bedspacesPath = singlePremisesPath.path('bedspaces')
+
 const paths = {
   premises: {
     index: premisesPath,
     new: premisesPath.path('new'),
     create: premisesPath,
     show: singlePremisesPath,
+    bedspaces: {
+      new: bedspacesPath.path('new'),
+      create: bedspacesPath,
+    },
   },
 }
 

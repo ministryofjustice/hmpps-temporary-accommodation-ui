93099d58549f5d16375bdcfd9e930d9802b04b61
Bryan Gale <bryan.gale@dxw.com>
2022-10-28 16:12:50 +0100

    Stub characteristics reference data for integration tests

--- integration_tests/mockApis/referenceData.ts
+++ integration_tests/mockApis/referenceData.ts
@@ -1,7 +1,8 @@
 import { SuperAgentRequest } from 'superagent'
 import { stubFor } from '../../wiremock'
-import { cancellationReasons } from '../../wiremock/referenceDataStubs'
+import { cancellationReasons, characteristics } from '../../wiremock/referenceDataStubs'
 
 export default {
   stubCancellationReferenceData: (): SuperAgentRequest => stubFor(cancellationReasons),
+  stubCharacteristicsReferenceData: (): SuperAgentRequest => stubFor(characteristics),
 }

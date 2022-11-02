c6f4ce2e8da12e4df42f07e31304cd8233b96c45
Bryan Gale <bryan.gale@dxw.com>
2022-11-02 13:05:09 +0000

    Reduce configurability of paths
    This is driven by the need for paths not to depend on filesystem access, which was happening via config.ts. We need to remove this dependency, so E2E tests do not depend on filesystem access, which is unavailable
    
    Since Temporary Accommodation is now a distinct project from Approved Premises, that will be running in its own environment, we do not need this configurability

--- server/paths/service.ts
+++ server/paths/service.ts
@@ -1,7 +1,4 @@
 import { path } from 'static-path'
-import config from '../config'
 
-const prependPath = config.serviceSignifier === 'path'
-
-export const approvedPremisesPath = path(prependPath ? `/${config.approvedPremisesRootPath}` : '/')
-export const temporaryAccommodationPath = path(prependPath ? `/${config.temporaryAccommodationRootPath}` : '/')
+export const approvedPremisesPath = path('/approved-premises')
+export const temporaryAccommodationPath = path('/')

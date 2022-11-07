aa4d8bd8eb990e26471260ec9d1c86f56fd1e854
Bryan Gale <bryan.gale@dxw.com>
2022-11-07 14:30:40 +0000

    Extract shared partial _editable.njk template from our new bedspace page
    As a lot of form fields will be shared between our new bedspace and edit bedspace page, we extract this partial template for later use. Will also extract a base class from our integration test helper BedspaceNewPage to mirror this

---
+++ cypress_shared/pages/temporary-accommodation/manage/bedspaceEditable.ts
@@ -0,0 +1,15 @@
+import type { NewRoom, UpdateRoom } from '@approved-premises/api'
+import Page from '../../page'
+
+export default abstract class BedspaceEditablePage extends Page {
+  protected completeEditableForm(newOrUpdateRoom: NewRoom | UpdateRoom): void {
+    newOrUpdateRoom.characteristicIds.forEach(characteristicId => {
+      this.checkCheckboxByNameAndValue('characteristicIds[]', characteristicId)
+    })
+
+    this.getLabel('Please provide any further bedspace details')
+    this.getTextInputByIdAndEnterDetails('notes', newOrUpdateRoom.notes)
+
+    this.clickSubmit()
+  }
+}

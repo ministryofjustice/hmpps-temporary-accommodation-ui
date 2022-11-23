e467e3a3e2074bef0297b8a6740f4a661dc07bd8
Bryan Gale <bryan.gale@dxw.com>
2022-11-23 15:45:26 +0000

    Update bookingActions helper function
    We heavily revise this function to handle the actions currently supported by TemporaryAccommodation. We also remove it from our Nunjucks globals, as we will use it within the bookings controller

--- server/utils/bookingUtils.test.ts
+++ server/utils/bookingUtils.test.ts
@@ -1,72 +1,34 @@
 import { bookingActions, formatStatus } from './bookingUtils'
 import bookingFactory from '../testutils/factories/booking'
-import paths from '../paths/manage'
+import paths from '../paths/temporary-accommodation/manage'
+
+const premisesId = 'premisesId'
+const roomId = 'roomId'
 
 describe('bookingUtils', () => {
   describe('bookingActions', () => {
-    it('should return null when the booking is cancelled, departed or did not arrive', () => {
-      const cancelledBooking = bookingFactory.cancelledWithFutureArrivalDate().build()
-      const departedBooking = bookingFactory.departedToday().build()
-      const nonArrivedBooking = bookingFactory.notArrived().build()
+    it('returns a mark as confirmed action for a provisional booking', () => {
+      const booking = bookingFactory.provisional().build()
 
-      expect(bookingActions(cancelledBooking, 'premisesId')).toEqual(null)
-      expect(bookingActions(departedBooking, 'premisesId')).toEqual(null)
-      expect(bookingActions(nonArrivedBooking, 'premisesId')).toEqual(null)
-    })
-
-    it('should return arrival, non-arrival and cancellation actions if a booking is awaiting arrival', () => {
-      const booking = bookingFactory.arrivingToday().build()
-
-      expect(bookingActions(booking, 'premisesId')).toEqual([
+      expect(bookingActions(premisesId, roomId, booking)).toEqual([
         {
           items: [
             {
-              text: 'Mark as arrived',
-              classes: 'govuk-button--secondary',
-              href: paths.bookings.arrivals.new({ premisesId: 'premisesId', bookingId: booking.id }),
-            },
-            {
-              text: 'Mark as not arrived',
-              classes: 'govuk-button--secondary',
-              href: paths.bookings.nonArrivals.new({ premisesId: 'premisesId', bookingId: booking.id }),
-            },
-            {
-              text: 'Extend placement',
-              classes: 'govuk-button--secondary',
-              href: paths.bookings.extensions.new({ premisesId: 'premisesId', bookingId: booking.id }),
-            },
-            {
-              text: 'Cancel placement',
-              classes: 'govuk-button--secondary',
-              href: paths.bookings.cancellations.new({ premisesId: 'premisesId', bookingId: booking.id }),
+              text: 'Mark as confirmed',
+              classes: '',
+              href: paths.bookings.confirmations.new({ premisesId, roomId, bookingId: booking.id }),
             },
           ],
         },
       ])
     })
 
-    it('should return a departure action if a booking is arrived', () => {
-      const booking = bookingFactory.arrived().build()
+    it('returns an empty list of action items for a confirmed booking', () => {
+      const booking = bookingFactory.confirmed().build()
 
-      expect(bookingActions(booking, 'premisesId')).toEqual([
+      expect(bookingActions('premisesId', 'roomId', booking)).toEqual([
         {
-          items: [
-            {
-              text: 'Log departure',
-              classes: 'govuk-button--secondary',
-              href: paths.bookings.departures.new({ premisesId: 'premisesId', bookingId: booking.id }),
-            },
-            {
-              text: 'Extend placement',
-              classes: 'govuk-button--secondary',
-              href: paths.bookings.extensions.new({ premisesId: 'premisesId', bookingId: booking.id }),
-            },
-            {
-              text: 'Cancel placement',
-              classes: 'govuk-button--secondary',
-              href: paths.bookings.cancellations.new({ premisesId: 'premisesId', bookingId: booking.id }),
-            },
-          ],
+          items: [],
         },
       ])
     })
--- server/utils/bookingUtils.ts
+++ server/utils/bookingUtils.ts
@@ -1,60 +1,19 @@
 import type { IdentityBarMenu } from '@approved-premises/ui'
 import type { Booking } from '@approved-premises/api'
-import paths from '../paths/manage'
+import paths from '../paths/temporary-accommodation/manage'
 
-export function bookingActions(booking: Booking, premisesId: string): Array<IdentityBarMenu> {
-  if (booking.status === 'awaiting-arrival' || booking.status === 'arrived') {
-    const items = []
+export function bookingActions(premisesId: string, roomId: string, booking: Booking): Array<IdentityBarMenu> {
+  const items = []
 
-    if (booking.status === 'awaiting-arrival') {
-      items.push({
-        text: 'Mark as arrived',
-        classes: 'govuk-button--secondary',
-        href: paths.bookings.arrivals.new({ premisesId, bookingId: booking.id }),
-      })
-      items.push({
-        text: 'Mark as not arrived',
-        classes: 'govuk-button--secondary',
-        href: paths.bookings.nonArrivals.new({ premisesId, bookingId: booking.id }),
-      })
-      items.push({
-        text: 'Extend placement',
-        classes: 'govuk-button--secondary',
-        href: paths.bookings.extensions.new({ premisesId, bookingId: booking.id }),
-      })
-      items.push({
-        text: 'Cancel placement',
-        classes: 'govuk-button--secondary',
-        href: paths.bookings.cancellations.new({ premisesId, bookingId: booking.id }),
-      })
-    }
-
-    if (booking.status === 'arrived') {
-      items.push({
-        text: 'Log departure',
-        classes: 'govuk-button--secondary',
-        href: paths.bookings.departures.new({ premisesId, bookingId: booking.id }),
-      })
-      items.push({
-        text: 'Extend placement',
-        classes: 'govuk-button--secondary',
-        href: paths.bookings.extensions.new({ premisesId, bookingId: booking.id }),
-      })
-      items.push({
-        text: 'Cancel placement',
-        classes: 'govuk-button--secondary',
-        href: paths.bookings.cancellations.new({ premisesId, bookingId: booking.id }),
-      })
-    }
-
-    return [
-      {
-        items,
-      },
-    ]
+  if (booking.status === 'provisional') {
+    items.push({
+      text: 'Mark as confirmed',
+      classes: '',
+      href: paths.bookings.confirmations.new({ premisesId, roomId, bookingId: booking.id }),
+    })
   }
 
-  return null
+  return [{ items }]
 }
 
 export const allStatuses: Array<{ name: string; id: Booking['status']; tagClass: string }> = [

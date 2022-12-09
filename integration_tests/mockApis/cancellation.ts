5c3750c975bc6fe400e38841ab3094bf6048f863
Bryan Gale <bryan.gale@dxw.com>
2022-12-09 11:39:16 +0000

    Update cancellation API stubbing
    We update the the stubbing of cancellation-related API calls to follow the conventions of arrivals, departures, etc. within Temporary Accommodation

--- integration_tests/mockApis/cancellation.ts
+++ integration_tests/mockApis/cancellation.ts
@@ -3,6 +3,7 @@ import type { Cancellation } from '@approved-premises/api'
 
 import { stubFor, getMatchingRequests } from '../../wiremock'
 import { errorStub } from '../../wiremock/utils'
+import { cancellationReasons } from '../../wiremock/referenceDataStubs'
 
 export default {
   stubCancellationCreate: (args: {
@@ -35,7 +36,7 @@ export default {
         jsonBody: args.cancellation,
       },
     }),
-  stubCancellationErrors: (args: { premisesId: string; bookingId: string; params: Array<string> }) =>
+  stubCancellationCreateErrors: (args: { premisesId: string; bookingId: string; params: Array<string> }) =>
     stubFor(errorStub(args.params, `/premises/${args.premisesId}/bookings/${args.bookingId}/cancellations`, 'POST')),
   verifyCancellationCreate: async (args: { premisesId: string; bookingId: string; cancellation: Cancellation }) =>
     (
@@ -44,4 +45,5 @@ export default {
         url: `/premises/${args.premisesId}/bookings/${args.bookingId}/cancellations`,
       })
     ).body.requests,
+  stubCancellationReferenceData: () => stubFor(cancellationReasons),
 }

d9fed7e7ceab481f55567b0411517280b6db18d3
Bryan Gale <bryan.gale@dxw.com>
2022-12-09 11:32:15 +0000

    Update CancellationService
    We update CancellationService to follow the conventions of ArrivalService, DepartureService, etc.

--- server/services/cancellationService.ts
+++ server/services/cancellationService.ts
@@ -2,6 +2,10 @@ import type { ReferenceData } from '@approved-premises/ui'
 import type { Cancellation, NewCancellation } from '@approved-premises/api'
 import type { BookingClient, RestClientBuilder, ReferenceDataClient } from '../data'
 
+export type CancellationReferenceData = {
+  cancellationReasons: Array<ReferenceData>
+}
+
 export default class CancellationService {
   constructor(
     private readonly bookingClientFactory: RestClientBuilder<BookingClient>,
@@ -34,11 +38,11 @@ export default class CancellationService {
     return booking
   }
 
-  async getCancellationReasons(token: string): Promise<Array<ReferenceData>> {
+  async getReferenceData(token: string): Promise<CancellationReferenceData> {
     const referenceDataClient = this.referenceDataClientFactory(token)
 
-    const reasons = await referenceDataClient.getReferenceData('cancellation-reasons')
+    const cancellationReasons = await referenceDataClient.getReferenceData('cancellation-reasons')
 
-    return reasons as Array<ReferenceData>
+    return { cancellationReasons }
   }
 }

b3162c2ccd587798c80203886e24b73066716c23
Bryan Gale <bryan.gale@dxw.com>
2022-12-09 11:31:57 +0000

    Update NewCancellation factory
    We remove mocked date components from our NewCancellation factory, which previously represented data returned from the browser rather than part of the NewCancellation type, as we will not use these in our testing

--- server/testutils/factories/newCancellation.ts
+++ server/testutils/factories/newCancellation.ts
@@ -5,16 +5,10 @@ import type { NewCancellation } from '@approved-premises/api'
 import referenceDataFactory from './referenceData'
 import { DateFormats } from '../../utils/dateUtils'
 
-export default Factory.define<NewCancellation>(() => {
-  const date = faker.date.soon()
-  return {
-    id: faker.datatype.uuid(),
-    date: DateFormats.formatApiDate(faker.date.soon()),
-    'date-day': date.getDate().toString(),
-    'date-month': date.getMonth().toString(),
-    'date-year': date.getFullYear().toString(),
-    bookingId: faker.datatype.uuid(),
-    reason: referenceDataFactory.cancellationReasons().build().id,
-    notes: faker.lorem.sentence(),
-  }
-})
+export default Factory.define<NewCancellation>(() => ({
+  id: faker.datatype.uuid(),
+  date: DateFormats.formatApiDate(faker.date.soon()),
+  bookingId: faker.datatype.uuid(),
+  reason: referenceDataFactory.cancellationReasons().build().id,
+  notes: faker.lorem.sentence(),
+}))

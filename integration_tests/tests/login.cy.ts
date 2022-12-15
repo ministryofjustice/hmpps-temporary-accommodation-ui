82cce38897e27462a97ddbc0c150a5e53c1cec06
Bryan Gale <bryan.gale@dxw.com>
2022-12-15 17:18:27 +0000

    Update DashboardController
    We update DashboardController to present a dashboard page, rather than redirect to our premises listing. The CSS for this page presents a one, two, or three column set of cards, depending on screen size.
    
    We also add a DashboardPage helper class, to help us work with this page in our integration tests. We need here to update our login E2E tests, as the user will land on the dashboard, rather than the premises listing

--- integration_tests/tests/login.cy.ts
+++ integration_tests/tests/login.cy.ts
@@ -1,4 +1,4 @@
-import PremisesListPage from '../../cypress_shared/pages/temporary-accommodation/manage/premisesList'
+import DashboardPage from '../../cypress_shared/pages/temporary-accommodation/dashboardPage'
 import AuthSignInPage from '../../cypress_shared/pages/authSignIn'
 import Page from '../../cypress_shared/pages/page'
 import AuthManageDetailsPage from '../../cypress_shared/pages/authManageDetails'
@@ -22,20 +22,20 @@ context('SignIn', () => {
 
   it('User name visible in header', () => {
     cy.signIn()
-    const indexPage = Page.verifyOnPage(PremisesListPage)
+    const indexPage = Page.verifyOnPage(DashboardPage)
     indexPage.headerUserName().should('contain.text', 'J. Smith')
   })
 
   it('User can log out', () => {
     cy.signIn()
-    const indexPage = Page.verifyOnPage(PremisesListPage)
+    const indexPage = Page.verifyOnPage(DashboardPage)
     indexPage.signOut().click()
     Page.verifyOnPage(AuthSignInPage)
   })
 
   it('User can manage their details', () => {
     cy.signIn()
-    const indexPage = Page.verifyOnPage(PremisesListPage)
+    const indexPage = Page.verifyOnPage(DashboardPage)
 
     indexPage.manageDetails().get('a').invoke('removeAttr', 'target')
     indexPage.manageDetails().click()
@@ -44,7 +44,7 @@ context('SignIn', () => {
 
   it('Token verification failure takes user to sign in page', () => {
     cy.signIn()
-    Page.verifyOnPage(PremisesListPage)
+    Page.verifyOnPage(DashboardPage)
     cy.task('stubVerifyToken', false)
 
     // can't do a visit here as cypress requires only one domain
@@ -53,7 +53,7 @@ context('SignIn', () => {
 
   it('Token verification failure clears user session', () => {
     cy.signIn()
-    const indexPage = Page.verifyOnPage(PremisesListPage)
+    const indexPage = Page.verifyOnPage(DashboardPage)
     cy.task('stubVerifyToken', false)
 
     // can't do a visit here as cypress requires only one domain
--- server/controllers/dashboardController.ts
+++ server/controllers/dashboardController.ts
@@ -1,10 +1,9 @@
 import type { Response, Request, RequestHandler } from 'express'
-import managePaths from '../paths/temporary-accommodation/manage'
 
 export default class DashboardController {
   index(): RequestHandler {
     return (_req: Request, res: Response) => {
-      res.redirect(managePaths.premises.index({}))
+      res.render('temporary-accommodation/dashboard/index')
     }
   }
 }

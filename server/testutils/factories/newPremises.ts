f617aefbabea4fbe31a0809af49e8ead29049d49
Bryan Gale <bryan.gale@dxw.com>
2023-01-05 12:50:24 +0000

    Update factories to include new address fields

--- server/testutils/factories/newPremises.ts
+++ server/testutils/factories/newPremises.ts
@@ -7,6 +7,8 @@ import { unique } from '../../utils/utils'
 export default Factory.define<NewPremises>(() => ({
   name: `${faker.word.adjective()} ${faker.word.adverb()} ${faker.word.noun()}`,
   addressLine1: faker.address.streetAddress(),
+  addressLine2: faker.address.secondaryAddress(),
+  town: faker.address.cityName(),
   postcode: faker.address.zipCode(),
   localAuthorityAreaId: referenceDataFactory.localAuthority().build().id,
   characteristicIds: unique([referenceDataFactory.characteristic('premises').build()]).map(
--- server/testutils/factories/updatePremises.ts
+++ server/testutils/factories/updatePremises.ts
@@ -6,6 +6,8 @@ import { unique } from '../../utils/utils'
 
 export default Factory.define<UpdatePremises>(() => ({
   addressLine1: faker.address.streetAddress(),
+  addressLine2: faker.address.secondaryAddress(),
+  town: faker.address.city(),
   postcode: faker.address.zipCode(),
   localAuthorityAreaId: referenceDataFactory.localAuthority().build().id,
   characteristicIds: unique([referenceDataFactory.characteristic('premises').build()]).map(

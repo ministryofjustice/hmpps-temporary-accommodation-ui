29a63f5dca8b93a4a727af2a867636e0252f3dbc
Bryan Gale <bryan.gale@dxw.com>
2023-01-18 15:05:05 +0000

    Use auditMiddleware
    We ensure that all request handlers are wrapped using this middleware when setting up our routes. This allows us to approach auditing as a matter of configuration, rather than making changes within each of our controllers

--- server/routes/temporary-accommodation/index.ts
+++ server/routes/temporary-accommodation/index.ts
@@ -3,20 +3,21 @@
 import { Router } from 'express'
 
 import type { Controllers } from '../../controllers'
+import { Services } from '../../services'
 import actions from '../utils'
 
 import manageRoutes from './manage'
 
-export default function routes(controllers: Controllers): Router {
+export default function routes(controllers: Controllers, services: Services): Router {
   const router = Router()
 
   const { dashboardController } = controllers
 
-  const { get } = actions(router)
+  const { get } = actions(router, services.auditService)
 
   get('/', dashboardController.index())
 
-  manageRoutes(controllers, router)
+  manageRoutes(controllers, services, router)
 
   return router
 }
--- server/routes/temporary-accommodation/manage.ts
+++ server/routes/temporary-accommodation/manage.ts
@@ -4,11 +4,12 @@ import type { Router } from 'express'
 
 import type { Controllers } from '../../controllers'
 import paths from '../../paths/temporary-accommodation/manage'
+import { Services } from '../../services'
 
 import actions from '../utils'
 
-export default function routes(controllers: Controllers, router: Router): Router {
-  const { get, post, put } = actions(router)
+export default function routes(controllers: Controllers, services: Services, router: Router): Router {
+  const { get, post, put } = actions(router, services.auditService)
 
   const {
     premisesController,

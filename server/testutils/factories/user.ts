5bbf58676cf7fa39672c394bcfe521c67f7dd640
Bryan Gale <bryan.gale@dxw.com>
2023-01-20 10:29:05 +0000

    Add UserFactory

---
+++ server/testutils/factories/user.ts
@@ -0,0 +1,11 @@
+import { Factory } from 'fishery'
+import { faker } from '@faker-js/faker/locale/en_GB'
+import type { TemporaryAccommodationUser as User } from '@approved-premises/api'
+import referenceData from './referenceData'
+
+export default Factory.define<User>(() => ({
+  id: faker.datatype.uuid(),
+  roles: [],
+  region: referenceData.probationRegion().build(),
+  service: 'temporary-accommodation',
+}))

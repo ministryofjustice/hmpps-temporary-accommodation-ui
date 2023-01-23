2344260ba7df4415bfed5082fa6904c74301aed3
Bryan Gale <bryan.gale@dxw.com>
2023-01-23 11:21:58 +0000

    Users can only download reports for their region
    We modify the controller so that the only option the user can choose is their own region, and this will be pre-selected for the user. We also remove the ability for users to download a report for all regions

--- cypress_shared/pages/temporary-accommodation/manage/bookingReportNew.ts
+++ cypress_shared/pages/temporary-accommodation/manage/bookingReportNew.ts
@@ -1,5 +1,6 @@
 import type { ProbationRegion } from '@approved-premises/api'
 import paths from '../../../../server/paths/temporary-accommodation/manage'
+import { exact } from '../../../../server/utils/utils'
 import Page from '../../page'
 
 export default class BookingReportNewPage extends Page {
@@ -12,10 +13,13 @@ export default class BookingReportNewPage extends Page {
     return new BookingReportNewPage()
   }
 
-  completeForm(probationRegion: ProbationRegion): void {
-    this.getLabel('Select a probation region')
-    this.getSelectInputByIdAndSelectAnEntry('probationRegionId', probationRegion.id)
-
-    this.clickSubmit()
+  shouldPreselectProbationRegion(probationRegion: ProbationRegion): void {
+    cy.get('label')
+      .contains('Select a probation region')
+      .siblings('select')
+      .children('option')
+      .should('have.length', 2)
+      .contains(exact(probationRegion.name))
+      .should('be.selected')
   }
 }
--- e2e/tests/report.feature
+++ e2e/tests/report.feature
@@ -2,12 +2,7 @@ Feature: Manage Temporary Accommodation - Report
     Background:
         Given I am logged in
 
-    Scenario: Downloading a booking report for all probation regions
-        Given I'm downloading a booking report
-        And I select to download a report for all probation regions
-        Then I should download a booking report
-
     Scenario: Downloading a booking report for a probation region
         Given I'm downloading a booking report
-        And I select a probation region to download a report for
+        And I download a report for the preselected probation region
         Then I should download a booking report

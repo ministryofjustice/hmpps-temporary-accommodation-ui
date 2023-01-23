18d52e5550e227fde96695e843b307904c07d18f
Bryan Gale <bryan.gale@dxw.com>
2023-01-23 12:55:58 +0000

    Add filterProbationRegions helper function
    We will want to filter probation regions down to the current user's probation region in multiple places, so we add a helper function for this purpose

---
+++ server/utils/userUtils.test.ts
@@ -0,0 +1,24 @@
+import { createMock } from '@golevelup/ts-jest'
+import { Request } from 'express'
+import probationRegionFactory from '../testutils/factories/probationRegion'
+import filterProbationRegions from './userUtils'
+
+describe('filterProbationRegions', () => {
+  it('filters given probation regions by the region on the request session', () => {
+    const nonUserRegion1 = probationRegionFactory.build({
+      id: 'some-non-user-region-1',
+    })
+    const nonUserRegion2 = probationRegionFactory.build({
+      id: 'some-non-user-region-2',
+    })
+    const userRegion = probationRegionFactory.build({
+      id: 'user-region',
+    })
+
+    const request = createMock<Request>()
+
+    request.session.probationRegion = probationRegionFactory.build({ id: 'user-region' })
+
+    expect(filterProbationRegions([nonUserRegion1, userRegion, nonUserRegion2], request)).toEqual([userRegion])
+  })
+})

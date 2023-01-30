9e0a32ddc7253cf8a8270a689a2b3d2cca63fcc7
Bryan Gale <bryan.gale@dxw.com>
2023-01-30 15:35:28 +0000

    Add probation region to CallConfig
    We add probation region to CallConfig, and update our extractCallConfig helper to pull this information from the now-populated session

--- server/utils/restUtils.test.ts
+++ server/utils/restUtils.test.ts
@@ -1,13 +1,26 @@
 import { createMock } from '@golevelup/ts-jest'
 import { Request } from 'express'
+import probationRegionFactory from '../testutils/factories/probationRegion'
 import extractCallConfig from './restUtils'
 
 describe('extractCallConfig', () => {
-  it('extracts the token from the given request', () => {
+  it('extracts the token and from the given request', () => {
     const request = createMock<Request>({
       user: { token: 'some-token' },
     })
 
     expect(extractCallConfig(request)).toEqual({ token: 'some-token' })
+  })
+
+  it('extracts the token and the probation region from the given request', () => {
+    const request = createMock<Request>({
+      user: { token: 'some-token' },
+      session: { probationRegion: probationRegionFactory.build() },
+    })
+
+    expect(extractCallConfig(request)).toEqual({
+      token: 'some-token',
+      probationRegion: request.session.probationRegion,
+    })
   })
 })

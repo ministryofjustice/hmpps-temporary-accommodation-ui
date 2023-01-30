fe1ccaa71651af95708c4e8bc1724b5cbfe52bf7
Bryan Gale <bryan.gale@dxw.com>
2023-01-30 15:35:28 +0000

    Add probation region to CallConfig
    We add probation region to CallConfig, and update our extractCallConfig helper to pull this information from the now-populated session

--- server/utils/restUtils.ts
+++ server/utils/restUtils.ts
@@ -2,5 +2,5 @@ import { Request } from 'express'
 import { CallConfig } from '../data/restClient'
 
 export default function extractCallConfig(req: Request): CallConfig {
-  return { token: req.user.token }
+  return { token: req.user.token, probationRegion: req.session.probationRegion }
 }

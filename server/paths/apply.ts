b0201776ed18f552e63b7d2ce690ecce58645f51
Bryan Gale <bryan.gale@dxw.com>
2023-02-07 17:15:08 +0000

    Update Apply paths to match AP

--- server/paths/apply.ts
+++ server/paths/apply.ts
@@ -1,19 +1,27 @@
-import { approvedPremisesPath } from './service'
+import { path } from 'static-path'
 
-const applicationsPath = approvedPremisesPath.path('applications')
+const applicationsPath = path('/applications')
 const applicationPath = applicationsPath.path(':id')
 
 const pagesPath = applicationPath.path('tasks/:task/pages/:page')
+const peoplePath = applicationsPath.path('people')
+const personPath = peoplePath.path(':crn')
 
 const paths = {
   applications: {
     new: applicationsPath.path('new'),
     start: applicationsPath.path('start'),
-    people: applicationsPath.path('people').path('find'),
+    people: {
+      find: peoplePath.path('find'),
+      selectOffence: personPath.path('select-offence'),
+      documents: personPath.path('documents/:documentId'),
+    },
     create: applicationsPath,
     index: applicationsPath,
     show: applicationPath,
     update: applicationPath,
+    checkYourAnswers: applicationPath.path('check-your-answers'),
+    submission: applicationPath.path('submission'),
     pages: {
       show: pagesPath,
       update: pagesPath,

94c283f0c2e23b677fdd111a0e25970a1b23169d
Bryan Gale <bryan.gale@dxw.com>
2023-02-13 14:45:53 +0000

    Setup development-only Apply routes
    We make our Apply controllers reachable when our application is running locally or within a development environment

---
+++ server/routes/apply.ts
@@ -0,0 +1,38 @@
+/* istanbul ignore file */
+
+import type { Router } from 'express'
+import Apply from '../form-pages/apply'
+
+import type { Controllers } from '../controllers'
+import paths from '../paths/apply'
+import { Services } from '../services'
+
+import actions from './utils'
+
+export default function routes(controllers: Controllers, services: Services, router: Router): Router {
+  const { pages } = Apply
+  const { get, post, put } = actions(router, services.auditService)
+  const { applicationsController, pagesController, peopleController, offencesController, documentsController } =
+    controllers
+
+  get(paths.applications.start.pattern, applicationsController.start())
+  get(paths.applications.index.pattern, applicationsController.index())
+  get(paths.applications.new.pattern, applicationsController.new())
+  get(paths.applications.show.pattern, applicationsController.show())
+  post(paths.applications.create.pattern, applicationsController.create())
+  post(paths.applications.submission.pattern, applicationsController.submit())
+
+  post(paths.applications.people.find.pattern, peopleController.find())
+  get(paths.applications.people.selectOffence.pattern, offencesController.selectOffence())
+  get(paths.applications.people.documents.pattern, documentsController.show())
+
+  Object.keys(pages).forEach((taskKey: string) => {
+    Object.keys(pages[taskKey]).forEach((pageKey: string) => {
+      const { pattern } = paths.applications.show.path(`tasks/${taskKey}/pages/${pageKey}`)
+      get(pattern, pagesController.show(taskKey, pageKey))
+      put(pattern, pagesController.update(taskKey, pageKey))
+    })
+  })
+
+  return router
+}

25fd2e83a5735f35743a658da2d77ef87a805163
Bryan Gale <bryan.gale@dxw.com>
2023-02-15 14:44:10 +0000

    Copy Apply test helper from AP
    We copy the ApplyHelper class from AP, and its dependencies. At this point, we need to delete sections that are irrelevant to TA's minimal Apply journey so that our integration tests can pass

---
+++ cypress_shared/helpers/apply.ts
@@ -0,0 +1,164 @@
+import {
+  ActiveOffence,
+  Adjudication,
+  ApprovedPremisesApplication as Application,
+  Document,
+  OASysSection,
+  Person,
+} from '@approved-premises/api'
+import { PersonRisksUI } from '@approved-premises/ui'
+
+import { CheckYourAnswersPage, ConfirmDetailsPage, EnterCRNPage, StartPage, TaskListPage } from '../pages/apply'
+
+import Page from '../pages'
+
+import documentFactory from '../../server/testutils/factories/document'
+
+import ApplyPage from '../pages/apply/applyPage'
+import { documentsFromApplication } from '../../server/utils/assessments/documentUtils'
+import ExamplePage from '../pages/apply/example'
+
+export default class ApplyHelper {
+  pages = {
+    exampleSection: [] as Array<ApplyPage>,
+  }
+
+  uiRisks?: PersonRisksUI
+
+  oasysSectionsLinkedToReoffending: Array<OASysSection> = []
+
+  otherOasysSections: Array<OASysSection> = []
+
+  adjudications: Array<Adjudication> = []
+
+  documents: Array<Document> = []
+
+  selectedDocuments: Array<Document> = []
+
+  constructor(
+    private readonly application: Application,
+    private readonly person: Person,
+    private readonly offences: Array<ActiveOffence>,
+    private readonly type: 'e2e' | 'integration',
+  ) {}
+
+  initializeE2e(oasysSectionsLinkedToReoffending: Array<OASysSection>, otherOasysSections: Array<OASysSection>) {
+    this.oasysSectionsLinkedToReoffending = oasysSectionsLinkedToReoffending
+    this.otherOasysSections = otherOasysSections
+  }
+
+  setupApplicationStubs(uiRisks?: PersonRisksUI) {
+    this.uiRisks = uiRisks
+    this.stubPersonEndpoints()
+    this.stubApplicationEndpoints()
+    this.stubDocumentEndpoints()
+    this.stubOffences()
+  }
+
+  startApplication() {
+    // Given I visit the start page
+    const startPage = StartPage.visit()
+    startPage.startApplication()
+
+    // And I complete the first step
+    const crnPage = new EnterCRNPage()
+    crnPage.enterCrn(this.person.crn)
+    crnPage.clickSubmit()
+
+    // And I see the person on the confirmation page
+    const confirmDetailsPage = new ConfirmDetailsPage(this.person)
+    confirmDetailsPage.verifyPersonIsVisible()
+
+    // And I confirm the person is who I expect to see
+    confirmDetailsPage.clickSubmit()
+  }
+
+  completeApplication() {
+    this.completeExampleSection()
+    this.completeCheckYourAnswersSection()
+    this.submitApplication()
+  }
+
+  numberOfPages() {
+    return [...this.pages.exampleSection].length
+  }
+
+  private stubPersonEndpoints() {
+    cy.task('stubPersonRisks', { person: this.person, risks: this.application.risks })
+    cy.task('stubFindPerson', { person: this.person })
+  }
+
+  private stubOffences() {
+    cy.task('stubPersonOffences', { person: this.person, offences: this.offences })
+  }
+
+  private stubApplicationEndpoints() {
+    // Given I can create an application
+    cy.task('stubApplicationCreate', { application: this.application })
+    cy.task('stubApplicationUpdate', { application: this.application })
+    cy.task('stubApplicationGet', { application: this.application })
+  }
+
+  private stubDocumentEndpoints() {
+    // And there are documents in the database
+    this.selectedDocuments = documentsFromApplication(this.application)
+    this.documents = [this.selectedDocuments, documentFactory.buildList(4)].flat()
+
+    cy.task('stubApplicationDocuments', { application: this.application, documents: this.documents })
+    this.documents.forEach(document => {
+      cy.task('stubPersonDocument', { person: this.person, document })
+    })
+
+    // And the application exists in the database
+    cy.task('stubApplicationSubmit', { application: this.application })
+  }
+
+  completeExampleSection() {
+    const examplePage = new ExamplePage(this.application)
+    examplePage.completeForm()
+    examplePage.clickSubmit()
+
+    this.pages.exampleSection = [examplePage]
+
+    // Then I should be redirected to the task list
+    const tasklistPage = Page.verifyOnPage(TaskListPage)
+
+    // And the task should be marked as completed
+    tasklistPage.shouldShowTaskStatus('example-task', 'Completed')
+
+    // And the next task should be marked as not started
+    tasklistPage.shouldShowTaskStatus('check-your-answers', 'Not started')
+
+    // And the risk widgets should be visible
+    if (this.uiRisks) {
+      tasklistPage.shouldShowRiskWidgets(this.uiRisks)
+    }
+  }
+
+  private completeCheckYourAnswersSection() {
+    // Given I click the check your answers task
+    cy.get('[data-cy-task-name="check-your-answers"]').click()
+
+    // Then I should be on the check your answers page
+    const checkYourAnswersPage = new CheckYourAnswersPage(this.application)
+
+    // And the page should be populated with my answers
+    checkYourAnswersPage.shouldShowExampleTask(this.pages.exampleSection)
+
+    // When I have checked my answers
+    checkYourAnswersPage.clickSubmit()
+
+    // Then I should be taken back to the task list
+    const tasklistPage = Page.verifyOnPage(TaskListPage)
+
+    // And the check your answers task should show a completed status
+    tasklistPage.shouldShowTaskStatus('check-your-answers', 'Completed')
+  }
+
+  private submitApplication() {
+    const tasklistPage = Page.verifyOnPage(TaskListPage)
+    tasklistPage.checkCheckboxByLabel('submit')
+
+    tasklistPage.clickSubmit()
+  }
+}

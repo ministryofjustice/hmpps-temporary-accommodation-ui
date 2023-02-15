597549c9a118401d48cebc0ea64e57cd318f814b
Bryan Gale <bryan.gale@dxw.com>
2023-02-15 14:43:47 +0000

    Update person test stubs to match AP
    We update our person test stubs to match AP, as these will be used in our Apply integration tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -20,7 +20,7 @@ import schemaValidator from './integration_tests/tasks/schemaValidator'
 
 export default defineConfig({
   chromeWebSecurity: false,
-  fixturesFolder: 'integration_tests/fixtures',
+  fixturesFolder: 'cypress_shared/fixtures',
   screenshotsFolder: 'integration_tests/screenshots',
   videosFolder: 'integration_tests/videos',
   reporter: 'cypress-multi-reporters',
--- integration_tests/mockApis/person.ts
+++ integration_tests/mockApis/person.ts
@@ -1,6 +1,18 @@
 import { SuperAgentRequest } from 'superagent'
+import { readFileSync } from 'fs'
+import path from 'path'
+import qs from 'qs'
 
-import type { Person, PersonRisks } from '@approved-premises/api'
+import type {
+  Person,
+  PersonRisks,
+  PrisonCaseNote,
+  Adjudication,
+  ActiveOffence,
+  OASysSection,
+  OASysSections,
+  Document,
+} from '@approved-premises/api'
 
 import { stubFor, getMatchingRequests } from '../../wiremock'
 
@@ -39,6 +51,18 @@ export default {
         jsonBody: args.risks,
       },
     }),
+  stubPersonOffences: (args: { person: Person; offences: Array<ActiveOffence> }): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'GET',
+        url: `/people/${args.person.crn}/offences`,
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: args.offences,
+      },
+    }),
   verifyFindPerson: async (args: { person: Person }) =>
     (
       await getMatchingRequests({
@@ -46,4 +70,95 @@ export default {
         url: `/people/search?crn=${args.person.crn}`,
       })
     ).body.requests,
+
+  stubPrisonCaseNotes: (args: { person: Person; prisonCaseNotes: Array<PrisonCaseNote> }) =>
+    stubFor({
+      request: {
+        method: 'GET',
+        url: `/people/${args.person.crn}/prison-case-notes`,
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: args.prisonCaseNotes,
+      },
+    }),
+
+  stubAdjudications: (args: { person: Person; adjudications: Array<Adjudication> }) =>
+    stubFor({
+      request: {
+        method: 'GET',
+        url: `/people/${args.person.crn}/adjudications`,
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: args.adjudications,
+      },
+    }),
+
+  stubOasysSelection: (args: { person: Person; oasysSelection: Array<OASysSection> }) =>
+    stubFor({
+      request: {
+        method: 'GET',
+        url: `/people/${args.person.crn}/oasys/selection`,
+      },
+
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: args.oasysSelection,
+      },
+    }),
+
+  stubOasysSections: (args: { person: Person; oasysSections: OASysSections }) =>
+    stubFor({
+      request: {
+        method: 'GET',
+        url: `/people/${args.person.crn}/oasys/sections`,
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: args.oasysSections,
+      },
+    }),
+
+  stubOasysSectionsWithSelectedSections: (args: {
+    person: Person
+    oasysSections: OASysSections
+    selectedSections: Array<number>
+  }) =>
+    stubFor({
+      request: {
+        method: 'GET',
+        url: `/people/${args.person.crn}/oasys/sections?${qs.stringify(
+          { 'selected-sections': args.selectedSections },
+          { encode: false, indices: false },
+        )}`,
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: args.oasysSections,
+      },
+    }),
+
+  stubPersonDocument: (args: { person: Person; document: Document }) =>
+    stubFor({
+      request: {
+        method: 'GET',
+        url: `/documents/${args.person.crn}/${args.document.id}`,
+      },
+      response: {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/octet-stream',
+          'content-disposition': `attachment; filename=${args.document.fileName}`,
+        },
+        base64Body: readFileSync(path.resolve(__dirname, '..', '..', 'cypress_shared', 'fixtures', 'document.pdf'), {
+          encoding: 'base64',
+        }),
+      },
+    }),
 }

e7ab2f22525fed530080c5961bcf3ca0d1a612e4
Bryan Gale <bryan.gale@dxw.com>
2023-02-16 16:01:34 +0000

    Update application stubs to use static paths

--- wiremock/applicationStubs.ts
+++ wiremock/applicationStubs.ts
@@ -1,12 +1,13 @@
 import { guidRegex } from './index'
 
 import applicationFactory from '../server/testutils/factories/application'
+import paths from '../server/paths/api'
 
 export default [
   {
     request: {
       method: 'GET',
-      url: `/applications`,
+      url: paths.applications.index({}),
     },
     response: {
       status: 200,
@@ -17,7 +18,7 @@ export default [
   {
     request: {
       method: 'GET',
-      urlPathPattern: `/applications/${guidRegex}`,
+      urlPathPattern: paths.applications.show({ id: guidRegex }),
     },
     response: {
       status: 200,
@@ -28,7 +29,7 @@ export default [
   {
     request: {
       method: 'POST',
-      url: `/applications`,
+      url: paths.applications.index({}),
     },
     response: {
       status: 200,

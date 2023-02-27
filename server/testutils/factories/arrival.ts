7c447e1b6447ea268003013f210e47e2759c68b8
Bryan Gale <bryan.gale@dxw.com>
2023-02-27 17:05:27 +0000

    Ensure bookings departures are never before arrivals
    We update our factories so that departures cannot be before arrivals. This fixes some random failures in our E2E tests, where the API validates that departures are not before arrivals

--- server/testutils/factories/arrival.ts
+++ server/testutils/factories/arrival.ts
@@ -1,13 +1,17 @@
-import { Factory } from 'fishery'
 import { faker } from '@faker-js/faker/locale/en_GB'
+import { Factory } from 'fishery'
 
 import type { Arrival } from '@approved-premises/api'
 import { DateFormats } from '../../utils/dateUtils'
 
-export default Factory.define<Arrival>(() => ({
-  arrivalDate: DateFormats.dateObjToIsoDate(faker.date.soon()),
-  bookingId: faker.datatype.uuid(),
-  expectedDepartureDate: DateFormats.dateObjToIsoDate(faker.date.future()),
-  notes: faker.lorem.sentence(),
-  createdAt: DateFormats.dateObjToIsoDate(faker.date.past()),
-}))
+export default Factory.define<Arrival>(() => {
+  const arrivalDate = faker.date.soon()
+
+  return {
+    arrivalDate: DateFormats.dateObjToIsoDate(arrivalDate),
+    bookingId: faker.datatype.uuid(),
+    expectedDepartureDate: DateFormats.dateObjToIsoDate(faker.date.future(1, arrivalDate)),
+    notes: faker.lorem.sentence(),
+    createdAt: DateFormats.dateObjToIsoDate(faker.date.past()),
+  }
+})

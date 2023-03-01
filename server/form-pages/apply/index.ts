61303301448d1ba50e088764fdd852bd092d56d5
Bryan Gale <bryan.gale@dxw.com>
2023-03-01 12:29:23 +0000

    Rename misnamed Apply page folder
    We move 'example-page' to 'example-section'

--- server/form-pages/apply/index.ts
+++ server/form-pages/apply/index.ts
@@ -2,7 +2,7 @@
 import BaseForm from '../baseForm'
 import { Form } from '../utils/decorators'
 import CheckYourAnswers from './check-your-answers'
-import ExampleSection from './example-page'
+import ExampleSection from './example-section'
 
 @Form({ sections: [ExampleSection, CheckYourAnswers] })
 export default class Apply extends BaseForm {}

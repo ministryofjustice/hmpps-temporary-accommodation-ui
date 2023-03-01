cc2cfd4404cbd8e8b03c1a966da5c620cfa81a85
Bryan Gale <bryan.gale@dxw.com>
2023-03-01 14:27:23 +0000

    Add OASys import Apply task
    We add a section and task to contain our optional OASys sections page. We follow naming from Approved Premises

---
+++ server/form-pages/apply/risk-and-need-factors/oasys-import/index.ts
@@ -0,0 +1,8 @@
+import { Task } from '../../../utils/decorators'
+
+@Task({
+  slug: 'oasys-import',
+  name: 'Choose sections of OASys to import',
+  pages: [],
+})
+export default class OasysImport {}

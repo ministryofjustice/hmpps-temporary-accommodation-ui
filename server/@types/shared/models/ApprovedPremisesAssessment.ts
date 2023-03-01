cade09dcb84cc4720d28e8dc1d922a77857606dc
pezholio <pezholio@users.noreply.github.com>
2023-03-01 13:48:21 +0000

    Updating hmpps-approved-premises-api models from OpenAPI specification

--- server/@types/shared/models/ApprovedPremisesAssessment.ts
+++ server/@types/shared/models/ApprovedPremisesAssessment.ts
@@ -3,9 +3,11 @@
 /* eslint-disable */
 
 import type { ApprovedPremisesApplication } from './ApprovedPremisesApplication';
+import type { ApprovedPremisesUser } from './ApprovedPremisesUser';
 import type { Assessment } from './Assessment';
 
 export type ApprovedPremisesAssessment = (Assessment & {
     application: ApprovedPremisesApplication;
+    allocatedToStaffMember?: ApprovedPremisesUser;
 });
 

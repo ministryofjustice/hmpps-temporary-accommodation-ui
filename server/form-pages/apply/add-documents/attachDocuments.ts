2f6a0e9d57986e0b6b26838fa55c2ee1e353f5b6
Bryan Gale <bryan.gale@dxw.com>
2023-03-02 11:06:47 +0000

    Add AttachDocuments Apply form page
    We directly copy the AttachDocuments page from Approved Premises

---
+++ server/form-pages/apply/add-documents/attachDocuments.ts
@@ -0,0 +1,84 @@
+import type { ApprovedPremisesApplication, Document } from '@approved-premises/api'
+import { DataServices, TaskListErrors } from '@approved-premises/ui'
+import { Page } from '../../utils/decorators'
+
+import { CallConfig } from '../../../data/restClient'
+import TasklistPage from '../../tasklistPage'
+
+type AttachDocumentsBody = {
+  selectedDocuments?: Array<Document>
+}
+
+type AttachDocumentsResponse = {
+  selectedDocuments?: Array<Document>
+  documentIds?: Array<string> | string
+  documentDescriptions?: Record<string, string>
+}
+
+@Page({ name: 'attach-documents', bodyProperties: ['selectedDocuments'] })
+export default class AttachDocuments implements TasklistPage {
+  title = 'Select any additional documents that are required to support your application'
+
+  documents: Array<Document> | undefined
+
+  constructor(public body: AttachDocumentsBody, public readonly application: ApprovedPremisesApplication) {}
+
+  static async initialize(
+    body: AttachDocumentsResponse,
+    application: ApprovedPremisesApplication,
+    callConfig: CallConfig,
+    dataServices: DataServices,
+  ): Promise<AttachDocuments> {
+    const documents = await dataServices.applicationService.getDocuments(callConfig, application)
+    const documentIds = [body.documentIds].flat()
+    const documentDescriptions = body.documentDescriptions || {}
+
+    const selectedDocuments = body.selectedDocuments
+      ? body.selectedDocuments
+      : documents
+          .filter((d: Document) => documentIds.includes(d.id))
+          .map(document => {
+            return {
+              ...document,
+              description: documentDescriptions[document.id],
+            }
+          })
+
+    const page = new AttachDocuments({ selectedDocuments }, application)
+    page.documents = documents
+
+    return page
+  }
+
+  previous() {
+    return ''
+  }
+
+  next() {
+    return ''
+  }
+
+  response() {
+    const response = {}
+
+    this.body.selectedDocuments.forEach(d => {
+      response[d.fileName] = d.description || 'No description'
+    })
+
+    return response
+  }
+
+  errors() {
+    const errors: TaskListErrors<this> = {}
+
+    this.body.selectedDocuments.forEach(document => {
+      if (!document.description) {
+        errors[
+          `selectedDocuments_${document.id}`
+        ] = `You must enter a description for the document '${document.fileName}'`
+      }
+    })
+
+    return errors
+  }
+}

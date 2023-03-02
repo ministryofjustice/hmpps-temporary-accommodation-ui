abcf59124e0245a203ae25620990107db04ddf24
Bryan Gale <bryan.gale@dxw.com>
2023-03-02 10:01:57 +0000

    Sort imports

--- server/testutils/factories/newLostBed.ts
+++ server/testutils/factories/newLostBed.ts
@@ -1,9 +1,9 @@
-import { Factory } from 'fishery'
 import { faker } from '@faker-js/faker/locale/en_GB'
+import { Factory } from 'fishery'
 
 import type { NewLostBed } from '@approved-premises/api'
-import referenceDataFactory from './referenceData'
 import { DateFormats } from '../../utils/dateUtils'
+import referenceDataFactory from './referenceData'
 
 export default Factory.define<NewLostBed>(() => {
   const startDate = faker.date.soon()

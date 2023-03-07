eaf15e9b3082fed2058d20b847bafa74576e0322
Bryan Gale <bryan.gale@dxw.com>
2023-03-07 12:28:12 +0000

    Remove unused getDateRangeWithNegativeBeds function
    We remove this function inherited from Approved Premises that we are unlikely to need

--- server/utils/premisesUtils.ts
+++ server/utils/premisesUtils.ts
@@ -1,27 +1,4 @@
-import type { DateCapacity, PropertyStatus } from '@approved-premises/api'
-
-export type NegativeDateRange = { start?: string; end?: string }
-
-export function getDateRangesWithNegativeBeds(premisesCapacity: DateCapacity[]): NegativeDateRange[] {
-  let dateRange: NegativeDateRange = {}
-  const result: NegativeDateRange[] = []
-
-  premisesCapacity.forEach((premisesCapacityItem, i, arr) => {
-    if (premisesCapacityItem.availableBeds < 0 && !dateRange?.start) {
-      dateRange.start = premisesCapacityItem.date
-    } else if (premisesCapacityItem.availableBeds < 0 && dateRange.start) {
-      dateRange.end = premisesCapacityItem.date
-    } else if (premisesCapacityItem.availableBeds >= 0 && dateRange.start) {
-      result.push(dateRange)
-      dateRange = {}
-    }
-    if (arr.length === i + 1 && dateRange.start) {
-      result.push(dateRange)
-    }
-  })
-
-  return result
-}
+import type { PropertyStatus } from '@approved-premises/api'
 
 export const allStatuses: Array<{ name: string; id: PropertyStatus; isActive: boolean }> = [
   {

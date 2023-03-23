ea09809455fc2e4af65232fee2ad4e4a1000163e
Bryan Gale <bryan.gale@dxw.com>
2023-03-23 12:32:22 +0000

    Add bedspace search related factories
    We add factories for the new shared types added as part of the bedspace search feature. Some methods will not be touched during unit testing, but only by integration tests and wiremock setup.We mark these with "istanbul ignore" so they our not included in our test coverage checking

---
+++ server/testutils/factories/bedSearchParameters.ts
@@ -0,0 +1,12 @@
+import { faker } from '@faker-js/faker/locale/en_GB'
+import { Factory } from 'fishery'
+
+import { TemporaryAccommodationBedSearchParameters as BedSearchParameters } from '@approved-premises/api'
+import { DateFormats } from '../../utils/dateUtils'
+import referenceData from './referenceData'
+
+export default Factory.define<BedSearchParameters>(() => ({
+  startDate: DateFormats.dateObjToIsoDate(faker.date.soon()),
+  durationDays: faker.datatype.number({ min: 1, max: 10 }),
+  probationDeliveryUnit: referenceData.pdu().build().id,
+}))

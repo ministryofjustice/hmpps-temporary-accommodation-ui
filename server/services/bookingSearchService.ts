177e7e38cf33bde05af163fba458bfa469d94228
Patrick Fleming <patrick.fleming@dwx.com>
2023-03-28 11:02:24 +0100

    Add BookingSearchService
    We add a new service to handle Booking search, and a method that formats booking summary data into a table for the controller.

---
+++ server/services/bookingSearchService.ts
@@ -0,0 +1,40 @@
+import type { TableRow } from '@approved-premises/ui'
+import type { RestClientBuilder } from '../data'
+import BookingClient from '../data/bookingClient'
+import { CallConfig } from '../data/restClient'
+import { DateFormats } from '../utils/dateUtils'
+import paths from '../paths/temporary-accommodation/manage'
+
+export default class BookingSearchService {
+  constructor(private readonly bookingClientFactory: RestClientBuilder<BookingClient>) {}
+
+  async getTableRowsForFindBooking(callConfig: CallConfig): Promise<Array<TableRow>> {
+    const bookingClient = this.bookingClientFactory(callConfig)
+    const bookingSummaries = await bookingClient.index()
+
+    return bookingSummaries.results.map(summary => {
+      return [
+        this.textValue(summary.person.name),
+        this.textValue(summary.person.crn),
+        this.textValue(summary.premises.addressLine1),
+        this.textValue(DateFormats.isoDateToUIDate(summary.booking.startDate, { format: 'short' })),
+        this.textValue(DateFormats.isoDateToUIDate(summary.booking.endDate, { format: 'short' })),
+        this.htmlValue(
+          `<a href="${paths.bookings.show({
+            premisesId: summary.premises.id,
+            roomId: summary.room.id,
+            bookingId: summary.booking.id,
+          })}">View<span class="govuk-visually-hidden"> booking for person with CRN ${summary.person.crn}</span></a>`,
+        ),
+      ]
+    })
+  }
+
+  private textValue(value: string) {
+    return { text: value }
+  }
+
+  private htmlValue(value: string) {
+    return { html: value }
+  }
+}

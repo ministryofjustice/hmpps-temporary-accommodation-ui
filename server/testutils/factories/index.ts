44f04fa7b120312230f63083808798afbc35ff72
Bryan Gale <bryan.gale@dxw.com>
2023-03-30 12:09:09 +0100

    Add a factories index file
    This will allow us to do named imports from "/testutils/factories" which will help keep naming consistent and help with autocomplete

---
+++ server/testutils/factories/index.ts
@@ -0,0 +1,97 @@
+import activeOffenceFactory from './activeOffence'
+import adjudicationFactory from './adjudication'
+import applicationFactory from './application'
+import arrivalFactory from './arrival'
+import assessmentFactory from './assessment'
+import bedFactory from './bed'
+import bedSearchParametersFactory from './bedSearchParameters'
+import bedSearchResultFactory from './bedSearchResult'
+import bedSearchResultsFactory from './bedSearchResults'
+import bookingFactory from './booking'
+import cancellationFactory from './cancellation'
+import characteristicFactory from './characteristic'
+import confirmationFactory from './confirmation'
+import dateCapacityFactory from './dateCapacity'
+import departureFactory from './departure'
+import documentFactory from './document'
+import extensionFactory from './extension'
+import localAuthorityFactory from './localAuthority'
+import lostBedFactory from './lostBed'
+import lostBedCancellationFactory from './lostBedCancellation'
+import newArrivalFactory from './newArrival'
+import newBookingFactory from './newBooking'
+import newCancellationFactory from './newCancellation'
+import newConfirmationFactory from './newConfirmation'
+import newDepartureFactory from './newDeparture'
+import newExtensionFactory from './newExtension'
+import newLostBedFactory from './newLostBed'
+import newLostBedCancellationFactory from './newLostBedCancellation'
+import newPremisesFactory from './newPremises'
+import newRoomFactory from './newRoom'
+import nonArrivalFactory from './nonArrival'
+import oasysSectionsFactory, { roshSummaryFactory } from './oasysSections'
+import oasysSelectionFactory from './oasysSelection'
+import pduFactory from './pdu'
+import personFactory from './person'
+import premisesFactory from './premises'
+import prisonCaseNotesFactory from './prisonCaseNotes'
+import probationRegionFactory from './probationRegion'
+import referenceDataFactory from './referenceData'
+import risksFactory, { tierEnvelopeFactory } from './risks'
+import roomFactory from './room'
+import staffMemberFactory from './staffMember'
+import updateLostBedFactory from './updateLostBed'
+import updatePremisesFactory from './updatePremises'
+import updateRoomFactory from './updateRoom'
+import userFactory from './user'
+
+export {
+  activeOffenceFactory,
+  adjudicationFactory,
+  applicationFactory,
+  arrivalFactory,
+  assessmentFactory,
+  bedFactory,
+  bedSearchParametersFactory,
+  bedSearchResultFactory,
+  bedSearchResultsFactory,
+  bookingFactory,
+  cancellationFactory,
+  characteristicFactory,
+  confirmationFactory,
+  dateCapacityFactory,
+  departureFactory,
+  documentFactory,
+  extensionFactory,
+  localAuthorityFactory,
+  lostBedFactory,
+  lostBedCancellationFactory,
+  newArrivalFactory,
+  newBookingFactory,
+  newCancellationFactory,
+  newConfirmationFactory,
+  newDepartureFactory,
+  newExtensionFactory,
+  newLostBedFactory,
+  newLostBedCancellationFactory,
+  newPremisesFactory,
+  newRoomFactory,
+  nonArrivalFactory,
+  oasysSectionsFactory,
+  oasysSelectionFactory,
+  pduFactory,
+  personFactory,
+  premisesFactory,
+  prisonCaseNotesFactory,
+  probationRegionFactory,
+  referenceDataFactory,
+  risksFactory,
+  roomFactory,
+  roshSummaryFactory,
+  staffMemberFactory,
+  tierEnvelopeFactory,
+  updateLostBedFactory,
+  updatePremisesFactory,
+  updateRoomFactory,
+  userFactory,
+}

f06c6dedefa3814e3690e9df852d4bf488a2b92f
Patrick Fleming <patrick.fleming@dwx.com>
2023-04-12 09:51:44 +0100

    Update getSections test

--- server/utils/assessments/getSections.test.ts
+++ server/utils/assessments/getSections.test.ts
@@ -2,7 +2,7 @@ import { assessmentFactory } from '../../testutils/factories'
 import getSections from './getSections'
 
 describe('getSections', () => {
-  const assessment = assessmentFactory.build({ status: 'pending' })
+  const assessment = assessmentFactory.build({ status: 'awaiting_response' })
 
   it('returns an empty array', () => {
     const sections = getSections(assessment)

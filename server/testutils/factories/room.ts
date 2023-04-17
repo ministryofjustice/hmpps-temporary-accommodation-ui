0c2c806688f3b16feb4c07d4845ac58f304b2149
Bryan Gale <bryan.gale@dxw.com>
2023-04-17 14:48:18 +0100

    Update room related factories
    We add helper methods in our room related factories to reduce duplication in our E2E tests

--- server/testutils/factories/room.ts
+++ server/testutils/factories/room.ts
@@ -2,11 +2,29 @@ import { faker } from '@faker-js/faker/locale/en_GB'
 import { Factory } from 'fishery'
 
 import { Room } from '@approved-premises/api'
+import { ReferenceData } from '../../@types/ui'
 import { unique } from '../../utils/utils'
 import bedFactory from './bed'
+import characteristicFactory from './characteristic'
 import referenceDataFactory from './referenceData'
 
-export default Factory.define<Room>(() => ({
+class RoomFactory extends Factory<Room> {
+  /* istanbul ignore next */
+  forEnvironment(characteristics: ReferenceData[]) {
+    return this.params({
+      characteristics: faker.helpers
+        .arrayElements(characteristics, faker.datatype.number({ min: 1, max: 5 }))
+        .map(characteristic =>
+          characteristicFactory.build({
+            ...characteristic,
+            modelScope: 'room',
+          }),
+        ),
+    })
+  }
+}
+
+export default RoomFactory.define(() => ({
   id: faker.datatype.uuid(),
   name: `${faker.word.adjective()} ${faker.word.adverb()} ${faker.word.noun()}`,
   characteristics: unique(

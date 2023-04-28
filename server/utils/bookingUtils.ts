8cf6d9c2a7a88db3d48fd107377bfc3bf6f354f3
Bryan Gale <bryan.gale@dxw.com>
2023-04-28 10:34:57 +0100

    Update deriveBookingHistory
    We update deriveBookingHistory to account for bookings with the closed status. Rather than attempt to show the full history of turnaround changes, we simply display the history up until the departed state

--- server/utils/bookingUtils.ts
+++ server/utils/bookingUtils.ts
@@ -139,8 +139,9 @@ export const deriveBookingHistory = (booking: Booking) => {
   const departures = [...booking.departures].sort(compareBookingState)
   const cancellations = [...booking.cancellations].sort(compareBookingState)
 
-  const bookingWithSortedExensions = {
+  const bookingWithSortedExensions: Booking = {
     ...booking,
+    status: booking.status === 'closed' ? 'departed' : booking.status,
     extensions,
     departures,
     cancellations,

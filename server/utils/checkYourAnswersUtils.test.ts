87995de3ad87f287291e88fee37ad3727a645594
Bryan Gale <bryan.gale@dxw.com>
2023-05-31 10:32:47 +0100

    Fix possible HTML injection in checkYourAnswerUtils
    When constructing a summary list for use on the Apply check your answers screen, we now escape key and value strings rather than assuming they contain no HTML special characters

--- server/utils/checkYourAnswersUtils.test.ts
+++ server/utils/checkYourAnswersUtils.test.ts
@@ -7,35 +7,42 @@ import {
   getTaskResponsesAsSummaryListItems,
 } from './checkYourAnswersUtils'
 import reviewSections from './reviewUtils'
+import { escape } from './viewUtils'
 
 jest.mock('./reviewUtils')
 jest.mock('./applicationUtils')
+jest.mock('./viewUtils')
 
 describe('checkYourAnswersUtils', () => {
   describe('embeddedSummaryListItem', () => {
     it('returns a summary list for an array of records', () => {
+      ;(escape as jest.Mock).mockImplementation((value: string) => `Escaped "${value}"`)
+
       const result = embeddedSummaryListItem([
         { foo: 'bar', bar: 'baz' },
         { foo: 'bar', bar: 'baz' },
       ]).replace(/\s+/g, ``)
 
+      expect(escape).toHaveBeenCalledWith('foo')
+      expect(escape).toHaveBeenCalledWith('bar')
+      expect(escape).toHaveBeenCalledWith('baz')
       expect(result).toEqual(
         `
       <dl class="govuk-summary-list govuk-summary-list--embedded">
         <div class="govuk-summary-list__row govuk-summary-list__row--embedded">
           <dt class="govuk-summary-list__key govuk-summary-list__key--embedded">
-            foo
+            Escaped "foo"
           </dt>
           <dd class="govuk-summary-list__value govuk-summary-list__value--embedded">
-            bar
+            Escaped "bar"
           </dd>
         </div>
         <div class="govuk-summary-list__row govuk-summary-list__row--embedded">
           <dt class="govuk-summary-list__key govuk-summary-list__key--embedded">
-            bar
+            Escaped "bar"
           </dt>
           <dd class="govuk-summary-list__value govuk-summary-list__value--embedded">
-            baz
+            Escaped "baz"
           </dd>
         </div>
       </dl>
@@ -43,18 +50,18 @@ describe('checkYourAnswersUtils', () => {
       <dl class="govuk-summary-list govuk-summary-list--embedded">
         <div class="govuk-summary-list__row govuk-summary-list__row--embedded">
           <dt class="govuk-summary-list__key govuk-summary-list__key--embedded">
-            foo
+            Escaped "foo"
           </dt>
           <dd class="govuk-summary-list__value govuk-summary-list__value--embedded">
-            bar
+            Escaped "bar"
           </dd>
         </div>
         <div class="govuk-summary-list__row govuk-summary-list__row--embedded">
           <dt class="govuk-summary-list__key govuk-summary-list__key--embedded">
-            bar
+            Escaped "bar"
           </dt>
           <dd class="govuk-summary-list__value govuk-summary-list__value--embedded">
-            baz
+            Escaped "baz"
           </dd>
         </div>
       </dl>`.replace(/\s+/g, ``),

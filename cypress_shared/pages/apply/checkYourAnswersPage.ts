54fe48e9e52cc55eb202a672bada0517b842d1a2
Bryan Gale <bryan.gale@dxw.com>
2023-05-31 16:14:49 +0100

    Update CheckYourAnswersPage test helper
    An Apply page can show its results in the check your answers screen in two forms - a simple set of key/value pairs shown as a GDS summary list. Or, if the value is itself an array of sets of key/value pairs, a GDS summary list containing one or more embedded GDS summary lists. This commit updates shouldShowAnswersForTask() to handle the latter where before it only handled the former

--- cypress_shared/pages/apply/checkYourAnswersPage.ts
+++ cypress_shared/pages/apply/checkYourAnswersPage.ts
@@ -34,7 +34,26 @@ export default class CheckYourAnswersPage extends ApplyPage {
       pages.forEach(page => {
         const responses = page.tasklistPage.response()
         Object.keys(responses).forEach(key => {
-          this.assertDefinition(key, responses[key] as string)
+          const value = responses[key] as string | Array<Record<string, unknown>>
+
+          if (typeof value === 'string') {
+            this.assertDefinition(key, responses[key] as string)
+          } else {
+            cy.get('dt')
+              .contains(key)
+              .parent()
+              .within(() => {
+                value.forEach((embeddedRecord, index) => {
+                  cy.get('dl.govuk-summary-list--embedded')
+                    .eq(index)
+                    .within(() => {
+                      Object.keys(embeddedRecord).forEach(embeddedKey => {
+                        this.assertDefinition(embeddedKey, embeddedRecord[embeddedKey] as string)
+                      })
+                    })
+                })
+              })
+          }
         })
       })
     })

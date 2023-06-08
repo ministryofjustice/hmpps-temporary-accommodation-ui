cb00eafa2d79854cbe29d6ae6b237271226eed50
Bryan Gale <bryan.gale@dxw.com>
2023-06-08 11:27:50 +0100

    Improve testing on classes that extend ContactDetails
    We explicitly test that BackupContact and ProbationPractitioner extend ContactDetails so our tests will fail if this ever changes and we'd want more testing on these classes

--- server/form-pages/apply/accommodation-need/contact-details/probationPractitioner.test.ts
+++ server/form-pages/apply/accommodation-need/contact-details/probationPractitioner.test.ts
@@ -1,9 +1,14 @@
 import { applicationFactory } from '../../../../testutils/factories'
 import { itShouldHaveNextValue, itShouldHavePreviousValue } from '../../../shared-examples'
+import ContactDetails from '../../contactDetails'
 import ProbationPractitioner from './probationPractitioner'
 
 describe('ProbationPractitioner', () => {
   const application = applicationFactory.build()
+
+  it('extends ContactDetails', () => {
+    expect(new ProbationPractitioner({}, application)).toBeInstanceOf(ContactDetails)
+  })
 
   itShouldHavePreviousValue(new ProbationPractitioner({}, application), 'dashboard')
   itShouldHaveNextValue(new ProbationPractitioner({}, application), 'backup-contact')

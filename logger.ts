4f3e85b84bc0e930a07d411589d97ea2383aee4e
Tom Hipkin <tomhipkin@gmail.com>
2023-06-16 14:55:30 +0100

    Use the TA name in logs

--- logger.ts
+++ logger.ts
@@ -5,7 +5,7 @@ const formatOut = bunyanFormat({ outputMode: 'short', color: true })
 
 const logger =
   process.env.NODE_ENV !== 'test'
-    ? bunyan.createLogger({ name: 'Approved Premises Ui', stream: formatOut, level: 'debug' })
-    : bunyan.createLogger({ name: 'Approved Premises Ui', stream: formatOut, level: 'fatal' })
+    ? bunyan.createLogger({ name: 'Temporary Accommodation UI', stream: formatOut, level: 'debug' })
+    : bunyan.createLogger({ name: 'Temporary Accommodation UI', stream: formatOut, level: 'fatal' })
 
 export default logger

744501412589538360461cac919ccbaced281e08
Bryan Gale <bryan.gale@dxw.com>
2023-06-27 16:03:33 +0100

    Use person details component on confirm screen
    We update our Apply confirm screen to use our new person details component

--- cypress_shared/helpers/apply.ts
+++ cypress_shared/helpers/apply.ts
@@ -139,7 +139,7 @@ export default class ApplyHelper {
 
     // And I see the person on the confirmation page
     const confirmDetailsPage = new ConfirmDetailsPage(this.person)
-    confirmDetailsPage.verifyPersonIsVisible()
+    confirmDetailsPage.shouldPersonDetails()
 
     // And I confirm the person is who I expect to see
     confirmDetailsPage.clickSubmit()

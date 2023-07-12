2a816dabd33e435e2447750d9a176cf3438484f7
Bryan Gale <bryan.gale@dxw.com>
2023-07-12 10:49:03 +0100

    Remove populateUserRegion()
    Enabled by our changes to UserService.getActingUser(), we remove populateUserRegion(), and move its responsibilities into populateCurrentUser(). This saves two largely identical tasks being spread across different pieces of middleware. We take this as an opportunity to add missing testing to populateCurrentUser().

--- server/middleware/populateCurrentUser.ts
+++ server/middleware/populateCurrentUser.ts
@@ -9,7 +9,10 @@ export default function populateCurrentUser(userService: UserService): RequestHa
       if (req.user) {
         const callConfig = extractCallConfig(req)
         const userDetails = req.session.userDetails || (await userService.getActingUser(callConfig))
+
         req.session.userDetails = userDetails
+        req.session.probationRegion = userDetails.region
+
         res.locals.user = { ...userDetails, ...res.locals.user }
 
         if (!userDetails) {
--- server/middleware/setUpCurrentUser.ts
+++ server/middleware/setUpCurrentUser.ts
@@ -2,14 +2,11 @@ import { Router } from 'express'
 import auth from '../authentication/auth'
 import tokenVerifier from '../data/tokenVerification'
 import type { Services } from '../services'
-import populateUserRegion from './populateUserRegion'
-import asyncMiddleware from './asyncMiddleware'
 import populateCurrentUser from './populateCurrentUser'
 
 export default function setUpCurrentUser({ userService }: Services): Router {
   const router = Router({ mergeParams: true })
   router.use(auth.authenticationMiddleware(tokenVerifier))
   router.use(populateCurrentUser(userService))
-  router.use(asyncMiddleware(populateUserRegion(userService)))
   return router
 }

d3a25024272c34558b091c66c2d91a70ef1a43cd
rich <rich.james@dxw.com>
2023-07-25 17:50:40 +0100

    Add further integration tests
    Now we have the journey complete we can go through all the way from 'unallocated' to 'closed'

--- integration_tests/mockApis/assessments.ts
+++ integration_tests/mockApis/assessments.ts
@@ -2,7 +2,7 @@ import type { SuperAgentRequest } from 'superagent'
 import { Assessment } from '../../server/@types/shared'
 
 import api from '../../server/paths/api'
-import { stubFor } from '../../wiremock'
+import { getMatchingRequests, stubFor } from '../../wiremock'
 
 export default {
   stubAssessments: (assessments: Array<Assessment>): SuperAgentRequest =>
@@ -29,4 +29,99 @@ export default {
         jsonBody: assessment,
       },
     }),
+  stubUnallocateAssessment: (assessment: Assessment): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'DELETE',
+        url: api.assessments.allocation({ id: assessment.id }),
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: {},
+      },
+    }),
+  verifyUnallocateAssessment: async (assessmentId: string) =>
+    (
+      await getMatchingRequests({
+        method: 'DELETE',
+        url: api.assessments.allocation({ id: assessmentId }),
+      })
+    ).body.requests,
+  stubAllocateAssessment: (assessment: Assessment): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'POST',
+        url: api.assessments.allocation({ id: assessment.id }),
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: {},
+      },
+    }),
+  verifyAllocateAssessment: async (assessmentId: string) =>
+    (
+      await getMatchingRequests({
+        method: 'POST',
+        url: api.assessments.allocation({ id: assessmentId }),
+      })
+    ).body.requests,
+  stubRejectAssessment: (assessment: Assessment): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'POST',
+        url: api.assessments.rejection({ id: assessment.id }),
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: {},
+      },
+    }),
+  verifyRejectAssessment: async (assessmentId: string) =>
+    (
+      await getMatchingRequests({
+        method: 'POST',
+        url: api.assessments.rejection({ id: assessmentId }),
+      })
+    ).body.requests,
+  stubAcceptAssessment: (assessment: Assessment): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'POST',
+        url: api.assessments.acceptance({ id: assessment.id }),
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: {},
+      },
+    }),
+  verifyAcceptAssessment: async (assessmentId: string) =>
+    (
+      await getMatchingRequests({
+        method: 'POST',
+        url: api.assessments.acceptance({ id: assessmentId }),
+      })
+    ).body.requests,
+  stubCloseAssessment: (assessment: Assessment): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'POST',
+        url: api.assessments.closure({ id: assessment.id }),
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: {},
+      },
+    }),
+  verifyCloseAssessment: async (assessmentId: string) =>
+    (
+      await getMatchingRequests({
+        method: 'POST',
+        url: api.assessments.closure({ id: assessmentId }),
+      })
+    ).body.requests,
 }
--- integration_tests/tests/temporary-accommodation/assess/assess.cy.ts
+++ integration_tests/tests/temporary-accommodation/assess/assess.cy.ts
@@ -65,7 +65,7 @@ context('Apply', () => {
 
     describe('Show an assessment', () => {
       it('I can change the state of an assessment', () => {
-        const assessment = assessmentFactory.build({ status: 'unallocated' })
+        const assessment = assessmentFactory.build({ status: 'unallocated', decision: 'accepted' })
         const assessmentSummary = assessmentSummaryFactory.buildList(1, {
           status: 'unallocated',
           person: assessment.application.person,
@@ -84,13 +84,78 @@ context('Apply', () => {
         listPage.clickAssessment(assessment)
 
         // Then I should be taken to the referral show page
-        Page.verifyOnPage(AssessmentShowPage, assessment.application.person.name, assessment.status)
+        const assessmentPage = Page.verifyOnPage(
+          AssessmentShowPage,
+          assessment.application.person.name,
+          assessment.status,
+        )
+
         // Given I am on the referral page
         // When I click on the 'Update referral status to: In review' button
         assessmentPage.clickAction('In review')
 
         // Then I am taken to the confirmation page
-        Page.verifyOnPage(AssessmentConfirmPage, 'Mark this referral as in review')
+        const confirmationPage = Page.verifyOnPage(AssessmentConfirmPage, 'Mark this referral as in review')
+
+        // Given I confirm the action
+        // When the action is submitted
+        const updatedAssessment = { ...assessment, status: 'in_review' }
+        cy.task('stubAllocateAssessment', updatedAssessment)
+        cy.task('stubAcceptAssessment', updatedAssessment)
+        cy.task('stubCloseAssessment', updatedAssessment)
+        cy.task('stubFindAssessment', updatedAssessment)
+        confirmationPage.clickSubmit()
+
+        // I am taken to the show page and a banner is shown
+        Page.verifyOnPage(AssessmentShowPage, assessment.application.person.name, 'in_review')
+        assessmentPage.shouldShowBanner('Assessment updated status updated to "in review"')
+
+        // And the assessment is updated in the database
+        cy.task('verifyAllocateAssessment', assessment.id).then(requests => {
+          expect(requests).to.have.length(1)
+        })
+
+        // Given the assessment is in the 'in review' state
+        // When I click on the Update status to 'Ready to place' button
+        assessmentPage.clickAction('Ready to place')
+
+        // Then I am taken to the confirmation page
+        Page.verifyOnPage(AssessmentConfirmPage, 'Confirm this referral is ready to place')
+        cy.task('stubFindAssessment', { ...updatedAssessment, status: 'ready_to_place' })
+
+        // Given I confirm the action
+        // When the action is submitted
+        confirmationPage.clickSubmit()
+
+        // I am taken to the show page and a banner is shown
+        Page.verifyOnPage(AssessmentShowPage, assessment.application.person.name, 'ready_to_place')
+        assessmentPage.shouldShowBanner('Assessment updated status updated to "ready to place"')
+
+        // And the assessment is updated in the database
+        cy.task('verifyAcceptAssessment', assessment.id).then(requests => {
+          expect(requests).to.have.length(1)
+        })
+
+        // Given the assessment is in the 'ready to place' state
+        // When I click on the Update status to 'Closed' button
+        assessmentPage.clickAction('Close')
+
+        // Then I am taken to the confirmation page
+        Page.verifyOnPage(AssessmentConfirmPage, 'Archive this referral')
+        cy.task('stubFindAssessment', { ...updatedAssessment, status: 'closed' })
+
+        // Given I confirm the action
+        // When the action is submitted
+        confirmationPage.clickSubmit()
+
+        // I am taken to the show page and a banner is shown
+        Page.verifyOnPage(AssessmentShowPage, assessment.application.person.name, 'closed')
+        assessmentPage.shouldShowBanner('Assessment updated status updated to "closed"')
+
+        // And the assessment is updated in the database
+        cy.task('verifyCloseAssessment', assessment.id).then(requests => {
+          expect(requests).to.have.length(1)
+        })
       })
     })
   })

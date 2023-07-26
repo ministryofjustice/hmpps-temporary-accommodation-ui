8e8c76bd123144cd691a13a1586b0e72767b14a8
Bryan Gale <bryan.gale@dxw.com>
2023-07-26 12:08:02 +0100

    Add placement considerations Apply task
    We add this new task, ready to be populated with Apply pages

---
+++ server/form-pages/apply/assess-risks-for-placement/placement-considerations/index.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file */
+
+import { Task } from '../../../utils/decorators'
+
+@Task({
+  name: 'Placement considerations',
+  actionText: 'Enter placement considerations',
+  slug: 'placement-considerations',
+  pages: [],
+})
+export default class PlacementConsiderations {}

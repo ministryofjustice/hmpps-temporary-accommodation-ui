4c87835b668e4d4b59aa0364c4c2b70f0a88a419
Bryan Gale <bryan.gale@dxw.com>
2023-07-31 15:06:37 +0100

    Update risk management plan Apply page
    We update this page to import questions and pre-fill their answers from OASys, using getOasysSections(). In the case where OASys is not available, this will fall back to using the data in oasysStubs.json.
    
    This replaces a previous version of this page that used a fixed set of questions.

--- server/form-pages/apply/assess-placement-risks-and-needs/placement-considerations/riskManagementPlan.test.ts
+++ server/form-pages/apply/assess-placement-risks-and-needs/placement-considerations/riskManagementPlan.test.ts
@@ -1,18 +1,38 @@
-import { PersonRisksUI } from '../../../../@types/ui'
 import { applicationFactory, personFactory } from '../../../../testutils/factories'
-import { mapApiPersonRisksForUi } from '../../../../utils/utils'
+import { getOasysSections, oasysImportReponse, validateOasysEntries } from '../../../../utils/oasysImportUtils'
 import { itShouldHaveNextValue, itShouldHavePreviousValue } from '../../../shared-examples'
 import RiskManagementPlan from './riskManagementPlan'
 
-jest.mock('../../../../utils/utils')
+jest.mock('../../../../utils/oasysImportUtils')
 
 const body = {
-  victimSafetyPlanning: 'Victim safety planning detail',
-  supervision: 'Supervision detail',
-  monitoringAndControl: 'Monitoring and control detail',
-  interventionAndTreatment: 'Intervention and treatment detail',
+  oasysImported: '2023-08-02',
+  oasysCompleted: '2023-08-02',
+  riskManagementAnswers: {
+    Q1: 'Some answer for the first risk management question. With an extra comment 1',
+    Q2: 'Some answer for the second risk management question. With an extra comment 2',
+    Q3: 'Some answer for the third risk management question. With an extra comment 3',
+  },
+  riskManagementSummaries: [
+    {
+      questionNumber: '1',
+      label: 'The first risk management question',
+      answer: 'Some answer for the first risk management question. With an extra comment 1',
+    },
+    {
+      questionNumber: '2',
+      label: 'The second risk management question',
+      answer: 'Some answer for the second risk management question. With an extra comment 2',
+    },
+    {
+      questionNumber: '3',
+      label: 'The third risk management question',
+      answer: 'Some answer for the third risk management question. With an extra comment 3',
+    },
+  ],
 }
-const personRisksUi = { flags: { value: ['Some flag'] } } as PersonRisksUI
+const dataServices = {}
+const callConfig = { token: 'some-token' }
 
 describe('RiskManagementPlan', () => {
   const application = applicationFactory.build({
@@ -21,66 +41,49 @@ describe('RiskManagementPlan', () => {
     }),
   })
 
-  describe('constructor', () => {
-    it('sets the body and risk information', () => {
-      ;(mapApiPersonRisksForUi as jest.MockedFunction<typeof mapApiPersonRisksForUi>).mockReturnValue(personRisksUi)
+  describe('initialize', () => {
+    it('returns the result of getOasysSections', async () => {
+      const page = new RiskManagementPlan(body)
+      ;(getOasysSections as jest.MockedFunction<typeof getOasysSections>).mockResolvedValue(page)
 
-      const page = new RiskManagementPlan(body, application)
+      const result = await RiskManagementPlan.initialize(body, application, callConfig, dataServices)
 
-      expect(page.body).toEqual(body)
-      expect(page.risks).toEqual(personRisksUi)
-
-      expect(mapApiPersonRisksForUi).toHaveBeenCalledWith(application.risks)
+      expect(result).toEqual(page)
+      expect(getOasysSections).toHaveBeenCalledWith(body, application, callConfig, dataServices, RiskManagementPlan, {
+        sectionName: 'riskManagementPlan',
+        summaryKey: 'riskManagementSummaries',
+        answerKey: 'riskManagementAnswers',
+      })
     })
   })
 
-  itShouldHavePreviousValue(new RiskManagementPlan({}, application), 'rosh-level')
-  itShouldHaveNextValue(new RiskManagementPlan({}, application), '')
+  itShouldHavePreviousValue(new RiskManagementPlan({}), 'rosh-level')
+  itShouldHaveNextValue(new RiskManagementPlan({}), '')
 
   describe('errors', () => {
-    it('returns an empty object if all fields are populated', () => {
-      const page = new RiskManagementPlan(body, application)
-      expect(page.errors()).toEqual({})
-    })
+    it('returns the result of validateOasysEntries', () => {
+      const page = new RiskManagementPlan(body)
 
-    it('returns an error if the victim safety planning field is not populated', () => {
-      const page = new RiskManagementPlan({ ...body, victimSafetyPlanning: undefined }, application)
-      expect(page.errors()).toEqual({
-        victimSafetyPlanning: 'You must provide details of victim safety planning',
+      ;(validateOasysEntries as jest.MockedFunction<typeof validateOasysEntries>).mockReturnValue({
+        someField: 'An error message',
       })
-    })
 
-    it('returns an error if the supervision field is not populated', () => {
-      const page = new RiskManagementPlan({ ...body, supervision: undefined }, application)
       expect(page.errors()).toEqual({
-        supervision: 'You must provide details of supervision',
-      })
-    })
-
-    it('returns an error if the monitoring and control field is not populated', () => {
-      const page = new RiskManagementPlan({ ...body, monitoringAndControl: undefined }, application)
-      expect(page.errors()).toEqual({
-        monitoringAndControl: 'You must provide details of monitoring and control',
-      })
-    })
-
-    it('returns an error if the risk to staff field is not populated', () => {
-      const page = new RiskManagementPlan({ ...body, interventionAndTreatment: undefined }, application)
-      expect(page.errors()).toEqual({
-        interventionAndTreatment: 'You must provide details of intervention and treatment',
+        someField: 'An error message',
       })
     })
   })
 
   describe('response', () => {
-    it('returns a translated version of the response', () => {
-      const page = new RiskManagementPlan(body, application)
-      expect(page.response()).toEqual({
-        'Victim safety planning': 'Victim safety planning detail',
-        Supervision: 'Supervision detail',
-        'Monitoring and control': 'Monitoring and control detail',
-        'Intervention and treatment': 'Intervention and treatment detail',
+    it('returns the result of oasysImportReponse', () => {
+      const page = new RiskManagementPlan(body)
+
+      ;(oasysImportReponse as jest.MockedFunction<typeof oasysImportReponse>).mockReturnValue({
+        'A question': 'An answer',
       })
+
+      expect(page.response()).toEqual({ 'A question': 'An answer' })
+      expect(oasysImportReponse).toHaveBeenCalledWith(body.riskManagementAnswers, body.riskManagementSummaries)
     })
   })
 })

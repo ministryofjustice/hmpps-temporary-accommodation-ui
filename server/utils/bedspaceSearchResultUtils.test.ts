a43d7d01e9b842dca1db51ef6f9b807bfbd2fd60
Bryan Gale <bryan.gale@dxw.com>
2023-08-09 16:37:59 +0100

    Remove resultTableRows()
    We remove this function that is no longer used following our redesign of the bedspace search results page

--- server/utils/bedspaceSearchResultUtils.test.ts
+++ server/utils/bedspaceSearchResultUtils.test.ts
@@ -1,39 +1,7 @@
 import { bedSearchResultFactory, characteristicFactory, premisesFactory, roomFactory } from '../testutils/factories'
-import {
-  BedspaceSearchResultUtils,
-  bedspaceKeyCharacteristics,
-  premisesKeyCharacteristics,
-} from './bedspaceSearchResultUtils'
+import { bedspaceKeyCharacteristics, premisesKeyCharacteristics } from './bedspaceSearchResultUtils'
 
 describe('BedspaceSearchResultUtils', () => {
-  describe('resultTableRows', () => {
-    it('returns a govuk table row for each bed', () => {
-      const bedSeachResults = bedSearchResultFactory.buildList(2)
-      const firstBedResult = bedSeachResults[0]
-      const secondBedResult = bedSeachResults[1]
-
-      const tableRows = BedspaceSearchResultUtils.resultTableRows(bedSeachResults)
-
-      expect(tableRows[0]).toEqual([
-        { text: firstBedResult.room.name },
-        { text: `${firstBedResult.premises.addressLine1}, ${firstBedResult.premises.postcode}` },
-        { text: firstBedResult.premises.bedCount.toString() },
-        {
-          html: `<a href="/properties/${firstBedResult.premises.id}/bedspaces/${firstBedResult.room.id}">View<span class="govuk-visually-hidden"> bedspace for ${firstBedResult.premises.addressLine1}</span></a>`,
-        },
-      ])
-
-      expect(tableRows[1]).toEqual([
-        { text: secondBedResult.room.name },
-        { text: `${secondBedResult.premises.addressLine1}, ${secondBedResult.premises.postcode}` },
-        { text: secondBedResult.premises.bedCount.toString() },
-        {
-          html: `<a href="/properties/${secondBedResult.premises.id}/bedspaces/${secondBedResult.room.id}">View<span class="govuk-visually-hidden"> bedspace for ${secondBedResult.premises.addressLine1}</span></a>`,
-        },
-      ])
-    })
-  })
-
   describe('bedspaceKeyCharacteristics', () => {
     it('returns a sorted list of the characteristic names for the bedspace', () => {
       const premises = premisesFactory.build({

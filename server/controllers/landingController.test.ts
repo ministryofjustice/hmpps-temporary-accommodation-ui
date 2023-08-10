cd795a77f955cf2eb5b909d53356726359d8808a
patinipat <dxw@zemis.co.uk>
2023-08-10 16:12:26 +0100

    Update LandingController for Apply enabled region
    when referrers from a disabled region attempts to access the
    appliaciton journey they are redirected to the static page `use-ndelius`

--- server/controllers/landingController.test.ts
+++ server/controllers/landingController.test.ts
@@ -3,9 +3,20 @@ import type { NextFunction, Request, Response } from 'express'
 
 import paths from '../paths/apply'
 import managePaths from '../paths/temporary-accommodation/manage'
+import staticPaths from '../paths/temporary-accommodation/static'
 import { userFactory } from '../testutils/factories'
 import { UnauthorizedError } from '../utils/errors'
+import { isApplyEnabledForUser } from '../utils/userUtils'
 import LandingController from './landingController'
+
+jest.mock('../utils/userUtils', () => {
+  const module = jest.requireActual('../utils/userUtils')
+
+  return {
+    ...module,
+    isApplyEnabledForUser: jest.fn(),
+  }
+})
 
 describe('LandingController', () => {
   const request: DeepMocked<Request> = createMock<Request>({})
@@ -33,19 +44,38 @@ describe('LandingController', () => {
       expect(response.redirect).toHaveBeenCalledWith(managePaths.dashboard.index({}))
     })
 
-    it('redirects to the dashboard when the user is a referrer', () => {
+    it('redirects to the dashboard when the user is a referrer from an enbaled region', () => {
       const response: DeepMocked<Response> = createMock<Response>({
         locals: {
           user: userFactory.build({
             roles: ['referrer'],
+            region: { name: 'Kent, Surrey & Sussex' },
           }),
         },
       })
+      ;(isApplyEnabledForUser as jest.MockedFunction<typeof isApplyEnabledForUser>).mockReturnValue(true)
 
       const requestHandler = landingController.index()
       requestHandler(request, response, next)
 
       expect(response.redirect).toHaveBeenCalledWith(paths.applications.index({}))
+    })
+
+    it('redirects to use-ndelius page when the user is a referrer from a disabled region', () => {
+      const response: DeepMocked<Response> = createMock<Response>({
+        locals: {
+          user: userFactory.build({
+            roles: ['referrer'],
+            region: { name: 'London' },
+          }),
+        },
+      })
+      ;(isApplyEnabledForUser as jest.MockedFunction<typeof isApplyEnabledForUser>).mockReturnValue(false)
+
+      const requestHandler = landingController.index()
+      requestHandler(request, response, next)
+
+      expect(response.redirect).toHaveBeenCalledWith(staticPaths.static.useNDelius.pattern)
     })
 
     it('throws an error if the user is neither a assessor or a referrer', () => {
--- server/controllers/landingController.ts
+++ server/controllers/landingController.ts
@@ -2,18 +2,24 @@ import type { Request, RequestHandler, Response } from 'express'
 import { TemporaryAccommodationUser as User } from '../@types/shared'
 import applyPaths from '../paths/apply'
 import managePaths from '../paths/temporary-accommodation/manage'
+import staticPaths from '../paths/temporary-accommodation/static'
 import { UnauthorizedError } from '../utils/errors'
+import { isApplyEnabledForUser, userHasAssessorRole, userHasReferrerRole } from '../utils/userUtils'
 
 export default class LandingController {
   index(): RequestHandler {
     return (_req: Request, res: Response) => {
       const user = res.locals.user as User
 
-      if (user.roles.includes('assessor')) {
+      if (userHasAssessorRole(user)) {
         return res.redirect(managePaths.dashboard.index({}))
       }
-      if (user.roles.includes('referrer')) {
-        return res.redirect(applyPaths.applications.index({}))
+
+      if (userHasReferrerRole(user)) {
+        if (isApplyEnabledForUser(user)) {
+          return res.redirect(applyPaths.applications.index({}))
+        }
+        return res.redirect(staticPaths.static.useNDelius.pattern)
       }
 
       throw new UnauthorizedError()

1031e06857cd663f37ecc7e5b9f40227adf75bed
Bryan Gale <bryan.gale@dxw.com>
2023-08-14 11:41:19 +0100

    Fix bedspace E2E tests
    We fix how we query for a particular search result in our tests so as to not depend on the ID of the bedspace room, which we do not know in our E2E tests

--- cypress_shared/components/bedspaceSearchResult.ts
+++ cypress_shared/components/bedspaceSearchResult.ts
@@ -7,28 +7,30 @@ export default class BedspaceSearchResult extends Component {
   }
 
   shouldShowResult(checkCount = true): void {
-    cy.get(`[data-cy-room-id="${this.result.room.id}"]`).within(() => {
-      cy.get('h2').should('contain', this.result.room.name)
-      if (this.result.premises.town) {
-        cy.get('h3').should(
-          'contain',
-          `${this.result.premises.addressLine1}, ${this.result.premises.town}, ${this.result.premises.postcode}`,
-        )
-      } else {
-        cy.get('h3').should('contain', `${this.result.premises.addressLine1},${this.result.premises.postcode}`)
-      }
+    cy.get('h2')
+      .contains(this.result.room.name)
+      .parents('div[data-cy-bedspace]')
+      .within(() => {
+        if (this.result.premises.town) {
+          cy.get('h3').should(
+            'contain',
+            `${this.result.premises.addressLine1}, ${this.result.premises.town}, ${this.result.premises.postcode}`,
+          )
+        } else {
+          cy.get('h3').should('contain', `${this.result.premises.addressLine1},${this.result.premises.postcode}`)
+        }
 
-      this.result.premises.characteristics.forEach(characteristic => {
-        cy.get('ul[data-cy-premises-key-characteristics] > li').should('contain', characteristic.name)
+        this.result.premises.characteristics.forEach(characteristic => {
+          cy.get('ul[data-cy-premises-key-characteristics] > li').should('contain', characteristic.name)
+        })
+
+        this.result.room.characteristics.forEach(characteristic => {
+          cy.get('ul[data-cy-bedspace-key-characteristics] > li').should('contain', characteristic.name)
+        })
+
+        if (checkCount) {
+          this.shouldShowKeyAndValue('Number of bedspaces', `${this.result.premises.bedCount}`)
+        }
       })
-
-      this.result.room.characteristics.forEach(characteristic => {
-        cy.get('ul[data-cy-bedspace-key-characteristics] > li').should('contain', characteristic.name)
-      })
-
-      if (checkCount) {
-        this.shouldShowKeyAndValue('Number of bedspaces', `${this.result.premises.bedCount}`)
-      }
-    })
   }
 }

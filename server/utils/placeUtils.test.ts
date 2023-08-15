02c19d4c6f48ab5c75a8b38d6c2033894dcc77a4
Bryan Gale <bryan.gale@dxw.com>
2023-08-15 10:53:19 +0100

    Add preservePlaceContext() to placeUtils
    We add this function for retrieving place context information from the assessment service, and exposing it to Nunjucks, via res.locals. We want to call this in the request handler of any page we want to be aware of place context. It now makes sense to expose addPlaceContext() to Nunjucks, with the placeContext parameter being populated automatically.
    
    We swallow any errors from the assessment service, as place context is never essential to a page, and errors here should not prevent the user from progressing.
    
    We use declaration merging to add placeContext to the type of res.locals.

--- server/utils/placeUtils.test.ts
+++ server/utils/placeUtils.test.ts
@@ -1,8 +1,14 @@
+import { createMock } from '@golevelup/ts-jest'
+import { Request, Response } from 'express'
+import { CallConfig } from '../data/restClient'
+import type { AssessmentsService } from '../services'
 import { assessmentFactory, placeContextFactory } from '../testutils/factories'
-import { addPlaceContext, createPlaceContext } from './placeUtils'
+import { addPlaceContext, createPlaceContext, preservePlaceContext } from './placeUtils'
+import extractCallConfig from './restUtils'
 import { appendQueryString } from './utils'
 
 jest.mock('./utils')
+jest.mock('./restUtils')
 
 describe('placeUtils', () => {
   beforeEach(() => {
@@ -36,6 +42,72 @@ describe('placeUtils', () => {
     it('returns the original path if the place context is undefinded', () => {
       expect(addPlaceContext('/some/path', undefined)).toEqual('/some/path')
       expect(appendQueryString).not.toHaveBeenCalled()
+    })
+  })
+
+  describe('preservePlaceContext', () => {
+    it('uses the assessment service to set and return place context data', async () => {
+      const callConfig = { token: 'some-call-config-token' } as CallConfig
+
+      ;(extractCallConfig as jest.MockedFn<typeof extractCallConfig>).mockReturnValue(callConfig)
+
+      const assessmentService = createMock<AssessmentsService>()
+
+      const assessment = assessmentFactory.build()
+      assessmentService.findAssessment.mockResolvedValue(assessment)
+
+      const req = createMock<Request>({
+        query: { placeContextAssessmentId: 'some-assessment-id' },
+      })
+      const res = createMock<Response>({
+        locals: {},
+      })
+
+      const result = await preservePlaceContext(req, res, assessmentService)
+
+      expect(result).toEqual({ assessment })
+      expect(res.locals.placeContext).toEqual({ assessment })
+      expect(assessmentService.findAssessment).toHaveBeenCalledWith(callConfig, 'some-assessment-id')
+    })
+
+    it('returns undefined when the assessment service throws an error', async () => {
+      const callConfig = { token: 'some-call-config-token' } as CallConfig
+
+      ;(extractCallConfig as jest.MockedFn<typeof extractCallConfig>).mockReturnValue(callConfig)
+
+      const assessmentService = createMock<AssessmentsService>()
+
+      assessmentService.findAssessment.mockRejectedValue(new Error())
+
+      const req = createMock<Request>({
+        query: { placeContextAssessmentId: 'some-assessment-id' },
+      })
+      const res = createMock<Response>({
+        locals: {},
+      })
+
+      const result = await preservePlaceContext(req, res, assessmentService)
+
+      expect(result).toEqual(undefined)
+      expect(res.locals.placeContext).toEqual(undefined)
+      expect(assessmentService.findAssessment).toHaveBeenCalledWith(callConfig, 'some-assessment-id')
+    })
+
+    it('does nothing when the place context assessment ID is not set in the query parameters', async () => {
+      const assessmentService = createMock<AssessmentsService>()
+
+      const req = createMock<Request>({
+        query: {},
+      })
+      const res = createMock<Response>({
+        locals: {},
+      })
+
+      const result = await preservePlaceContext(req, res, assessmentService)
+
+      expect(result).toEqual(undefined)
+      expect(res.locals.placeContext).toEqual(undefined)
+      expect(assessmentService.findAssessment).not.toHaveBeenCalled()
     })
   })
 })

72562a510d6c2a1f3214a28c87f4a8891edebbc8
Bryan Gale <bryan.gale@dxw.com>
2023-08-15 10:36:19 +0100

    Add place context header macro
    We add a Nunjucks macro for displaying place context on pages in the user journey from an assessment through to completing a booking. This will display essential information about the person being placed.
    
    We also add PlaceContextHeaderComponent, and add shouldShowPlaceContextHeader() and shouldNotShowPlaceContextHeader() to our base Page class to assist integration testing this component.

---
+++ cypress_shared/components/placeContextHeader.ts
@@ -0,0 +1,42 @@
+import { PlaceContext } from '../../server/@types/ui'
+import { DateFormats } from '../../server/utils/dateUtils'
+import { isFullPerson, personName } from '../../server/utils/personUtils'
+import Component from './component'
+
+export default class PlaceContextHeaderComponent extends Component {
+  constructor(private readonly placeContext: PlaceContext) {
+    super()
+  }
+
+  static shouldNotShowPlaceContextDetails(): void {
+    cy.get('.place-context-header').should('not.exist')
+  }
+
+  shouldShowPlaceContextDetails(): void {
+    cy.get('.place-context-header').within(() => {
+      const { application } = this.placeContext!.assessment
+      const { person } = application
+
+      cy.root().should('contain', personName(person, 'Limited access offender'))
+
+      if (isFullPerson(person)) {
+        cy.root().should(
+          'contain',
+          `Date of birth: ${DateFormats.isoDateToUIDate(person.dateOfBirth, { format: 'short' })}`,
+        )
+        cy.root().should('contain', `Sex: ${person.sex}`)
+
+        if (person.genderIdentity) {
+          cy.root().should('contain', `Gender identity: ${person.genderIdentity}`)
+        } else {
+          cy.root().should('not.contain', `Gender identity`)
+        }
+      }
+      cy.root().should('contain', `CRN: ${person.crn}`)
+      cy.root().should(
+        'contain',
+        `Accommodation required from: ${DateFormats.isoDateToUIDate(application.arrivalDate, { format: 'short' })}`,
+      )
+    })
+  }
+}

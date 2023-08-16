a333dfb3ab789e2ce0e45b6ea745779e49e05a00
Bryan Gale <bryan.gale@dxw.com>
2023-08-16 14:51:22 +0100

    Integration test beginning of place journey
    We can now integration test our first steps of the place journey. We will expand this as further steps are made place context-aware. We add PlaceHelper, similar to ApplyHelper, with the intent that we can reuse this when we E2E test our place journey

---
+++ cypress_shared/helpers/place.ts
@@ -0,0 +1,72 @@
+import { BedSearchResults, TemporaryAccommodationPremises as Premises, Room } from '../../server/@types/shared'
+import { PlaceContext } from '../../server/@types/ui'
+import {
+  bedSearchParametersFactory,
+  bedSearchResultFactory,
+  bedSearchResultsFactory,
+} from '../../server/testutils/factories'
+import AssessmentSummaryPage from '../pages/assess/summary'
+import Page from '../pages/page'
+import BedspaceSearchPage from '../pages/temporary-accommodation/manage/bedspaceSearch'
+
+export default class PlaceHelper {
+  private readonly bedSearchResults: BedSearchResults
+
+  constructor(
+    private readonly placeContext: NonNullable<PlaceContext>,
+    private readonly premises: Premises,
+    private readonly room: Room,
+  ) {
+    this.bedSearchResults = bedSearchResultsFactory.build({
+      results: [bedSearchResultFactory.forBedspace(this.premises, this.room).build()],
+    })
+  }
+
+  setupStubs() {
+    cy.task('stubFindAssessment', this.placeContext.assessment)
+    cy.task('stubBedspaceSearchReferenceData')
+    cy.task('stubBedSearch', this.bedSearchResults)
+  }
+
+  startPlace() {
+    AssessmentSummaryPage.visit(this.placeContext.assessment)
+  }
+
+  progressPlace() {
+    this.assessmentToBedspaceSearch()
+    this.bedspaceSearchToSearchResults()
+  }
+
+  private assessmentToBedspaceSearch() {
+    // Given I am viewing a ready to place assessment
+    const assessmentSummaryPage = Page.verifyOnPage(AssessmentSummaryPage, this.placeContext.assessment)
+
+    // When I click "Place referral"
+    assessmentSummaryPage.clickAction('Place referral')
+
+    // I am taken to the bedspace search page
+    const bedspaceSearchPage = Page.verifyOnPage(BedspaceSearchPage)
+
+    // And the place context header is visible
+    bedspaceSearchPage.shouldShowPlaceContextHeader(this.placeContext)
+
+    // And the start date is prefilled
+    bedspaceSearchPage.shouldShowPrefilledSearchParametersFromPlaceContext(this.placeContext)
+  }
+
+  private bedspaceSearchToSearchResults() {
+    // Given I am viewing the bedspace search page
+    const bedspaceSearchPage = Page.verifyOnPage(BedspaceSearchPage)
+
+    // When I fill out the form
+    const searchParameters = bedSearchParametersFactory.build()
+    bedspaceSearchPage.completeForm(searchParameters)
+    bedspaceSearchPage.clickSubmit()
+
+    // I am taken to the search results page
+    const resultsBedspaceSearchPage = Page.verifyOnPage(BedspaceSearchPage, this.bedSearchResults)
+
+    // And the place context header is visible
+    resultsBedspaceSearchPage.shouldShowPlaceContextHeader(this.placeContext)
+  }
+}

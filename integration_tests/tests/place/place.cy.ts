3c687025de5eb93912da40d7fda4df04b9bff213
Bryan Gale <bryan.gale@dxw.com>
2023-08-16 14:51:22 +0100

    Integration test beginning of place journey
    We can now integration test our first steps of the place journey. We will expand this as further steps are made place context-aware. We add PlaceHelper, similar to ApplyHelper, with the intent that we can reuse this when we E2E test our place journey

---
+++ integration_tests/tests/place/place.cy.ts
@@ -0,0 +1,28 @@
+import PlaceHelper from '../../../cypress_shared/helpers/place'
+import { setupTestUser } from '../../../cypress_shared/utils/setupTestUser'
+import { placeContextFactory, premisesFactory, roomFactory } from '../../../server/testutils/factories'
+
+context('Place', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    setupTestUser('assessor')
+  })
+
+  it('allows the user progress along the "place" joureny', () => {
+    // Given I am signed in
+    cy.signIn()
+
+    const premises = premisesFactory.build()
+    const room = roomFactory.build()
+
+    const placeContext = placeContextFactory.build()
+    const placeHelper = new PlaceHelper(placeContext, premises, room)
+    placeHelper.setupStubs()
+
+    // And I am at the start of the "place" journey
+    placeHelper.startPlace()
+
+    // I can progress along the "place" journey
+    placeHelper.progressPlace()
+  })
+})

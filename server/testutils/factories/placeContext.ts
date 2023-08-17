35d3f654e39a0f8320521039e5c2c04e0806ad71
Bryan Gale <bryan.gale@dxw.com>
2023-08-17 09:14:37 +0100

    Add updatePlaceContextWithArrivalDate()
    Within a place context, we want the arrival date of a booking to come from the date that a user searched for when finding a bedspace. To that end, we expand our PlaceContext type, and add updatePlaceContextWithArrivalDate()

--- server/testutils/factories/placeContext.ts
+++ server/testutils/factories/placeContext.ts
@@ -1,9 +1,12 @@
+import { faker } from '@faker-js/faker'
 import { Factory } from 'fishery'
 import { PlaceContext } from '../../@types/ui'
+import { DateFormats } from '../../utils/dateUtils'
 import assessmentFactory from './assessment'
 
 export default Factory.define<PlaceContext>(() => ({
   assessment: assessmentFactory.build({
     status: 'ready_to_place',
   }),
+  arrivalDate: DateFormats.dateObjToIsoDate(faker.date.future()),
 }))

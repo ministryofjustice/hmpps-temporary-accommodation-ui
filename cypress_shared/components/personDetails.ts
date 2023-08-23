599978e4db24e2828371200ea0fc29097fc97179
Bryan Gale <bryan.gale@dxw.com>
2023-08-23 14:05:32 +0100

    Update person details component to handle LAOs
    We update our person details component, used in our Apply journey, to handle LAOs, by always using isFullPerson(), rather than accessing optional fields directly

--- cypress_shared/components/personDetails.ts
+++ cypress_shared/components/personDetails.ts
@@ -1,5 +1,6 @@
 import type { Person } from '@approved-premises/api'
 import { DateFormats } from '../../server/utils/dateUtils'
+import { isFullPerson } from '../../server/utils/personUtils'
 import Component from './component'
 
 export default class PersonDetailsComponent extends Component {
@@ -9,22 +10,35 @@ export default class PersonDetailsComponent extends Component {
 
   shouldShowPersonDetails(): void {
     cy.get('dl[data-cy-person-info]').within(() => {
-      this.assertDefinition('Name', this.person.name)
+      if (isFullPerson(this.person)) {
+        this.assertDefinition('Name', this.person.name)
+      }
       this.assertDefinition('CRN', this.person.crn)
-      this.assertDefinition('Date of birth', DateFormats.isoDateToUIDate(this.person.dateOfBirth, { format: 'short' }))
-      this.assertDefinition('NOMS number', this.person.nomsNumber)
-      if (this.person.nationality) {
-        this.assertDefinition('Nationality', this.person.nationality)
+      if (isFullPerson(this.person)) {
+        this.assertDefinition(
+          'Date of birth',
+          DateFormats.isoDateToUIDate(this.person.dateOfBirth, { format: 'short' }),
+        )
+
+        if (this.person.nomsNumber) {
+          this.assertDefinition('NOMS number', this.person.nomsNumber)
+        }
+        if (this.person.nationality) {
+          this.assertDefinition('Nationality', this.person.nationality)
+        }
+        if (this.person.religionOrBelief) {
+          this.assertDefinition('Religion or belief', this.person.religionOrBelief)
+        }
+        if (this.person.ethnicity) {
+          this.assertDefinition('Ethnicity', this.person.ethnicity)
+        }
+        this.assertDefinition('Sex', this.person.sex)
+        cy.get(`[data-cy-status]`).should('have.attr', 'data-cy-status').and('equal', this.person.status)
+
+        if (this.person.prisonName) {
+          this.assertDefinition('Prison', this.person.prisonName)
+        }
       }
-      if (this.person.religionOrBelief) {
-        this.assertDefinition('Religion or belief', this.person.religionOrBelief)
-      }
-      if (this.person.ethnicity) {
-        this.assertDefinition('Ethnicity', this.person.ethnicity)
-      }
-      this.assertDefinition('Sex', this.person.sex)
-      cy.get(`[data-cy-status]`).should('have.attr', 'data-cy-status').and('equal', this.person.status)
-      this.assertDefinition('Prison', this.person.prisonName)
     })
   }
 }

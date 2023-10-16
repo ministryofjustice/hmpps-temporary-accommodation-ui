4c330f7e796c35c9881c1eb9aece928aef6b9948
Daniel Liburd <daniel.liburd@gmail.com>
2023-10-16 15:22:30 +0100

    Allow reporters to only access the reports page
    We add a condition to the middleware check that ensures a user
    has the reporter role and the path they are trying to access is a
    reporting path.
    
    We introduce a new userIsAuthorisedForManage function to encapsulate
    this new condition and the original condition for checking for the
    'assessor' role.

--- server/middleware/userCheckMiddleware.test.ts
+++ server/middleware/userCheckMiddleware.test.ts
@@ -2,8 +2,9 @@ import { createMock } from '@golevelup/ts-jest'
 import { Request, Response } from 'express'
 import { userFactory } from '../testutils/factories'
 import { UnauthorizedError } from '../utils/errors'
-import { TemporaryAccommodationUser as User } from '../@types/shared'
-import { createUserCheckMiddleware } from './userCheckMiddleware'
+import { TemporaryAccommodationUserRole, TemporaryAccommodationUser as User } from '../@types/shared'
+import { createUserCheckMiddleware, userIsAuthorisedForManage } from './userCheckMiddleware'
+import paths from '../paths/temporary-accommodation/manage'
 
 describe('createUserCheckMiddleware', () => {
   it('returns middleware that calls the handler when the user predicate succeeds', async () => {
@@ -44,5 +45,98 @@ describe('createUserCheckMiddleware', () => {
 
     expect(handler).not.toHaveBeenCalled()
     expect(next).toHaveBeenCalledWith(new UnauthorizedError())
+  })
+
+  describe('userIsAuthorisedForManage', () => {
+    it('returns middleware that calls next() with an unauthorized error when the path is not reports', async () => {
+      const handler = jest.fn()
+      const request = createMock<Request>({ path: paths.assessments.index({}) })
+      const response = createMock<Response>({
+        locals: {
+          user: userFactory.build({ roles: ['referrer'] }),
+        },
+      })
+      const next = jest.fn()
+
+      const middleware = createUserCheckMiddleware(userIsAuthorisedForManage)
+
+      middleware(handler)(request, response, next)
+
+      expect(handler).not.toHaveBeenCalled()
+      expect(next).toHaveBeenCalledWith(new UnauthorizedError())
+    })
+
+    describe('when the user has assessor role', () => {
+      it('returns middleware that calls the handler', async () => {
+        const handler = jest.fn()
+        const request = createMock<Request>({ path: paths.assessments.index({}) })
+        const response = createMock<Response>({
+          locals: {
+            user: userFactory.build({ roles: ['assessor'] }),
+          },
+        })
+        const next = jest.fn()
+
+        const middleware = createUserCheckMiddleware(userIsAuthorisedForManage)
+
+        middleware(handler)(request, response, next)
+
+        expect(handler).toHaveBeenCalledWith(request, response, next)
+      })
+    })
+
+    describe('when the user has reporter role', () => {
+      it('returns middleware that calls the handler when the the path is reports#new', async () => {
+        const handler = jest.fn()
+        const request = createMock<Request>({ path: paths.reports.new({}) })
+        const response = createMock<Response>({
+          locals: {
+            user: userFactory.build({ roles: ['reporter' as TemporaryAccommodationUserRole] }),
+          },
+        })
+        const next = jest.fn()
+
+        const middleware = createUserCheckMiddleware(userIsAuthorisedForManage)
+
+        middleware(handler)(request, response, next)
+
+        expect(handler).toHaveBeenCalledWith(request, response, next)
+      })
+
+      it('returns middleware that calls the handler when the path is reports#create', async () => {
+        const handler = jest.fn()
+        const request = createMock<Request>({ path: paths.reports.create({}) })
+        const response = createMock<Response>({
+          locals: {
+            user: userFactory.build({ roles: ['reporter' as TemporaryAccommodationUserRole] }),
+          },
+        })
+        const next = jest.fn()
+
+        const middleware = createUserCheckMiddleware(userIsAuthorisedForManage)
+
+        middleware(handler)(request, response, next)
+
+        expect(handler).toHaveBeenCalledWith(request, response, next)
+      })
+
+      it('returns middleware that calls next() with an unauthorized error when the path is not reports', async () => {
+        const handler = jest.fn()
+        const request = createMock<Request>({ path: paths.assessments.index({}) })
+        const response = createMock<Response>({
+          locals: {
+            user: userFactory.build({ roles: ['reporter' as TemporaryAccommodationUserRole] }),
+          },
+        })
+        const next = jest.fn()
+
+        const middleware = createUserCheckMiddleware(userIsAuthorisedForManage)
+
+        middleware(handler)(request, response, next)
+
+        expect(handler).not.toHaveBeenCalled()
+        expect(next).toHaveBeenCalledWith(new UnauthorizedError())
+      })
+    })
   })
 })
--- server/middleware/userCheckMiddleware.ts
+++ server/middleware/userCheckMiddleware.ts
@@ -1,17 +1,27 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express'
+import paths from '../paths/temporary-accommodation/manage'
 import { TemporaryAccommodationUser as User } from '../@types/shared'
 import { UnauthorizedError } from '../utils/errors'
+import { userHasAssessorRole, userHasReporterRole } from '../utils/userUtils'
 
-export const createUserCheckMiddleware = (check: (user: User) => boolean) => {
+export const createUserCheckMiddleware = (check: (user: User, path: string) => boolean) => {
   return (handler: RequestHandler) => wrapHandler(handler, check)
 }
 
 const wrapHandler =
-  (handler: RequestHandler, check: (user: User) => boolean) =>
+  (handler: RequestHandler, check: (user: User, path: string) => boolean) =>
   async (req: Request, res: Response, next: NextFunction) => {
-    if (check(res.locals.user)) {
+    if (check(res.locals.user, req.path)) {
       await handler(req, res, next)
     } else {
       next(new UnauthorizedError())
     }
   }
+
+const isReportPath = (path: string): boolean => path === paths.reports.new({}) || path === paths.reports.create({})
+
+const userHasReporterRoleAndPathIsReports = (user: User, path: string): boolean =>
+  userHasReporterRole(user) && isReportPath(path)
+
+export const userIsAuthorisedForManage = (user: User, path: string): boolean =>
+  userHasAssessorRole(user) || userHasReporterRoleAndPathIsReports(user, path)

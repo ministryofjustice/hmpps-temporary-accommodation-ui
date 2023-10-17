fe5bc0d512e01af009349b8aee20cae60d47d0fa
Daniel Liburd <daniel.liburd@gmail.com>
2023-10-17 16:12:42 +0100

    Add missing report stubs
    Add stubs for bedspace utilisation and bedspace usage.

--- wiremock/reportStubs.ts
+++ wiremock/reportStubs.ts
@@ -2,18 +2,46 @@ import paths from '../server/paths/api'
 
 const reportStubs: Array<Record<string, unknown>> = []
 
-reportStubs.push({
-  request: {
-    method: 'GET',
-    urlPath: paths.reports.bookings({}),
-  },
-  response: {
-    status: 200,
-    headers: {
-      'Content-Type': 'text/csv',
+reportStubs.push(
+  {
+    request: {
+      method: 'GET',
+      urlPath: paths.reports.bookings({}),
     },
-    body: '"Column 1","Column 2"\n"Entry 1A","Entry 1B"\n"Entry 2A","Entry 2B"',
+    response: {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/csv',
+      },
+      body: '"Column 1","Column 2"\n"Entry 1A","Entry 1B"\n"Entry 2A","Entry 2B"',
+    },
   },
-})
+  {
+    request: {
+      method: 'GET',
+      urlPath: paths.reports.bedspaceUsage({}),
+    },
+    response: {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/csv',
+      },
+      body: '"Column 1","Column 2"\n"Entry 1A","Entry 1B"\n"Entry 2A","Entry 2B"',
+    },
+  },
+  {
+    request: {
+      method: 'GET',
+      urlPath: paths.reports.bedspaceUtilisation({}),
+    },
+    response: {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/csv',
+      },
+      body: '"Column 1","Column 2"\n"Entry 1A","Entry 1B"\n"Entry 2A","Entry 2B"',
+    },
+  },
+)
 
 export default reportStubs

4ea22dea29b579e367d2f70412f0a1a205905beb
Daniel Liburd <daniel.liburd@gmail.com>
2023-11-02 17:55:13 +0000

    Render local authority dropdown on DTR form page
    We introduce an optional local authority field. As it is optional, it
    should not prevent the application from being submitted if the fetch for
    local authorities fails. In this scenario, the select field isn't
    rendered.
    
    Moreover, we don't add any validation to this field.

--- server/form-pages/apply/required-referrals/accommodation-referral-details/dtrDetails.test.ts
+++ server/form-pages/apply/required-referrals/accommodation-referral-details/dtrDetails.test.ts
@@ -1,6 +1,9 @@
-import { applicationFactory } from '../../../../testutils/factories'
+import { createMock } from '@golevelup/ts-jest'
+import { applicationFactory, localAuthorityFactory } from '../../../../testutils/factories'
 import { dateAndTimeInputsAreValidDates, dateIsBlank, dateIsInFuture } from '../../../../utils/dateUtils'
 import { itShouldHaveNextValue, itShouldHavePreviousValue } from '../../../shared-examples'
+import { CallConfig } from '../../../../data/restClient'
+import { ReferenceDataService } from '../../../../services'
 import DtrDetails from './dtrDetails'
 
 jest.mock('../../../../utils/dateUtils', () => {
@@ -14,14 +17,21 @@ jest.mock('../../../../utils/dateUtils', () => {
   }
 })
 
-const body = { reference: 'ABC123', 'date-year': '2022', 'date-month': '7', 'date-day': '23' }
+const localAuthorities = localAuthorityFactory.buildList(3)
+const body = {
+  reference: 'ABC123',
+  'date-year': '2022',
+  'date-month': '7',
+  'date-day': '23',
+  localAuthorityAreaName: localAuthorities[0].name,
+}
 
 describe('DtrDetails', () => {
   const application = applicationFactory.build()
 
   describe('body', () => {
     it('sets the body', () => {
-      const page = new DtrDetails(body, application)
+      const page = new DtrDetails(body, application, localAuthorities)
 
       expect(page.body).toEqual({
         ...body,
@@ -30,8 +40,8 @@ describe('DtrDetails', () => {
     })
   })
 
-  itShouldHavePreviousValue(new DtrDetails({}, application), 'dtr-submitted')
-  itShouldHaveNextValue(new DtrDetails({}, application), 'crs-submitted')
+  itShouldHavePreviousValue(new DtrDetails({}, application, localAuthorities), 'dtr-submitted')
+  itShouldHaveNextValue(new DtrDetails({}, application, localAuthorities), 'crs-submitted')
 
   describe('errors', () => {
     it('returns an empty object if the DTR details are populated', () => {
@@ -39,7 +49,7 @@ describe('DtrDetails', () => {
       ;(dateAndTimeInputsAreValidDates as jest.Mock).mockReturnValue(true)
       ;(dateIsInFuture as jest.Mock).mockReturnValue(false)
 
-      const page = new DtrDetails(body, application)
+      const page = new DtrDetails(body, application, localAuthorities)
       expect(page.errors()).toEqual({})
     })
 
@@ -48,7 +58,7 @@ describe('DtrDetails', () => {
       ;(dateAndTimeInputsAreValidDates as jest.Mock).mockReturnValue(true)
       ;(dateIsInFuture as jest.Mock).mockReturnValue(false)
 
-      const page = new DtrDetails({ ...body, reference: undefined }, application)
+      const page = new DtrDetails({ ...body, reference: undefined }, application, localAuthorities)
       expect(page.errors()).toEqual({
         reference: 'You must specify the DTR / NOP reference number',
       })
@@ -59,7 +69,7 @@ describe('DtrDetails', () => {
       ;(dateAndTimeInputsAreValidDates as jest.Mock).mockReturnValue(true)
       ;(dateIsInFuture as jest.Mock).mockReturnValue(false)
 
-      const page = new DtrDetails(body, application)
+      const page = new DtrDetails(body, application, localAuthorities)
       expect(page.errors()).toEqual({
         date: 'You must specify the date DTR / NOP was submitted',
       })
@@ -70,7 +80,7 @@ describe('DtrDetails', () => {
       ;(dateAndTimeInputsAreValidDates as jest.Mock).mockReturnValue(false)
       ;(dateIsInFuture as jest.Mock).mockReturnValue(false)
 
-      const page = new DtrDetails(body, application)
+      const page = new DtrDetails(body, application, localAuthorities)
       expect(page.errors()).toEqual({
         date: 'You must specify a valid date DTR / NOP was submitted',
       })
@@ -81,7 +91,7 @@ describe('DtrDetails', () => {
       ;(dateAndTimeInputsAreValidDates as jest.Mock).mockReturnValue(true)
       ;(dateIsInFuture as jest.Mock).mockReturnValue(true)
 
-      const page = new DtrDetails(body, application)
+      const page = new DtrDetails(body, application, localAuthorities)
       expect(page.errors()).toEqual({
         date: 'The date DTR / NOP was submitted must not be in the future',
       })
@@ -89,12 +99,72 @@ describe('DtrDetails', () => {
   })
 
   describe('response', () => {
-    it('returns a translated version of the response', () => {
-      const page = new DtrDetails(body, application)
-      expect(page.response()).toEqual({
-        'DTR / NOP reference number': 'ABC123',
-        'Date DTR / NOP was submitted': '23 July 2022',
+    describe('when local authority is selected', () => {
+      it('returns a translated version of the response', () => {
+        const page = new DtrDetails(body, application, localAuthorities)
+        expect(page.response()).toEqual({
+          'DTR / NOP reference number': 'ABC123',
+          'Date DTR / NOP was submitted': '23 July 2022',
+          'What is the local authority (optional)?': localAuthorities[0].name,
+        })
       })
+    })
+
+    describe('when local authority is not selected', () => {
+      it('returns placeholder copy for local authority', () => {
+        const page = new DtrDetails({ ...body, localAuthorityAreaName: null }, application, localAuthorities)
+        expect(page.response()).toEqual({
+          'DTR / NOP reference number': 'ABC123',
+          'Date DTR / NOP was submitted': '23 July 2022',
+          'What is the local authority (optional)?': 'No local authority selected',
+        })
+      })
+    })
+  })
+
+  describe('initialize', () => {
+    describe('when fetch for local authorities is successful', () => {
+      it('populates the body with a list of local authorities', async () => {
+        const callConfig = { token: 'some-token' } as CallConfig
+        const getLocalAuthoritiesMock = jest.fn().mockResolvedValue(localAuthorities)
+        const referenceDataService = createMock<ReferenceDataService>({
+          getLocalAuthorities: getLocalAuthoritiesMock,
+        })
+
+        await DtrDetails.initialize({}, application, callConfig, { referenceDataService })
+
+        expect(getLocalAuthoritiesMock).toHaveBeenCalledWith(callConfig)
+      })
+    })
+
+    describe('when fetch for local authorities is unsuccessful', () => {
+      it('returns an empty list of local authorities', async () => {
+        const callConfig = { token: 'some-token' } as CallConfig
+        const getLocalAuthoritiesMock = jest.fn().mockImplementation(() => {
+          throw new Error()
+        })
+        const referenceDataService = createMock<ReferenceDataService>({
+          getLocalAuthorities: getLocalAuthoritiesMock,
+        })
+
+        const page = await DtrDetails.initialize({}, application, callConfig, { referenceDataService })
+
+        expect(page.getLocalAuthorities()).toEqual([])
+      })
+    })
+  })
+
+  describe('getLocalAuthorities', () => {
+    it('returns a list of local authorities', async () => {
+      const callConfig = { token: 'some-token' } as CallConfig
+      const getLocalAuthoritiesMock = jest.fn().mockResolvedValue(localAuthorities)
+      const referenceDataService = createMock<ReferenceDataService>({
+        getLocalAuthorities: getLocalAuthoritiesMock,
+      })
+
+      const page = await DtrDetails.initialize({}, application, callConfig, { referenceDataService })
+
+      expect(page.getLocalAuthorities()).toEqual(localAuthorities)
     })
   })
 })
--- server/services/index.ts
+++ server/services/index.ts
@@ -103,4 +103,5 @@ export {
   PremisesService,
   TurnaroundService,
   UserService,
+  ReferenceDataService,
 }

d3ecdde17dbd384f4a5d4cba93554b0d387361ba
Andrew Charnley <andrew.charnley@digital.justice.gov.uk>
2023-11-29 11:12:16 +0000

    Revert "Revert "Merge pull request #764 from ministryofjustice/feature/1684-add-pagination-to-bookings""
    This reverts commit a1178aab4a089401a4e985fc7fe8762b7a8b0bc4.

--- server/controllers/temporary-accommodation/manage/bookingSearchController.ts
+++ server/controllers/temporary-accommodation/manage/bookingSearchController.ts
@@ -1,7 +1,9 @@
 import type { Request, RequestHandler, Response } from 'express'
 import { BookingSearchService } from 'server/services'
+import { BookingSearchSortField, SortDirection } from '@approved-premises/api'
 import type { BookingSearchApiStatus } from '@approved-premises/ui'
 import extractCallConfig from '../../../utils/restUtils'
+import { pagination } from '../../../utils/pagination'
 import { convertApiStatusToUiStatus, createSideNavArr, createTableHeadings } from '../../../utils/bookingSearchUtils'
 
 export default class BookingSearchController {
@@ -11,13 +13,28 @@ export default class BookingSearchController {
     return async (req: Request, res: Response) => {
       const callConfig = extractCallConfig(req)
 
-      const bookingTableRows = await this.bookingSearchService.getTableRowsForFindBooking(callConfig, status)
+      const response = await this.bookingSearchService.getTableRowsForFindBooking(
+        callConfig,
+        status,
+        Number(req.query.page),
+        req.query.sortBy as BookingSearchSortField,
+        req.query.sortDirection as SortDirection,
+      )
+
+      // the params are defaulted downstream, inspect to find out what they are
+      const sortBy = response.url.params.get('sortField') as BookingSearchSortField
+      const ascending = response.url.params.get('sortOrder') === 'ascending'
 
       return res.render(`temporary-accommodation/booking-search/results`, {
         uiStatus: convertApiStatusToUiStatus(status),
         sideNavArr: createSideNavArr(status),
-        tableHeadings: createTableHeadings(status),
-        bookingTableRows,
+        tableHeadings: createTableHeadings(status, sortBy, ascending, ''), // dont send the page, will revert to 1 on sort change
+        pagination: pagination(
+          response.pageNumber,
+          response.totalPages,
+          `?sortBy=${sortBy}&sortDirection=${ascending ? 'asc' : 'desc'}`,
+        ),
+        response,
       })
     }
   }

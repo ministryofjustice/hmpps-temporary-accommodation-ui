127b65b1ebc25dc3cb592302b7b9a8248ab526a4
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2024-02-26 13:00:06 +0000

    Update link to survey for Beta banner

--- server/utils/phaseBannerUtils.ts
+++ server/utils/phaseBannerUtils.ts
@@ -1,6 +1,5 @@
 export const supportEmail = 'cas3support@digital.justice.gov.uk'
-export const feedbackLink =
-  'https://forms.office.com/Pages/ResponsePage.aspx?id=KEeHxuZx_kGp4S6MNndq2EXSevEnO7lHpH52Z5zJdrxUQjQ0OTZERFI4N0w4Q1Q5ODRYWVFIVFBGNy4u'
+export const feedbackLink = 'https://forms.office.com/e/SsWLbpUgtx'
 
 export const getContent = () => {
   return `This is a new service. <a class="govuk-link" href="${feedbackLink}">Give us your feedback</a> or <a class="govuk-link" href="mailto:${supportEmail}">email us</a> to report a bug`

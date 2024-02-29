9ea8eb925686f296c10ff70da4476704f2553f55
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2024-02-29 15:41:23 +0000

    Add e2e test for booking CRN search

--- e2e/tests/stepDefinitions/manage/bookingSearch.ts
+++ e2e/tests/stepDefinitions/manage/bookingSearch.ts
@@ -1,4 +1,4 @@
-import { Given, Then } from '@badeball/cypress-cucumber-preprocessor'
+import { Given, Then, When } from '@badeball/cypress-cucumber-preprocessor'
 import Page from '../../../../cypress_shared/pages/page'
 import BookingSearchPage from '../../../../cypress_shared/pages/temporary-accommodation/manage/bookingSearch'
 import DashboardPage from '../../../../cypress_shared/pages/temporary-accommodation/dashboardPage'
@@ -52,6 +52,55 @@ Then('I should see a summary of the booking on the departed bookings page', () =
     bookingSearchPage.clickOtherBookingStatusLink('departed')
     bookingSearchPage.checkBookingStatus('departed')
 
+    bookingSearchPage.checkBookingDetailsAndClickView(this.premises, this.booking)
+  })
+})
+
+When('I search for a CRN that does not exist in provisional bookings', () => {
+  cy.then(function _() {
+    const bookingSearchPage = Page.verifyOnPage(BookingSearchPage, 'provisional')
+    bookingSearchPage.searchByCRN('N000000', 'provisional')
+  })
+})
+
+Then('I should see a message that the provisional booking is not found', () => {
+  cy.then(function _() {
+    const bookingSearchPage = Page.verifyOnPage(BookingSearchPage, 'provisional')
+    bookingSearchPage.checkNoResultsByCRN('provisional', 'N000000')
+  })
+})
+
+When('I click on the Departed bookings tab', () => {
+  cy.then(function _() {
+    const bookingSearchPage = Page.verifyOnPage(BookingSearchPage)
+    bookingSearchPage.clickOtherBookingStatusLink('departed')
+  })
+})
+
+Then('I should see a message that the departed booking is not found', () => {
+  cy.then(function _() {
+    const bookingSearchPage = Page.verifyOnPage(BookingSearchPage, 'departed')
+    bookingSearchPage.checkNoResultsByCRN('departed', 'N000000')
+  })
+})
+
+When('I click on the Provisional bookings tab', () => {
+  cy.then(function _() {
+    const bookingSearchPage = Page.verifyOnPage(BookingSearchPage, 'departed')
+    bookingSearchPage.clickOtherBookingStatusLink('provisional')
+  })
+})
+
+When('I search for a valid CRN in provisional bookings', () => {
+  cy.then(function _() {
+    const bookingSearchPage = Page.verifyOnPage(BookingSearchPage, 'provisional')
+    bookingSearchPage.searchByCRN(this.booking.person.crn, 'provisional')
+  })
+})
+
+Then('I see the provisional booking I was searching for', () => {
+  cy.then(function _() {
+    const bookingSearchPage = Page.verifyOnPage(BookingSearchPage, 'provisional')
     bookingSearchPage.checkBookingDetailsAndClickView(this.premises, this.booking)
   })
 })

87e7ebfeb5bed051a8314caa5b9380eb68433009
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2024-05-14 12:24:48 +0100

    Remove extra comma from task name

--- server/form-pages/apply/requirements-for-placement/disability-cultural-and-specific-needs/index.ts
+++ server/form-pages/apply/requirements-for-placement/disability-cultural-and-specific-needs/index.ts
@@ -7,7 +7,7 @@ import ReligiousOrCulturalNeeds from './religiousOrCulturalNeeds'
 
 @Task({
   name: 'Health, disability and cultural needs',
-  actionText: 'Add health, disability, and cultural needs',
+  actionText: 'Add health, disability and cultural needs',
   slug: 'disability-cultural-and-specific-needs',
   pages: [Needs, PropertyAttributesOrAdaptations, ReligiousOrCulturalNeeds],
 })

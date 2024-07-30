a2e590e5d372dab4e1b1b42252cf2f57af9c972b
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2024-07-30 11:56:35 +0100

    Tidy up test suite
    This separates the concerns of showing the form and showing results to
    accommodate coming changes to layout.

--- server/controllers/temporary-accommodation/manage/bedspaceSearchController.test.ts
+++ server/controllers/temporary-accommodation/manage/bedspaceSearchController.test.ts
@@ -41,152 +41,137 @@ describe('BedspaceSearchController', () => {
   beforeEach(() => {
     request = createMock<Request>()
     ;(extractCallConfig as jest.MockedFn<typeof extractCallConfig>).mockReturnValue(callConfig)
+    ;(fetchErrorsAndUserInput as jest.Mock).mockReturnValue({ errors: {}, errorSummary: [], userInput: {} })
+    ;(preservePlaceContext as jest.MockedFunction<typeof preservePlaceContext>).mockResolvedValue(undefined)
+    bedspaceSearchService.getReferenceData.mockResolvedValue(referenceData)
   })
 
   describe('index', () => {
-    it('renders the search page with default duration when not given a search query', async () => {
-      request.query = {
-        'other-parameter': 'other-data',
-      }
+    describe('when searching', () => {
+      it('renders the search form with default values', async () => {
+        const requestHandler = bedspaceSearchController.index()
 
-      bedspaceSearchService.getReferenceData.mockResolvedValue(referenceData)
+        await requestHandler(request, response, next)
 
-      const requestHandler = bedspaceSearchController.index()
-      ;(fetchErrorsAndUserInput as jest.Mock).mockReturnValue({ errors: {}, errorSummary: [], userInput: {} })
+        expect(bedspaceSearchService.getReferenceData).toHaveBeenCalledWith(callConfig)
+        expect(bedspaceSearchService.search).not.toHaveBeenCalled()
+        expect(preservePlaceContext).toHaveBeenCalledWith(request, response, assessmentService)
 
-      await requestHandler(request, response, next)
+        expect(response.render).toHaveBeenCalledWith('temporary-accommodation/bedspace-search/index', {
+          allPdus: referenceData.pdus,
+          errors: {},
+          errorSummary: [],
+          durationDays: DEFAULT_DURATION_DAYS,
+        })
+      })
 
-      expect(bedspaceSearchService.getReferenceData).toHaveBeenCalledWith(callConfig)
-      expect(bedspaceSearchService.search).not.toHaveBeenCalled()
-      expect(preservePlaceContext).toHaveBeenCalledWith(request, response, assessmentService)
+      it('prefills the start date if an arrival date is present in a place context', async () => {
+        const placeContext = placeContextFactory.build({
+          assessment: assessmentFactory.build({
+            accommodationRequiredFromDate: '2024-02-01',
+          }),
+        })
 
-      expect(response.render).toHaveBeenCalledWith('temporary-accommodation/bedspace-search/index', {
-        allPdus: referenceData.pdus,
-        errors: {},
-        errorSummary: [],
-        durationDays: DEFAULT_DURATION_DAYS,
+        ;(preservePlaceContext as jest.MockedFunction<typeof preservePlaceContext>).mockResolvedValue(placeContext)
+
+        const requestHandler = bedspaceSearchController.index()
+
+        await requestHandler(request, response, next)
+
+        expect(response.render).toHaveBeenCalledWith(
+          'temporary-accommodation/bedspace-search/index',
+          expect.objectContaining(DateFormats.isoToDateAndTimeInputs('2024-02-01', 'startDate')),
+        )
+      })
+
+      it('renders with errors if the API returns an error', async () => {
+        const searchParameters = bedSearchParametersFactory.build()
+        const placeContext = placeContextFactory.build()
+
+        request.query = {
+          ...searchParameters,
+          durationDays: searchParameters.durationDays.toString(),
+          ...DateFormats.isoToDateAndTimeInputs(searchParameters.startDate, 'startDate'),
+        }
+
+        const err = new Error()
+        bedspaceSearchService.search.mockImplementation(() => {
+          throw err
+        })
+
+        const requestHandler = bedspaceSearchController.index()
+        ;(preservePlaceContext as jest.MockedFunction<typeof preservePlaceContext>).mockResolvedValue(placeContext)
+        ;(addPlaceContext as jest.MockedFunction<typeof addPlaceContext>).mockReturnValue('/path/with/place/context')
+
+        await requestHandler(request, response, next)
+
+        expect(bedspaceSearchService.search).toHaveBeenCalledWith(callConfig, expect.objectContaining(searchParameters))
+        expect(preservePlaceContext).toHaveBeenCalledWith(request, response, assessmentService)
+        expect(addPlaceContext).toHaveBeenCalledWith(paths.bedspaces.search({}), placeContext)
+
+        expect(setUserInput).toHaveBeenCalledWith(request.query, 'get')
+        expect(catchValidationErrorOrPropogate).toHaveBeenCalledWith(
+          request,
+          response,
+          err,
+          '/path/with/place/context',
+          'bedspaceSearch',
+        )
       })
     })
 
-    it('prefills the start date if an arrival date is present in a place context', async () => {
-      request.query = {}
-      const placeContext = placeContextFactory.build({
-        assessment: assessmentFactory.build({
-          accommodationRequiredFromDate: '2024-02-01',
-        }),
+    describe('when showing results', () => {
+      it('renders the search results page with search results when given a search query', async () => {
+        const searchParameters = bedSearchParametersFactory.build()
+
+        request.query = {
+          ...searchParameters,
+          durationDays: searchParameters.durationDays.toString(),
+          ...DateFormats.isoToDateAndTimeInputs(searchParameters.startDate, 'startDate'),
+        }
+
+        const searchResults = bedSearchResultsFactory.build()
+
+        bedspaceSearchService.search.mockResolvedValue(searchResults)
+
+        const requestHandler = bedspaceSearchController.index()
+
+        await requestHandler(request, response, next)
+
+        expect(response.render).toHaveBeenCalledWith('temporary-accommodation/bedspace-search/index', {
+          allPdus: referenceData.pdus,
+          results: searchResults,
+          errors: {},
+          errorSummary: [],
+          ...request.query,
+        })
       })
 
-      bedspaceSearchService.getReferenceData.mockResolvedValue(referenceData)
-      ;(preservePlaceContext as jest.MockedFunction<typeof preservePlaceContext>).mockResolvedValue(placeContext)
+      it('updates the place context when given a search query', async () => {
+        const searchParameters = bedSearchParametersFactory.build()
+        const placeContext = placeContextFactory.build()
 
-      const requestHandler = bedspaceSearchController.index()
-      ;(fetchErrorsAndUserInput as jest.Mock).mockReturnValue({ errors: {}, errorSummary: [], userInput: {} })
+        request.query = {
+          ...searchParameters,
+          durationDays: searchParameters.durationDays.toString(),
+          ...DateFormats.isoToDateAndTimeInputs(searchParameters.startDate, 'startDate'),
+        }
 
-      await requestHandler(request, response, next)
+        const searchResults = bedSearchResultsFactory.build()
 
-      expect(bedspaceSearchService.getReferenceData).toHaveBeenCalledWith(callConfig)
-      expect(bedspaceSearchService.search).not.toHaveBeenCalled()
-      expect(preservePlaceContext).toHaveBeenCalledWith(request, response, assessmentService)
+        bedspaceSearchService.search.mockResolvedValue(searchResults)
+        ;(preservePlaceContext as jest.MockedFunction<typeof preservePlaceContext>).mockResolvedValue(placeContext)
 
-      expect(response.render).toHaveBeenCalledWith(
-        'temporary-accommodation/bedspace-search/index',
-        expect.objectContaining(DateFormats.isoToDateAndTimeInputs('2024-02-01', 'startDate')),
-      )
-    })
+        const requestHandler = bedspaceSearchController.index()
 
-    it('renders the search page with search results when given a search query', async () => {
-      const searchParameters = bedSearchParametersFactory.build()
+        await requestHandler(request, response, next)
 
-      request.query = {
-        ...searchParameters,
-        durationDays: searchParameters.durationDays.toString(),
-        ...DateFormats.isoToDateAndTimeInputs(searchParameters.startDate, 'startDate'),
-      } as Record<string, string>
-
-      const searchResults = bedSearchResultsFactory.build()
-
-      bedspaceSearchService.getReferenceData.mockResolvedValue(referenceData)
-      bedspaceSearchService.search.mockResolvedValue(searchResults)
-
-      const requestHandler = bedspaceSearchController.index()
-      ;(fetchErrorsAndUserInput as jest.Mock).mockReturnValue({ errors: {}, errorSummary: [], userInput: {} })
-
-      await requestHandler(request, response, next)
-
-      expect(bedspaceSearchService.getReferenceData).toHaveBeenCalledWith(callConfig)
-      expect(bedspaceSearchService.search).toHaveBeenCalledWith(callConfig, expect.objectContaining(searchParameters))
-      expect(preservePlaceContext).toHaveBeenCalledWith(request, response, assessmentService)
-
-      expect(response.render).toHaveBeenCalledWith('temporary-accommodation/bedspace-search/index', {
-        allPdus: referenceData.pdus,
-        results: searchResults,
-        errors: {},
-        errorSummary: [],
-        ...request.query,
+        expect(updatePlaceContextWithArrivalDate).toHaveBeenCalledWith(
+          response,
+          placeContext,
+          searchParameters.startDate,
+        )
       })
-    })
-
-    it('updates the place context when given a search query', async () => {
-      const searchParameters = bedSearchParametersFactory.build()
-      const placeContext = placeContextFactory.build()
-
-      request.query = {
-        ...searchParameters,
-        durationDays: searchParameters.durationDays.toString(),
-        ...DateFormats.isoToDateAndTimeInputs(searchParameters.startDate, 'startDate'),
-      } as Record<string, string>
-
-      const searchResults = bedSearchResultsFactory.build()
-
-      bedspaceSearchService.getReferenceData.mockResolvedValue(referenceData)
-      bedspaceSearchService.search.mockResolvedValue(searchResults)
-      ;(preservePlaceContext as jest.MockedFunction<typeof preservePlaceContext>).mockResolvedValue(placeContext)
-
-      const requestHandler = bedspaceSearchController.index()
-      ;(fetchErrorsAndUserInput as jest.Mock).mockReturnValue({ errors: {}, errorSummary: [], userInput: {} })
-
-      await requestHandler(request, response, next)
-
-      expect(updatePlaceContextWithArrivalDate).toHaveBeenCalledWith(response, placeContext, searchParameters.startDate)
-    })
-
-    it('renders with errors if the API returns an error', async () => {
-      const searchParameters = bedSearchParametersFactory.build()
-      const placeContext = placeContextFactory.build()
-
-      request.query = {
-        ...searchParameters,
-        durationDays: searchParameters.durationDays.toString(),
-        ...DateFormats.isoToDateAndTimeInputs(searchParameters.startDate, 'startDate'),
-      } as Record<string, string>
-
-      bedspaceSearchService.getReferenceData.mockResolvedValue(referenceData)
-
-      const err = new Error()
-      bedspaceSearchService.search.mockImplementation(() => {
-        throw err
-      })
-
-      const requestHandler = bedspaceSearchController.index()
-      ;(fetchErrorsAndUserInput as jest.Mock).mockReturnValue({ errors: {}, errorSummary: [], userInput: {} })
-      ;(preservePlaceContext as jest.MockedFunction<typeof preservePlaceContext>).mockResolvedValue(placeContext)
-      ;(addPlaceContext as jest.MockedFunction<typeof addPlaceContext>).mockReturnValue('/path/with/place/context')
-
-      await requestHandler(request, response, next)
-
-      expect(bedspaceSearchService.getReferenceData).toHaveBeenCalledWith(callConfig)
-      expect(bedspaceSearchService.search).toHaveBeenCalledWith(callConfig, expect.objectContaining(searchParameters))
-      expect(preservePlaceContext).toHaveBeenCalledWith(request, response, assessmentService)
-      expect(addPlaceContext).toHaveBeenCalledWith(paths.bedspaces.search({}), placeContext)
-
-      expect(setUserInput).toHaveBeenCalledWith(request.query, 'get')
-      expect(catchValidationErrorOrPropogate).toHaveBeenCalledWith(
-        request,
-        response,
-        err,
-        '/path/with/place/context',
-        'bedspaceSearch',
-      )
     })
   })
 })

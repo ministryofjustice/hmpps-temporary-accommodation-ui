803c621cccbe32fb203c01e56cd9a1c09fb8a5b7
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2024-08-29 17:09:38 +0100

    Enable granular log level
    ALlows setting a `BUNYAN_LOG_LEVEL` environment variable to controll the
    level of logging for the Express server. If this is not set, the log
    level reverts to the default of `fatal` when NODE_ENV=test, or `debug`
    otherwise.

--- logger.ts
+++ logger.ts
@@ -1,11 +1,11 @@
-import bunyan from 'bunyan'
+import bunyan, { LogLevel } from 'bunyan'
 import bunyanFormat from 'bunyan-format'
 
 const formatOut = bunyanFormat({ outputMode: 'short', color: true })
 
-const logger =
-  process.env.NODE_ENV !== 'test'
-    ? bunyan.createLogger({ name: 'Temporary Accommodation UI', stream: formatOut, level: 'debug' })
-    : bunyan.createLogger({ name: 'Temporary Accommodation UI', stream: formatOut, level: 'fatal' })
+const logLevel: LogLevel =
+  (process.env.BUNYAN_LOG_LEVEL as LogLevel) || process.env.NODE_ENV !== 'test' ? 'debug' : 'fatal'
+
+const logger = bunyan.createLogger({ name: 'Temporary Accommodation UI', stream: formatOut, level: logLevel })
 
 export default logger

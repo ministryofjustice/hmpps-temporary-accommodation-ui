4236e91d934a1a13c8d9dccfd74e9cf9217c8c40
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2024-09-11 09:56:16 +0100

    Match new pathToRegexp signature and return type

--- server/middleware/auditMiddleware.ts
+++ server/middleware/auditMiddleware.ts
@@ -19,10 +19,10 @@ export const auditMiddleware = (
   auditEventSpec?: AuditEventSpec,
 ) => {
   if (auditEventSpec) {
-    const redirectMatchers: RedirectAuditMatcher[] = auditEventSpec.redirectAuditEventSpecs?.map(
+    const redirectMatchers: Array<RedirectAuditMatcher> = auditEventSpec.redirectAuditEventSpecs?.map(
       ({ path, auditEvent: redirectAuditEvent }) => {
-        const keys: Key[] = []
-        return { auditEvent: redirectAuditEvent, keys, regExp: pathToRegexp(path, keys) }
+        const parsedRegex = pathToRegexp(path)
+        return { auditEvent: redirectAuditEvent, keys: parsedRegex.keys, regExp: parsedRegex.regexp }
       },
     )
 

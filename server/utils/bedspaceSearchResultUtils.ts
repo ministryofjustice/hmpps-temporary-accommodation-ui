a5c204c6c13b69093c2ba513e272522d2a19994e
Brian McHugh <brian.mchugh@digital.justice.gov.uk>
2024-12-02 13:13:33 +0000

    Draft commit of adding referral info

--- server/utils/bedspaceSearchResultUtils.ts
+++ server/utils/bedspaceSearchResultUtils.ts
@@ -1,4 +1,7 @@
-import { BedSearchResult, TemporaryAccommodationBedSearchResultOverlap } from '@approved-premises/api'
+import { BedSearchResult, TemporaryAccommodationBedSearchResultOverlap, Person } from '@approved-premises/api'
+import { PersonType } from '../@types/shared/models/PersonType';
+import { person } from '../../e2e/tests/stepDefinitions/utils';
+import { personName } from '../utils/personUtils';
 
 export function premisesKeyCharacteristics(result: BedSearchResult): Array<string> {
   return result.premises.characteristics
@@ -13,15 +16,21 @@ export function bedspaceKeyCharacteristics(result: BedSearchResult): Array<strin
 }
 
 export function bedspaceOverlapResult(
-  overlapResult: Omit<TemporaryAccommodationBedSearchResultOverlap, 'name' | 'sex' | 'assesmentId' | 'personType'>,
+  overlapResult: Omit<TemporaryAccommodationBedSearchResultOverlap, 'sex'>,
 ) {
-  const { crn, days, roomId, bookingId } = overlapResult
+  const { crn, days, roomId, bookingId, personType, name, assessmentId } = overlapResult
   const overlapDays = `${days} ${days === 1 ? 'day' : 'days'} overlap`
+  // const person: Person = { crn, type: personType }
+  const personDisplayName = (personType === "FullPerson") ? name : 'Limited access offender'
 
   return {
     crn,
     overlapDays,
     roomId,
     bookingId,
+    name,
+    // person
+    personName: personDisplayName,
+    assessmentId
   }
 }

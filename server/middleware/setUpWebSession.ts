fcdb0290900bfc1a8267a019438b3619bf885bd6
Alistair Laing <aliuk2012@users.noreply.github.com>
2024-12-31 11:39:49 +0000

    Use the named export to address breaking change
    See https://github.com/tj/connect-redis/releases/tag/v8.0.0

--- server/middleware/setUpWebSession.ts
+++ server/middleware/setUpWebSession.ts
@@ -1,4 +1,4 @@
-import RedisStore from 'connect-redis'
+import { RedisStore } from 'connect-redis'
 import express, { Router } from 'express'
 import session from 'express-session'
 import logger from '../../logger'

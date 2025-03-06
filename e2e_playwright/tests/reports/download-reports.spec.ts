ea668b4ba3d4b47f0c1073833c759db8a4c0a439
Greg Hawkins <greg.hawkins@digital.justice.gov.uk>
2025-03-06 12:15:20 +0000

    Playwright CI config changes

--- e2e_playwright/tests/reports/download-reports.spec.ts
+++ e2e_playwright/tests/reports/download-reports.spec.ts
@@ -2,30 +2,27 @@ import { test } from '../../test'
 import { signIn } from '../../steps/signIn'
 import { visitReportsPageAndDownloadReport } from '../../steps/reports'
 
-const startDate = '01/03/2024'
-const endDate = '01/04/2024'
-
 test('Download booking report for a probation region', async ({ page, assessor }) => {
   await signIn(page, assessor)
-  await visitReportsPageAndDownloadReport(page, 'book', assessor.probationRegion, startDate, endDate)
+  await visitReportsPageAndDownloadReport(page, 'book', assessor.probationRegion)
 })
 
 test('Download bed-space usage report for a probation region', async ({ page, assessor }) => {
   await signIn(page, assessor)
-  await visitReportsPageAndDownloadReport(page, 'bedSpace', assessor.probationRegion, startDate, endDate)
+  await visitReportsPageAndDownloadReport(page, 'bedSpace', assessor.probationRegion)
 })
 
 test('Download future bookings report for a probation region', async ({ page, assessor }) => {
   await signIn(page, assessor)
-  await visitReportsPageAndDownloadReport(page, 'futureBookings', assessor.probationRegion, startDate, endDate)
+  await visitReportsPageAndDownloadReport(page, 'futureBookings', assessor.probationRegion)
 })
 
 test('Download occupancy report for a probation region', async ({ page, assessor }) => {
   await signIn(page, assessor)
-  await visitReportsPageAndDownloadReport(page, 'occupancy', assessor.probationRegion, startDate, endDate)
+  await visitReportsPageAndDownloadReport(page, 'occupancy', assessor.probationRegion)
 })
 
 test('Download referrals report for a probation region', async ({ page, assessor }) => {
   await signIn(page, assessor)
-  await visitReportsPageAndDownloadReport(page, 'referrals', assessor.probationRegion, startDate, endDate)
+  await visitReportsPageAndDownloadReport(page, 'referrals', assessor.probationRegion)
 })

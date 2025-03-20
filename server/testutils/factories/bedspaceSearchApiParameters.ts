5d483f49a39cc292c671ee12a2b2b446526ac207
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2025-03-20 12:53:11 +0000

    Remove unused bedspaceSearchAttributes from bedspaceSearchApiParameters factory

--- server/testutils/factories/bedspaceSearchApiParameters.ts
+++ server/testutils/factories/bedspaceSearchApiParameters.ts
@@ -1,19 +1,12 @@
 import { fakerEN_GB as faker } from '@faker-js/faker'
 import { Factory } from 'fishery'
 
-import { BedspaceSearchAttributes, Cas3BedspaceSearchParameters } from '@approved-premises/api'
+import { Cas3BedspaceSearchParameters } from '@approved-premises/api'
 import { DateFormats } from '../../utils/dateUtils'
 import referenceData from './referenceData'
-
-const bedspaceSearchAttributes: Array<BedspaceSearchAttributes> = [
-  'isSingleOccupancy',
-  'isSharedProperty',
-  'isWheelchairAccessible',
-]
 
 export default Factory.define<Cas3BedspaceSearchParameters>(() => ({
   startDate: DateFormats.dateObjToIsoDate(faker.date.soon()),
   durationDays: faker.number.int({ min: 1, max: 10 }),
   probationDeliveryUnits: [referenceData.pdu().build().id, referenceData.pdu().build().id],
-  attributes: faker.helpers.arrayElements(bedspaceSearchAttributes),
 }))

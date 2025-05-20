9595254d2c2d2118a3c7c4dd2bfa72c77a77a819
Greg Hawkins <greg.hawkins@digital.justice.gov.uk>
2025-05-20 12:02:36 +0100

    Change log-level to info + stop logging username

--- server/errorHandler.ts
+++ server/errorHandler.ts
@@ -7,7 +7,7 @@ import paths from './paths/temporary-accommodation/static'
 
 export default function createErrorHandler(production: boolean) {
   return (error: HTTPError, req: Request, res: Response, next: NextFunction): void => {
-    logger.error(`Error handling request for '${req.originalUrl}', user '${res.locals.user?.username}'`, error)
+    logger.error(`Error handling request for '${req.originalUrl}', user ID '${res.locals.user?.id}'`, error)
 
     if (error.status === 401 || error.status === 403) {
       logger.info('User not authorised')

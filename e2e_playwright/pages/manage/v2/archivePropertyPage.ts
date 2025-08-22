50786129b3858b747d1f866ed7532e329e3c5d28
robanderson-moj <rob.anderson1@justice.gov.uk>
2025-08-22 13:43:30 +0100

    E2E coverage for archive history

--- e2e_playwright/pages/manage/v2/archivePropertyPage.ts
+++ e2e_playwright/pages/manage/v2/archivePropertyPage.ts
@@ -6,4 +6,10 @@ export class ArchivePropertyPage extends BasePage {
     await expect(page.locator('h1')).toContainText(`When should ${addressLine1} be archived?`)
     return new ArchivePropertyPage(page)
   }
+
+  async archiveToday(): Promise<Date> {
+    await this.page.getByLabel('Today').click()
+    await this.clickSubmit()
+    return new Date()
+  }
 }

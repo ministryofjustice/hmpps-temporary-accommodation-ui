cec07bea51762a41eddaa6925c08d1dca7474f3a
robanderson-moj <rob.anderson1@justice.gov.uk>
2025-09-24 11:13:07 +0100

    Updated support content in the phase banner footer

--- server/utils/phaseBannerUtils.test.ts
+++ server/utils/phaseBannerUtils.test.ts
@@ -2,7 +2,7 @@ import { getContent } from './phaseBannerUtils'
 
 describe('when there is a feedback link', () => {
   it('returns correct feedback link for path', () => {
-    const content = `This is a new service. <a class="govuk-link" href="${'https://forms.office.com/e/SsWLbpUgtx'}">Give us your feedback</a> or <a class="govuk-link" href="mailto:cas3support@digital.justice.gov.uk">email us</a> to report a bug`
+    const content = `This is a new service. <a class="govuk-link" href="${'https://forms.office.com/e/SsWLbpUgtx'}">Give us your feedback</a> or raise a <a class="govuk-link" href="https://mojprod.service-now.com/moj_sp?id=sc_cat_item&table=sc_cat_item&sys_id=4f0128741b6fd61025dc6351f54bcb31">support ticket</a> to get help or report a bug.`
     expect(getContent()).toEqual(content)
   })
 })
--- server/utils/phaseBannerUtils.ts
+++ server/utils/phaseBannerUtils.ts
@@ -1,6 +1,8 @@
 export const supportEmail = 'cas3support@digital.justice.gov.uk'
 export const feedbackLink = 'https://forms.office.com/e/SsWLbpUgtx'
+export const supportLink =
+  'https://mojprod.service-now.com/moj_sp?id=sc_cat_item&table=sc_cat_item&sys_id=4f0128741b6fd61025dc6351f54bcb31'
 
 export const getContent = () => {
-  return `This is a new service. <a class="govuk-link" href="${feedbackLink}">Give us your feedback</a> or <a class="govuk-link" href="mailto:${supportEmail}">email us</a> to report a bug`
+  return `This is a new service. <a class="govuk-link" href="${feedbackLink}">Give us your feedback</a> or raise a <a class="govuk-link" href="${supportLink}">support ticket</a> to get help or report a bug.`
 }

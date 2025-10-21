fb9cb71d25ef3c213f77ab30bafccf96bf27b71e
Fred Marecesche <fred.marecesche@justice.gov.uk>
2025-10-21 16:03:36 +0100

    Clean up API endpoint paths definitions
    This removes any secondary variable assignments which were just making
    the file very difficult to read. The only set of endpoints that still
    has its own variable is the manage CAS3 paths, as these will be
    overwritten where necessary by the newer, cas3v2 endpoints.

--- server/paths/api.ts
+++ server/paths/api.ts
@@ -1,74 +1,54 @@
 import { path } from 'static-path'
 
+// CAS3 namespaced
 const cas3Path = path('/cas3')
-const assessmentsCas3Path = cas3Path.path('assessments')
-const premisesPath = path('/premises')
 const cas3PremisesPath = cas3Path.path('premises')
-const singlePremisesPath = premisesPath.path(':premisesId')
 const singlePremisesCas3Path = cas3PremisesPath.path(':premisesId')
 const singleBookingCas3Path = singlePremisesCas3Path.path('bookings/:bookingId')
-const premisesSearchPath = cas3PremisesPath.path('search')
-const premisesCanArchivePath = singlePremisesCas3Path.path('can-archive')
-const premisesArchivePath = singlePremisesCas3Path.path('archive')
-const premisesUnarchivePath = singlePremisesCas3Path.path('unarchive')
-const premisesCancelArchivePath = singlePremisesCas3Path.path('cancel-archive')
-const premisesCancelUnarchivePath = singlePremisesCas3Path.path('cancel-unarchive')
-
-const lostBedsPath = singlePremisesPath.path('lost-beds')
-const singleLostBedPath = lostBedsPath.path(':lostBedId')
-const cancelLostBedPath = singleLostBedPath.path('cancellations')
 
 const bedspacesCas3Path = singlePremisesCas3Path.path('bedspaces')
 const singleBedspacePath = bedspacesCas3Path.path(':bedspaceId')
 
-const bedspacesPath = cas3Path.path('/bedspaces')
-const searchBedspacesPath = bedspacesPath.path('search')
+const reportsCas3Path = cas3Path.path('reports')
 
-const bookingsPath = path('/bookings')
-const searchBookingsPath = bookingsPath.path('search')
+const timelineCas3Path = cas3Path.path('timeline').path(':assessmentId')
 
-const assessPaths = {
-  assessments: path('/assessments'),
-  singleAssessment: path('/assessments/:id'),
-  allocation: path('/tasks/assessment/:id/allocations'),
-  rejection: path('/assessments/:id/rejection'),
-  acceptance: path('/assessments/:id/acceptance'),
-  closure: path('/assessments/:id/closure'),
-}
+const applicationsCas3Path = cas3Path.path('applications')
+const singleApplicationCas3Path = applicationsCas3Path.path(':id')
 
-const clarificationNotePaths = {
-  notes: assessPaths.singleAssessment.path('notes'),
-}
+const assessmentsCas3Path = cas3Path.path('assessments')
+const singleAssessmentCas3Path = assessmentsCas3Path.path(':id')
 
-const managePaths = {
-  premises: {
-    lostBeds: {
-      show: singleLostBedPath,
-      create: lostBedsPath,
-      index: lostBedsPath,
-      cancel: cancelLostBedPath,
-      update: singleLostBedPath,
-    },
-  },
-  bedspaces: {
-    search: searchBedspacesPath,
-  },
-  bookings: {
-    search: searchBookingsPath,
-  },
-}
+// Non-namespaced
+const premisesPath = path('/premises')
+const singlePremisesPath = premisesPath.path(':premisesId')
+
+const lostBedsPath = singlePremisesPath.path('lost-beds')
+const singleLostBedPath = lostBedsPath.path(':lostBedId')
+
+const peoplePath = path('/people')
+const personPath = peoplePath.path(':crn')
+const oasysPath = personPath.path('oasys')
+
+const tasksPath = path('/tasks')
+const allocationPath = tasksPath.path('assessment/:id/allocations')
+
+const assessmentsPath = path('/assessments')
+const singleAssessmentPath = assessmentsPath.path(':id')
+
+const clarificationNotesPath = singleAssessmentPath.path('notes')
 
 const managePathsCas3 = {
   premises: {
-    search: premisesSearchPath,
+    search: cas3PremisesPath.path('search'),
     show: singlePremisesCas3Path,
     create: cas3PremisesPath,
     update: singlePremisesCas3Path,
-    canArchive: premisesCanArchivePath,
-    archive: premisesArchivePath,
-    unarchive: premisesUnarchivePath,
-    cancelArchive: premisesCancelArchivePath,
-    cancelUnarchive: premisesCancelUnarchivePath,
+    canArchive: singlePremisesCas3Path.path('can-archive'),
+    archive: singlePremisesCas3Path.path('archive'),
+    unarchive: singlePremisesCas3Path.path('unarchive'),
+    cancelArchive: singlePremisesCas3Path.path('cancel-archive'),
+    cancelUnarchive: singlePremisesCas3Path.path('cancel-unarchive'),
     totals: singlePremisesCas3Path.path('bedspace-totals'),
     bedspaces: {
       show: singleBedspacePath,
@@ -86,92 +66,53 @@ const managePathsCas3 = {
       departure: singleBookingCas3Path.path('departures'),
     },
   },
+  applications: {
+    index: applicationsCas3Path,
+    show: singleApplicationCas3Path,
+  },
+  assessments: {
+    index: assessmentsCas3Path,
+    show: assessmentsCas3Path.path(':id'),
+    update: assessmentsCas3Path.path(':id'),
+    notes: singleAssessmentCas3Path.path('referral-history-notes'),
+  },
 }
 
-const peoplePath = path('/people')
-const personPath = peoplePath.path(':crn')
-const oasysPath = personPath.path('oasys')
-
-const reportsCas3Path = cas3Path.path('reports')
-const timelineCas3Path = cas3Path.path('timeline').path(':assessmentId')
-const applicationsCas3Path = cas3Path.path('applications')
-const deleteApplicationCas3Path = cas3Path.path('/applications/:id')
-const submitApplicationCas3Path = cas3Path.path('/applications/:id/submission')
-const singleApplicationCas3Path = applicationsCas3Path.path(':id')
-
 export default {
-  cas3: {
-    premises: {
-      search: managePathsCas3.premises.search,
-      show: managePathsCas3.premises.show,
-      create: managePathsCas3.premises.create,
-      update: managePathsCas3.premises.update,
-      canArchive: managePathsCas3.premises.canArchive,
-      archive: managePathsCas3.premises.archive,
-      unarchive: managePathsCas3.premises.unarchive,
-      cancelArchive: managePathsCas3.premises.cancelArchive,
-      cancelUnarchive: managePathsCas3.premises.cancelUnarchive,
-      totals: managePathsCas3.premises.totals,
-      bedspaces: {
-        show: managePathsCas3.premises.bedspaces.show,
-        create: managePathsCas3.premises.bedspaces.create,
-        get: managePathsCas3.premises.bedspaces.get,
-        update: managePathsCas3.premises.bedspaces.update,
-        canArchive: managePathsCas3.premises.bedspaces.canArchive,
-        archive: managePathsCas3.premises.bedspaces.archive,
-        unarchive: managePathsCas3.premises.bedspaces.unarchive,
-        cancelArchive: managePathsCas3.premises.bedspaces.cancelArchive,
-        cancelUnarchive: managePathsCas3.premises.bedspaces.cancelUnarchive,
-      },
-      bookings: {
-        arrival: managePathsCas3.premises.bookings.arrival,
-        departure: managePathsCas3.premises.bookings.departure,
-      },
-    },
-    applications: {
-      index: applicationsCas3Path,
-      show: singleApplicationCas3Path,
-    },
-    assessments: {
-      index: assessmentsCas3Path,
-      show: assessmentsCas3Path.path(':id'),
-      update: assessmentsCas3Path.path(':id'),
-      notes: assessmentsCas3Path.path(':id/referral-history-notes'),
-    },
-  },
+  cas3: managePathsCas3,
   premises: {
     lostBeds: {
-      show: managePaths.premises.lostBeds.show,
-      create: managePaths.premises.lostBeds.create,
-      index: managePaths.premises.lostBeds.index,
-      cancel: managePaths.premises.lostBeds.cancel,
-      update: managePaths.premises.lostBeds.update,
+      show: singleLostBedPath,
+      create: lostBedsPath,
+      index: lostBedsPath,
+      cancel: singleLostBedPath.path('cancellations'),
+      update: singleLostBedPath,
     },
   },
   bedspaces: {
-    search: managePaths.bedspaces.search,
+    search: cas3Path.path('bedspaces/search'),
   },
   bookings: {
-    search: managePaths.bookings.search,
+    search: path('/bookings/search'),
   },
   applications: {
     show: singleApplicationCas3Path,
     index: applicationsCas3Path,
     update: singleApplicationCas3Path,
-    delete: deleteApplicationCas3Path,
+    delete: singleApplicationCas3Path,
     new: applicationsCas3Path,
-    submission: submitApplicationCas3Path,
+    submission: singleApplicationCas3Path.path('submission'),
   },
   assessments: {
-    index: assessPaths.assessments,
-    show: assessPaths.singleAssessment,
-    allocation: assessPaths.allocation,
-    rejection: assessPaths.rejection,
-    acceptance: assessPaths.acceptance,
-    closure: assessPaths.closure,
+    index: assessmentsPath,
+    show: singleAssessmentPath,
+    allocation: allocationPath,
+    rejection: singleAssessmentPath.path('rejection'),
+    acceptance: singleAssessmentPath.path('acceptance'),
+    closure: singleAssessmentPath.path('closure'),
     clarificationNotes: {
-      create: clarificationNotePaths.notes,
-      update: clarificationNotePaths.notes.path(':clarificationNoteId'),
+      create: clarificationNotesPath,
+      update: clarificationNotesPath.path(':clarificationNoteId'),
     },
     timeline: timelineCas3Path,
   },
@@ -182,7 +123,6 @@ export default {
     acctAlerts: personPath.path('acct-alerts'),
     offences: personPath.path('offences'),
     oasys: {
-      selection: oasysPath.path('selection'),
       sections: oasysPath.path('sections'),
     },
   },

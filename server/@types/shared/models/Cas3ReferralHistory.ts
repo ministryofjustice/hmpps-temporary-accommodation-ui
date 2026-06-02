51fa155e66ccd294dc4d947d614f58618fc0c19c
davidatkinsuk <22135634+davidatkinsuk@users.noreply.github.com>
2026-06-02 13:23:47 +0000

    Updating hmpps-approved-premises-api models from OpenAPI specification

--- server/@types/shared/models/Cas3ReferralHistory.ts
+++ server/@types/shared/models/Cas3ReferralHistory.ts
@@ -2,12 +2,20 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+import type { Cas3StaffDto } from './Cas3StaffDto';
 import type { ServiceType } from './ServiceType';
 import type { TemporaryAccommodationAssessmentStatus } from './TemporaryAccommodationAssessmentStatus';
 export type Cas3ReferralHistory = {
     applicationId: string;
     createdAt: string;
     id: string;
+    localAuthorityArea?: string;
+    pdu?: string;
+    placementAddress?: string;
+    placementStatus?: string;
+    referralRejectionReason?: string;
+    referralRejectionReasonDetail?: string;
+    referredBy?: Cas3StaffDto;
     status: TemporaryAccommodationAssessmentStatus;
     type: ServiceType;
 };
---
+++ server/@types/shared/models/Cas3SuitablePremisesDto.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type Cas3SuitablePremisesDto = {
+    addressLine1: string;
+    addressLine2?: string;
+    endDate?: string;
+    name?: string;
+    postcode: string;
+    startDate?: string;
+    town?: string;
+};
+

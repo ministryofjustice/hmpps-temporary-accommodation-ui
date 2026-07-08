5a2a8452fd57c533b30b6c4198d63ba5b2d4d00f
davidatkinsuk <22135634+davidatkinsuk@users.noreply.github.com>
2026-07-08 11:34:56 +0000

    Updating hmpps-approved-premises-api models from OpenAPI specification

--- server/@types/shared/models/Cas3ReferralHistory.ts
+++ server/@types/shared/models/Cas3ReferralHistory.ts
@@ -6,9 +6,11 @@ import type { ApplicationStatus } from './ApplicationStatus';
 import type { Cas3BookingStatus } from './Cas3BookingStatus';
 import type { Cas3StaffDto } from './Cas3StaffDto';
 import type { ServiceType } from './ServiceType';
+import type { TemporaryAccommodationAssessmentStatus } from './TemporaryAccommodationAssessmentStatus';
 export type Cas3ReferralHistory = {
     applicationId: string;
     applicationStatus: ApplicationStatus;
+    assessmentStatus?: TemporaryAccommodationAssessmentStatus;
     bookingStatus?: Cas3BookingStatus;
     createdAt: string;
     id: string;

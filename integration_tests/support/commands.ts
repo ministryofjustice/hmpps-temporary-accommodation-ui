def891a48ea3b178c812347fed388577e4b49e39
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2026-09-17 11:11:53 +0100

    Add clearCookies command to signIn func

--- integration_tests/support/commands.ts
+++ integration_tests/support/commands.ts
@@ -1,4 +1,5 @@
 Cypress.Commands.add('signIn', (options = { failOnStatusCode: true }) => {
+  cy.clearCookies()
   cy.request('/')
   return cy.task('getSignInUrl').then((url: string) => cy.visit(url, options))
 })

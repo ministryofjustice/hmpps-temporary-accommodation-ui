4222c2044f571e69e03f7eb54c845afe01daaabe
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2026-09-17 18:50:39 +0100

    Use request for signIn

--- integration_tests/support/commands.ts
+++ integration_tests/support/commands.ts
@@ -1,5 +1,17 @@
 Cypress.Commands.add('signIn', (options = { failOnStatusCode: true }) => {
   cy.clearCookies()
   cy.request('/')
-  return cy.task('getSignInUrl').then((url: string) => cy.visit(url, options))
+
+  return cy.task('getSignInUrl').then((url: string) => {
+    return cy
+      .request({
+        url,
+        ...options,
+      })
+      .then(() => {
+        if (options.failOnStatusCode) {
+          return cy.getCookie('connect.sid').should('exist')
+        }
+      })
+  })
 })
